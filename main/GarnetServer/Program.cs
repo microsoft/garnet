3a79558f036c96f29aba9dd741e38fff6822a9c0
nima nikoonazar <nikoonazar.nima@gmail.com>
2024-03-22 02:51:49 +0330

    Fix namespace in Program.cs file change to correct namespace

--- main/GarnetServer/Program.cs
+++ main/GarnetServer/Program.cs
@@ -3,9 +3,10 @@
 
 using System;
 using System.Threading;
+using Garnet;
 using Garnet.server;
 
-namespace Garnet
+namespace GarnetServer
 {
     /// <summary>
     /// Garnet server entry point
@@ -16,7 +17,7 @@ namespace Garnet
         {
             try
             {
-                using var server = new GarnetServer(args);
+                using var server = new Garnet.GarnetServer(args);
 
                 // Optional: register custom extensions
                 RegisterExtensions(server);
@@ -37,7 +38,7 @@ namespace Garnet
         /// commands such as db.Execute in StackExchange.Redis. Example:
         ///   db.Execute("SETIFPM", key, value, prefix);
         /// </summary>
-        static void RegisterExtensions(GarnetServer server)
+        static void RegisterExtensions(Garnet.GarnetServer server)
         {
             // Register custom command on raw strings (SETIFPM = "set if prefix match")
             server.Register.NewCommand("SETIFPM", 2, CommandType.ReadModifyWrite, new SetIfPMCustomCommand());
