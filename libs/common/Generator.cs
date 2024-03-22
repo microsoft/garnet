a8f9a399665062be99f79392421469c197b491cd
PaulusParssinen <paulus.parssinen@gmail.com>
2024-03-22 11:52:36 +0200

    Make Generator.cs generate cryptographically random values

--- libs/common/Generator.cs
+++ libs/common/Generator.cs
@@ -2,17 +2,15 @@
 // Licensed under the MIT license.
 
 using System;
-using System.Text;
+using System.Security.Cryptography;
 
 namespace Garnet.common
 {
     /// <summary>
     /// Collection of methods generating hex ids
     /// </summary>
-    public class Generator
+    public static class Generator
     {
-        static ReadOnlySpan<byte> hexchars => "0123456789abcdef"u8;
-
         /// <summary>
         /// Random hex id
         /// </summary>
@@ -20,10 +18,9 @@ namespace Garnet.common
         /// <returns></returns>
         public static string CreateHexId(int size = 40)
         {
-            var NodeId = new byte[size];
-            new Random(Guid.NewGuid().GetHashCode()).NextBytes(NodeId);
-            for (int i = 0; i < NodeId.Length; i++) NodeId[i] = hexchars[NodeId[i] & 0xf];
-            return Encoding.ASCII.GetString(NodeId);
+            Span<byte> nodeIdBuffer = stackalloc byte[size];
+            RandomNumberGenerator.Fill(nodeIdBuffer);
+            return Convert.ToHexString(nodeIdBuffer).ToLowerInvariant();
         }
 
         /// <summary>
@@ -33,9 +30,9 @@ namespace Garnet.common
         /// <returns></returns>
         public static string DefaultHexId(int size = 40)
         {
-            var NodeId = new byte[size];
-            for (int i = 0; i < NodeId.Length; i++) NodeId[i] = hexchars[0];
-            return Encoding.ASCII.GetString(NodeId);
+            Span<byte> nodeIdBuffer = stackalloc byte[size];
+            nodeIdBuffer.Clear();
+            return Convert.ToHexString(nodeIdBuffer).ToLowerInvariant();
         }
     }
 }
