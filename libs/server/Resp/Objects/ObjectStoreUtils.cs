aee4db1ba70b35d2b6a95d4d525b23d931b4197b
Lukas Maas <lumaas@microsoft.com>
2024-03-22 18:56:08 -0700

    Fixes #argument error handling and ranges for object store commands

--- libs/server/Resp/Objects/ObjectStoreUtils.cs
+++ libs/server/Resp/Objects/ObjectStoreUtils.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+using System;
 using System.Text;
 using Garnet.common;
 
@@ -11,18 +12,6 @@ namespace Garnet.server
     /// </summary>
     internal sealed unsafe partial class RespServerSession : ServerSessionBase
     {
-        /// <summary>
-        /// Send the error of missing arguments for any command
-        /// </summary>
-        /// <param name="cmdName"></param>
-        //todo move this method to write utils ??
-        private void WriteErrorTokenNumberInCommand(string cmdName)
-        {
-            var errorMessage = Encoding.ASCII.GetBytes($"-ERR wrong number of arguments for {cmdName} command.\r\n");
-            while (!RespWriteUtils.WriteResponse(errorMessage, ref dcurr, dend))
-                SendAndReset();
-        }
-
         /// <summary>
         /// Reads the n tokens from the current buffer, and returns
         /// total tokens read
@@ -40,6 +29,29 @@ namespace Garnet.server
             }
 
             return totalTokens;
+        }
+
+        /// <summary>
+        /// Aborts the execution of the current object store command and outputs
+        /// an error message to indicate a wrong number of arguments for the given command.
+        /// </summary>
+        /// <param name="cmdName">Name of the command that caused the error message</param>
+        /// <param name="count">Number of remaining tokens belonging to this command on the receive buffer</param>
+        /// <returns>true if the command was completely consumed, false if the input on the receive buffer was incomplete</returns>
+        private bool AbortWithWrongNumberOfArguments(string cmdName, int count)
+        {
+            // Abort command and discard any remaining tokens on the input buffer.
+            var bufSpan = new ReadOnlySpan<byte>(recvBufferPtr, bytesRead);
+
+            if (!DrainCommands(bufSpan, count))
+                return false;
+
+            // Print error message to result stream
+            var errorMessage = Encoding.ASCII.GetBytes($"-ERR wrong number of arguments for {cmdName} command.\r\n");
+            while (!RespWriteUtils.WriteResponse(errorMessage, ref dcurr, dend))
+                SendAndReset();
+
+            return true;
         }
     }
 }
