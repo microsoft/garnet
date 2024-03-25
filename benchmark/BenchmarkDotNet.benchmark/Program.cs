122fa9b0c8e198d3ca3b65aa72d1f3ce0407e5fc
PaulusParssinen <paulus.parssinen@gmail.com>
2024-03-25 18:17:48 +0200

    Give the .NET 6 benchmark job a name

--- benchmark/BenchmarkDotNet.benchmark/Program.cs
+++ benchmark/BenchmarkDotNet.benchmark/Program.cs
@@ -8,10 +8,11 @@ using BenchmarkDotNet.Running;
 
 var config = DefaultConfig.Instance
     .AddJob(Job.Default
-        .WithRuntime(CoreRuntime.Core60))
+        .WithRuntime(CoreRuntime.Core60)
+        .WithId(".NET 6"))
     .AddJob(Job.Default
         .WithRuntime(CoreRuntime.Core80)
         .WithEnvironmentVariables(new EnvironmentVariable("DOTNET_TieredPGO", "0"))
-        .WithId(".NET 8 PGO"));
+        .WithId(".NET 8"));
 
 BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
