a30be0253ed28017f941358a77fff804b0b9bb01
Vasileios Zois <vazois@microsoft.com>
2024-03-27 08:33:44 -0700

    code cleanup

--- benchmark/BDN.benchmark/Program.cs
+++ benchmark/BDN.benchmark/Program.cs
@@ -1,11 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Environments;
-using BenchmarkDotNet.Jobs;
-using BenchmarkDotNet.Running;
-
 var config = DefaultConfig.Instance
     .AddJob(Job.Default
         .WithRuntime(CoreRuntime.Core60)
--- benchmark/BDN.benchmark/Resp/RespIntegerReadBenchmarks.cs
+++ benchmark/BDN.benchmark/Resp/RespIntegerReadBenchmarks.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license.
 
 using System.Text;
-using BenchmarkDotNet.Attributes;
 using Garnet.common;
 
 namespace BDN.benchmark.Resp
