fba6e6cb489d7328d14667a69f05a7574a47871a
Zzhiter <2631992879@qq.com>
2024-03-29 14:51:37 +0800

    fix: spop set index get error

--- test/Garnet.test/RespSetTest.cs
+++ test/Garnet.test/RespSetTest.cs
@@ -388,6 +388,19 @@ namespace Garnet.test
             Assert.AreEqual(expectedResponse, strResponse);
         }
 
+        [Test]
+        public void CanRemoveAllElementsLC()
+        {
+            var myset = new HashSet<string> { "one", "two", "three", "four", "five" };
+
+            CreateLongSet();
+
+            using var lightClientRequest = TestUtils.CreateRequest();
+            
+            var response = lightClientRequest.SendCommand("SPOP myset 5", 4);
+            var strLen = Encoding.ASCII.GetString(response).Substring(1, 1);  
+            Assert.AreEqual(5, Int32.Parse(strLen));
+        }
 
         [Test]
         public void MultiWithNonExistingSet()
