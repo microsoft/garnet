3d3774b6ab52adfba117978cd18ed4cb68b4fcae
PaulusParssinen <paulus.parssinen@gmail.com>
2024-03-30 00:44:08 +0200

    Use regular array initializer to avoid dotnet-format bug

--- libs/common/NumUtils.cs
+++ libs/common/NumUtils.cs
@@ -403,8 +403,9 @@ namespace Garnet.common
         /// <summary>
         /// This table is based on the CRC-16-CCITT polynomial (0x1021)
         /// </summary>
-        private static ReadOnlySpan<ushort> Crc16Table =>
-        [
+#pragma warning disable IDE0300 // Simplify collection initialization
+        private static ReadOnlySpan<ushort> Crc16Table => new ushort[256]
+        {
             0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50A5, 0x60C6, 0x70E7,
             0x8108, 0x9129, 0xA14A, 0xB16B, 0xC18C, 0xD1AD, 0xE1CE, 0xF1EF,
             0x1231, 0x0210, 0x3273, 0x2252, 0x52B5, 0x4294, 0x72F7, 0x62D6,
@@ -437,7 +438,8 @@ namespace Garnet.common
             0x7C26, 0x6C07, 0x5C64, 0x4C45, 0x3CA2, 0x2C83, 0x1CE0, 0x0CC1,
             0xEF1F, 0xFF3E, 0xCF5D, 0xDF7C, 0xAF9B, 0xBFBA, 0x8FD9, 0x9FF8,
             0x6E17, 0x7E36, 0x4E55, 0x5E74, 0x2E93, 0x3EB2, 0x0ED1, 0x1EF0
-        ];
+        };
+#pragma warning restore IDE0300 // Simplify collection initialization
 
         public static unsafe ushort CRC16(byte* data, int len)
         {
