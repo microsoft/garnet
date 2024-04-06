65e2da7a27406f8c94783ab37237ad4a6616d2ee
PaulusParssinen <paulus.parssinen@gmail.com>
2024-04-06 19:09:38 +0300

    Make FAILOVER use generic error serialization

--- libs/cluster/Session/FailoverCommand.cs
+++ libs/cluster/Session/FailoverCommand.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license.
 
 using System;
-using System.Text;
 using System.Threading;
 using Garnet.common;
 using Garnet.server;
@@ -70,8 +69,7 @@ namespace Garnet.cluster
 
             if (clusterProvider.clusterManager.CurrentConfig.GetLocalNodeRole() != NodeRole.PRIMARY)
             {
-                var resp = CmdStrings.RESP_ERR_GENERIC_CANNOT_FAILOVER_FROM_NON_MASTER;
-                while (!RespWriteUtils.WriteDirect(resp, ref dcurr, dend))
+                while (!RespWriteUtils.WriteGenericError(CmdStrings.RESP_ERR_GENERIC_CANNOT_FAILOVER_FROM_NON_MASTER, ref dcurr, dend))
                     SendAndReset();
                 return true;
             }
@@ -82,8 +80,7 @@ namespace Garnet.cluster
                 var replicaNodeId = clusterProvider.clusterManager.CurrentConfig.GetWorkerNodeIdFromAddress(replicaAddress, replicaPort);
                 if (replicaNodeId == null)
                 {
-                    var resp = CmdStrings.RESP_ERR_GENERIC_UNKNOWN_ENDPOINT;
-                    while (!RespWriteUtils.WriteDirect(resp, ref dcurr, dend))
+                    while (!RespWriteUtils.WriteGenericError(CmdStrings.RESP_ERR_GENERIC_UNKNOWN_ENDPOINT, ref dcurr, dend))
                         SendAndReset();
                     return true;
                 }
@@ -91,16 +88,14 @@ namespace Garnet.cluster
                 var worker = clusterProvider.clusterManager.CurrentConfig.GetWorkerFromNodeId(replicaNodeId);
                 if (worker.role != NodeRole.REPLICA)
                 {
-                    var resp = Encoding.ASCII.GetBytes($"-ERR Node @{replicaAddress}:{replicaPort} is not a replica.\r\n");
-                    while (!RespWriteUtils.WriteDirect(resp, ref dcurr, dend))
+                    while (!RespWriteUtils.WriteGenericError($"Node @{replicaAddress}:{replicaPort} is not a replica.", ref dcurr, dend))
                         SendAndReset();
                     return true;
                 }
 
                 if (worker.replicaOfNodeId != clusterProvider.clusterManager.CurrentConfig.GetLocalNodeId())
                 {
-                    var resp = Encoding.ASCII.GetBytes($"-ERR Node @{replicaAddress}:{replicaPort} is not my replica.\r\n");
-                    while (!RespWriteUtils.WriteDirect(resp, ref dcurr, dend))
+                    while (!RespWriteUtils.WriteGenericError($"Node @{replicaAddress}:{replicaPort} is not my replica.", ref dcurr, dend))
                         SendAndReset();
                     return true;
                 }
