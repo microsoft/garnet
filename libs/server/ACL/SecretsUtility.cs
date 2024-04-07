8b35fc40ed73208c89ae52fb695a901a87ad179c
Steve Syfuhs <steve@syfuhs.net>
2024-04-07 16:13:06 -0700

    Constant time secret check
    Switch all secret comparisons to be constant time to reduce the risk of creating an oracle through side channel timing.

---
+++ libs/server/ACL/SecretsUtility.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Garnet.server.ACL
+{
+    internal static class SecretsUtility
+    {
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        public static bool ConstantEquals(string a, string b) => ConstantEquals(a.AsSpan(), b.AsSpan());
+
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        public static bool ConstantEquals(ReadOnlySpan<byte> a, ReadOnlySpan<byte> b)
+        {
+            var accumulator = 0;
+
+            accumulator |= a.Length ^ b.Length;
+
+            for (var i = 0; i < Math.Min(a.Length, b.Length); i++)
+            {
+                accumulator |= a[i] ^ b[i];
+            }
+
+            return accumulator == 0;
+        }
+
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        public static bool ConstantEquals(ReadOnlySpan<char> a, ReadOnlySpan<char> b)
+        {
+            var accumulator = 0;
+
+            accumulator |= a.Length ^ b.Length;
+
+            for (var i = 0; i < Math.Min(a.Length, b.Length); i++)
+            {
+                accumulator |= a[i] ^ b[i];
+            }
+
+            return accumulator == 0;
+        }
+    }
+}
