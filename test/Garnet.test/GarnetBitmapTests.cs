9f7467342226217b20cc51bb6b024967557e52ca
PaulusParssinen <paulus.parssinen@gmail.com>
2024-04-20 21:41:32 +0300

    Simplify GetSigned in GarnetBitmapTests
    * This table is unnecessary
    
    * Also it resulted the dotnet format bug

--- test/Garnet.test/GarnetBitmapTests.cs
+++ test/Garnet.test/GarnetBitmapTests.cs
@@ -1148,19 +1148,11 @@ namespace Garnet.test
         {
             ulong value = getUnsigned(ref bitmap, offset, bits);
 
-            ulong[] si = [
-                    1L << 0, 1L << 1, 1L << 2, 1L << 3, 1L << 4, 1L << 5, 1L << 6, 1L << 7,//0
-                    1L << 8, 1L << 9, 1L << 10, 1L << 11, 1L << 12, 1L << 13, 1L << 14, 1L << 15,//1
-                    1L << 16, 1L << 17, 1L << 18, 1L << 19, 1L << 20, 1L << 21, 1L << 22, 1L << 23,//2
-                    1L << 24, 1L << 25, 1L << 26, 1L << 27, 1L << 28, 1L << 29, 1L << 30, 1L << 31,//3
-                    1L << 32, 1L << 33, 1L << 34, 1L << 35, 1L << 36, 1L << 37, 1L << 38, 1L << 39,//4
-                    1L << 40, 1L << 41, 1L << 42, 1L << 43, 1L << 44, 1L << 45, 1L << 46, 1L << 47,//5
-                    1L << 48, 1L << 49, 1L << 50, 1L << 51, 1L << 52, 1L << 53, 1L << 54, 1L << 55,//6
-                    1L << 56, 1L << 57, 1L << 58, 1L << 59, 1L << 60, 1L << 61, 1L << 62, (ulong)1 << 63,//7                 
-                ];
+            if (bits < 64 && (value & (1UL << (int)(bits - 1))) != 0)
+            {
+                value |= ulong.MaxValue << (byte)bits;
+            }
 
-            if (bits < 64 && ((value & si[bits - 1]) > 0))
-                value |= (~(ulong)0) << ((byte)bits);
             return (long)value;
         }
 
