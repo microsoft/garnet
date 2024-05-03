ef64efb75826c98d28db87e66189fd78cafeac01
PaulusParssinen <paulus.parssinen@gmail.com>
2024-05-03 05:22:13 +0300

    Add GeoHash specific benchmarks

---
+++ benchmark/BDN.benchmark/GeoHashBenchmarks.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using BenchmarkDotNet.Attributes;
+
+using Garnet.server;
+
+namespace BDN.benchmark
+{
+    [MemoryDiagnoser]
+    public class GeoHashBenchmarks
+    {
+        private const double Latitude = 47.642219912251285;
+        private const double Longitude = -122.14205560231471;
+
+        private const long GeoHashInteger = 1557413161902764;
+
+        [Benchmark]
+        public long GeoToLongValue() => GeoHash.GeoToLongValue(Latitude, Longitude);
+
+        [Benchmark]
+        public (double, double) GetCoordinatesFromLong() => GeoHash.GetCoordinatesFromLong(GeoHashInteger);
+    }
+}
