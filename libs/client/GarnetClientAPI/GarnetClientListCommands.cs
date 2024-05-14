c1fc4234e5fdebc09837d26707cc7496e195e561
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-05-14 03:41:56 +0300

    Add LPUSH & RPUSH commands

---
+++ libs/client/GarnetClientAPI/GarnetClientListCommands.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garnet.client;
+
+public sealed partial class GarnetClient
+{
+    static readonly Memory<byte> LPUSH = "$5\r\nLPUSH\r\n"u8.ToArray();
+    static readonly Memory<byte> RPUSH = "$5\r\nRPUSH\r\n"u8.ToArray();
+
+    public void ListLeftPush(string key, string element, Action<long, long, string> callback, long context = 0)
+    {
+        var args = new List<Memory<byte>>
+        {
+            Encoding.ASCII.GetBytes(key),
+            Encoding.ASCII.GetBytes(element)
+        };
+
+        ExecuteForLongResult(callback, context, LPUSH, args);
+    }
+
+    public void ListLeftPush(string key, List<string> elements, Action<long, long, string> callback, long context = 0)
+    {
+        elements.Insert(0, key);
+        
+        ExecuteForLongResult(callback, context, "LPUSH", elements);
+        
+        elements.RemoveAt(0);
+    }
+
+    public async Task<long> ListLeftPushAsync(string key, params string[] elements)
+    {
+        if (string.IsNullOrEmpty(key))
+        {
+            throw new ArgumentException($"'{nameof(key)}' cannot be null or empty.", nameof(key));
+        }
+
+        if (elements is null)
+        {
+            throw new ArgumentNullException(nameof(elements));
+        }
+
+        return await ExecuteForLongResultAsync("LPUSH", [key, .. elements]);
+    }
+
+    public void ListRightPush(string key, string element, Action<long, long, string> callback, long context = 0)
+    {
+        var args = new List<Memory<byte>>
+        {
+            Encoding.ASCII.GetBytes(key),
+            Encoding.ASCII.GetBytes(element)
+        };
+
+        ExecuteForLongResult(callback, context, RPUSH, args);
+    }
+
+    public void ListRightPush(string key, List<string> elements, Action<long, long, string> callback, long context = 0)
+    {
+        elements.Insert(0, key);
+
+        ExecuteForLongResult(callback, context, "RPUSH", elements);
+
+        elements.RemoveAt(0);
+    }
+
+    public async Task<long> ListRightPushAsync(string key, params string[] elements)
+    {
+        if (string.IsNullOrEmpty(key))
+        {
+            throw new ArgumentException($"'{nameof(key)}' cannot be null or empty.", nameof(key));
+        }
+
+        if (elements is null)
+        {
+            throw new ArgumentNullException(nameof(elements));
+        }
+
+        return await ExecuteForLongResultAsync("RPUSH", [key, .. elements]);
+    }
+}
---
+++ test/Garnet.test/RespListGarnetClientTests.cs
@@ -0,0 +1,139 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Garnet.client;
+using NUnit.Framework;
+
+namespace Garnet.test;
+
+[TestFixture]
+public class RespListGarnetClientTests
+{
+    private GarnetServer _server;
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        TestUtils.DeleteDirectory(TestUtils.MethodTestDir, wait: true);
+        _server = TestUtils.CreateGarnetServer(TestUtils.MethodTestDir, lowMemory: true);
+        _server.Start();
+    }
+
+    [Test]
+    public void AddElementsToTheListHead_WithCallback()
+    {
+        // Arrange
+        using var db = new GarnetClient(TestUtils.Address, TestUtils.Port);
+        db.Connect();
+
+        ManualResetEventSlim e = new();
+
+        // Act & Assert
+        db.ListLeftPush("list1", "foo", (_, returnValue, _) =>
+        {
+            Assert.IsTrue(1 == returnValue);
+            e.Set();
+        });
+
+        e.Wait();
+        e.Reset();
+
+        db.ListLeftPush("list1", ["bar", "baz"], (_, returnValue, _) =>
+        {
+            Assert.IsTrue(3 == returnValue);
+            e.Set();
+        });
+
+        e.Wait();
+        e.Reset();
+
+        db.ListLeftPush("list2", ["foo", "baz"], (_, returnValue, _) =>
+        {
+            Assert.IsTrue(2 == returnValue);
+            e.Set();
+        });
+
+        e.Wait();
+        e.Reset();
+        e.Dispose();
+    }
+
+    [Test]
+    [TestCase("list3", new string[] { "foo", "bar" }, 2)]
+    [TestCase("list4", new string[] { "foo", "bar", "baz" }, 3)]
+    [TestCase("list3", new string[] { "baz" }, 3)]
+    [TestCase("list5", new string[] { "foo" }, 1)]
+    public async Task AddElementsToTheListHead_ReturnsListItemsCount(string key, string[] elements, int result)
+    {
+        // Arrange
+        using var db = new GarnetClient(TestUtils.Address, TestUtils.Port);
+        db.Connect();
+
+        // Act & Assert
+        Assert.AreEqual(result, await db.ListLeftPushAsync(key, elements));
+    }
+
+    [Test]
+    public void AddElementsToListTail_WithCallback()
+    {
+        // Arrange
+        using var db = new GarnetClient(TestUtils.Address, TestUtils.Port);
+        db.Connect();
+
+        ManualResetEventSlim e = new();
+
+        // Act & Assert
+        db.ListRightPush("list6", "foo", (_, returnValue, _) =>
+        {
+            Assert.IsTrue(1 == returnValue);
+            e.Set();
+        });
+
+        e.Wait();
+        e.Reset();
+
+        db.ListRightPush("list6", ["bar", "baz"], (_, returnValue, _) =>
+        {
+            Assert.IsTrue(3 == returnValue);
+            e.Set();
+        });
+
+        e.Wait();
+        e.Reset();
+
+        db.ListRightPush("list7", ["foo", "baz"], (_, returnValue, _) =>
+        {
+            Assert.IsTrue(2 == returnValue);
+            e.Set();
+        });
+
+        e.Wait();
+        e.Reset();
+        e.Dispose();
+    }
+
+    [Test]
+    [TestCase("list8", new string[] { "foo", "bar" }, 2)]
+    [TestCase("list9", new string[] { "foo", "bar", "baz" }, 3)]
+    [TestCase("list8", new string[] { "baz" }, 3)]
+    [TestCase("list10", new string[] { "foo" }, 1)]
+    public async Task AddElementsToTheListTail_ReturnsListItemsCount(string key, string[] elements, int result)
+    {
+        // Arrange
+        using var db = new GarnetClient(TestUtils.Address, TestUtils.Port);
+        db.Connect();
+
+        // Act & Assert
+        Assert.AreEqual(result, await db.ListRightPushAsync(key, elements));
+    }
+
+    [OneTimeTearDown]
+    public void TearDown()
+    {
+        _server.Dispose();
+
+        TestUtils.DeleteDirectory(TestUtils.MethodTestDir);
+    }
+}
