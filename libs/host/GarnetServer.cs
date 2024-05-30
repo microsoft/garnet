36f25c4ec21ff5daf526637671209216e61ddf44
Badrish Chandramouli <badrishc@microsoft.com>
2024-05-29 18:33:13 -0700

    Update version to 1.0.11 (#428)

--- libs/host/GarnetServer.cs
+++ libs/host/GarnetServer.cs
@@ -42,7 +42,7 @@ namespace Garnet
         protected StoreWrapper storeWrapper;
 
         // IMPORTANT: Keep the version in sync with .azure\pipelines\azure-pipelines-external-release.yml line ~6.
-        readonly string version = "1.0.10";
+        readonly string version = "1.0.11";
 
         /// <summary>
         /// Resp protocol version
