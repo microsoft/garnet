microsoft/garnet

commit a8570acb099b971d6061312dfc928948593d109f

Improve dockerfiles (#224)
* Improve dockerfiles

* Set docker image distro as ubuntu

* Add Alpine based Dockfile

* Remove line breaks

* Add .dockerignore

* Simply .dockerignore

* Fix arch issue for nano server

* Fix nanoserver docker build issue

* Revert nanoserver dockerfile

---------

Co-authored-by: Badrish Chandramouli <badrishc@microsoft.com>
Co-authored-by: Irina Spiridonova <irinasp@microsoft.com>

Dockerfile.alpine
@@ -1,4 +1,4 @@
-FROM --platform=$BUILDPLATFORM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+FROM --platform=$BUILDPLATFORM mcr.microsoft.com/dotnet/sdk:8.0-alpine AS build
 ARG TARGETARCH
 WORKDIR /src
 
@@ -26,7 +26,7 @@ WORKDIR /src/main/GarnetServer
 RUN dotnet publish -a $TARGETARCH -c Release -o /app --no-restore --self-contained false -f net8.0
 
 # Final stage/image
-FROM mcr.microsoft.com/dotnet/runtime:8.0 AS runtime
+FROM mcr.microsoft.com/dotnet/runtime:8.0-alpine AS runtime
 WORKDIR /app
 COPY --from=build /app .
 

Dockerfile.cbl-mariner
@@ -1,26 +1,40 @@
 FROM --platform=$BUILDPLATFORM mcr.microsoft.com/dotnet/sdk:8.0-cbl-mariner2.0 AS build
 ARG TARGETARCH
+WORKDIR /src
 
-WORKDIR /source
+# Copy csproj and restore as distinct layers
+COPY libs/client/*.csproj libs/client/
+COPY libs/cluster/*.csproj libs/cluster/
+COPY libs/common/*.csproj libs/common/
+COPY libs/host/*.csproj libs/host/
+COPY libs/server/*.csproj libs/server/
+COPY libs/storage/Tsavorite/cs/src/core/*.csproj libs/storage/Tsavorite/cs/src/core/
+COPY libs/storage/Tsavorite/cs/src/devices/AzureStorageDevice/*.csproj libs/storage/Tsavorite/cs/src/devices/AzureStorageDevice/
+COPY main/GarnetServer/*.csproj main/GarnetServer/
+COPY metrics/HdrHistogram/*.csproj metrics/HdrHistogram/
 
-# Copy files
-COPY . .
-WORKDIR /source/main/GarnetServer
+RUN dotnet restore main/GarnetServer/GarnetServer.csproj -a $TARGETARCH
 
-RUN dotnet restore -a $TARGETARCH
-RUN dotnet build -a $TARGETARCH -c Release
+# Copy everthing else and publish app
+COPY Garnet.snk Garnet.snk
+COPY libs/ libs/
+COPY main/ main/
+COPY metrics/ metrics/
+COPY test/testcerts test/testcerts
 
-# Copy and publish app and libraries
-RUN dotnet publish -a $TARGETARCH -c Release -o /app --self-contained false --no-restore -f net8.0
+WORKDIR /src/main/GarnetServer
+RUN dotnet publish -a $TARGETARCH -c Release -o /app --no-restore --self-contained false -f net8.0
 
 # Final stage/image
 FROM mcr.microsoft.com/dotnet/runtime:8.0-cbl-mariner2.0 AS runtime
-
 WORKDIR /app
 COPY --from=build /app .
+
+# Run container as a non-root user
+USER $APP_UID
 
 # For inter-container communication.
 EXPOSE 6379
 
 # Run GarnetServer with an index size of 128MB
-ENTRYPOINT ["/app/GarnetServer", "-i", "128m", "--port", "6379"]
+ENTRYPOINT ["./GarnetServer", "-i", "128m", "--port", "6379"]

Dockerfile.chiseled
@@ -1,21 +1,32 @@
 FROM --platform=$BUILDPLATFORM mcr.microsoft.com/dotnet/sdk:8.0-jammy AS build
 ARG TARGETARCH
+WORKDIR /src
 
-WORKDIR /source
+# Copy csproj and restore as distinct layers
+COPY libs/client/*.csproj libs/client/
+COPY libs/cluster/*.csproj libs/cluster/
+COPY libs/common/*.csproj libs/common/
+COPY libs/host/*.csproj libs/host/
+COPY libs/server/*.csproj libs/server/
+COPY libs/storage/Tsavorite/cs/src/core/*.csproj libs/storage/Tsavorite/cs/src/core/
+COPY libs/storage/Tsavorite/cs/src/devices/AzureStorageDevice/*.csproj libs/storage/Tsavorite/cs/src/devices/AzureStorageDevice/
+COPY main/GarnetServer/*.csproj main/GarnetServer/
+COPY metrics/HdrHistogram/*.csproj metrics/HdrHistogram/
 
-# Copy files
-COPY . .
-WORKDIR /source/main/GarnetServer
+RUN dotnet restore main/GarnetServer/GarnetServer.csproj -a $TARGETARCH
 
-RUN dotnet restore -a $TARGETARCH
-RUN dotnet build -a $TARGETARCH -c Release
+# Copy everthing else and publish app
+COPY Garnet.snk Garnet.snk
+COPY libs/ libs/
+COPY main/ main/
+COPY metrics/ metrics/
+COPY test/testcerts test/testcerts
 
-# Copy and publish app and libraries
-RUN dotnet publish -a $TARGETARCH -c Release -o /app --self-contained false --no-restore -f net8.0
+WORKDIR /src/main/GarnetServer
+RUN dotnet publish -a $TARGETARCH -c Release -o /app --no-restore --self-contained false -f net8.0
 
 # Final stage/image
 FROM mcr.microsoft.com/dotnet/runtime:8.0-jammy-chiseled AS runtime
-
 WORKDIR /app
 COPY --from=build /app .
 
@@ -23,4 +34,4 @@ COPY --from=build /app .
 EXPOSE 6379
 
 # Run GarnetServer with an index size of 128MB
-ENTRYPOINT ["/app/GarnetServer", "-i", "128m", "--port", "6379"]
+ENTRYPOINT ["./GarnetServer", "-i", "128m", "--port", "6379"]

Dockerfile.ubuntu
@@ -1,25 +1,40 @@
-FROM --platform=$BUILDPLATFORM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+FROM --platform=$BUILDPLATFORM mcr.microsoft.com/dotnet/sdk:8.0-jammy AS build
 ARG TARGETARCH
+WORKDIR /src
 
-WORKDIR /source
+# Copy csproj and restore as distinct layers
+COPY libs/client/*.csproj libs/client/
+COPY libs/cluster/*.csproj libs/cluster/
+COPY libs/common/*.csproj libs/common/
+COPY libs/host/*.csproj libs/host/
+COPY libs/server/*.csproj libs/server/
+COPY libs/storage/Tsavorite/cs/src/core/*.csproj libs/storage/Tsavorite/cs/src/core/
+COPY libs/storage/Tsavorite/cs/src/devices/AzureStorageDevice/*.csproj libs/storage/Tsavorite/cs/src/devices/AzureStorageDevice/
+COPY main/GarnetServer/*.csproj main/GarnetServer/
+COPY metrics/HdrHistogram/*.csproj metrics/HdrHistogram/
 
-# Copy files
-COPY . .
-WORKDIR /source/main/GarnetServer
+RUN dotnet restore main/GarnetServer/GarnetServer.csproj -a $TARGETARCH
 
-RUN dotnet restore -a $TARGETARCH
-RUN dotnet build -a $TARGETARCH -c Release
+# Copy everthing else and publish app
+COPY Garnet.snk Garnet.snk
+COPY libs/ libs/
+COPY main/ main/
+COPY metrics/ metrics/
+COPY test/testcerts test/testcerts
 
-# Copy and publish app and libraries
-RUN dotnet publish -a $TARGETARCH -c Release -o /app --self-contained false -f net8.0
+WORKDIR /src/main/GarnetServer
+RUN dotnet publish -a $TARGETARCH -c Release -o /app --no-restore --self-contained false -f net8.0
 
 # Final stage/image
-FROM mcr.microsoft.com/dotnet/runtime:8.0 AS runtime
+FROM mcr.microsoft.com/dotnet/runtime:8.0-jammy AS runtime
 WORKDIR /app
 COPY --from=build /app .
+
+# Run container as a non-root user
+USER $APP_UID
 
 # For inter-container communication.
 EXPOSE 6379
 
 # Run GarnetServer with an index size of 128MB
-ENTRYPOINT ["/app/GarnetServer", "-i", "128m", "--port", "6379"]
+ENTRYPOINT ["./GarnetServer", "-i", "128m", "--port", "6379"]

.dockerignore
@@ -1,0 +1,13 @@
+# Directories
+**/bin
+**/obj
+.git
+.vs
+.vscode
+
+# Files
+.gitignore
+**/*.md
+docker-compose.yml
+docker-compose.*.yml
+Dockerfile*

commit 53e4930987b11e0c035a8b0266cd4839297e50f9

Fix Dockerfile entrypoints
Fixes a regression introduced in #224;

entrypoints containing relative paths are incompatible with the usage of
custom working directories.

This causes unexpected failures particularly in CI (GitHub, Gitea,
Forgejo actions), where the repository is often mounted and set as the
working directory - notably, when garnet is used as a `service`.

Dockerfile
@@ -39,4 +39,4 @@ USER $APP_UID
 EXPOSE 6379
 
 # Run GarnetServer with an index size of 128MB
-ENTRYPOINT ["./GarnetServer", "-i", "128m", "--port", "6379"]
+ENTRYPOINT ["/app/GarnetServer", "-i", "128m", "--port", "6379"]

Dockerfile.alpine
@@ -39,4 +39,4 @@ USER $APP_UID
 EXPOSE 6379
 
 # Run GarnetServer with an index size of 128MB
-ENTRYPOINT ["./GarnetServer", "-i", "128m", "--port", "6379"]
+ENTRYPOINT ["/app/GarnetServer", "-i", "128m", "--port", "6379"]

Dockerfile.cbl-mariner
@@ -39,4 +39,4 @@ USER $APP_UID
 EXPOSE 6379
 
 # Run GarnetServer with an index size of 128MB
-ENTRYPOINT ["./GarnetServer", "-i", "128m", "--port", "6379"]
+ENTRYPOINT ["/app/GarnetServer", "-i", "128m", "--port", "6379"]

Dockerfile.chiseled
@@ -36,4 +36,4 @@ COPY --from=build /app .
 EXPOSE 6379
 
 # Run GarnetServer with an index size of 128MB
-ENTRYPOINT ["./GarnetServer", "-i", "128m", "--port", "6379"]
+ENTRYPOINT ["/app/GarnetServer", "-i", "128m", "--port", "6379"]

Dockerfile.ubuntu
@@ -39,4 +39,4 @@ USER $APP_UID
 EXPOSE 6379
 
 # Run GarnetServer with an index size of 128MB
-ENTRYPOINT ["./GarnetServer", "-i", "128m", "--port", "6379"]
+ENTRYPOINT ["/app/GarnetServer", "-i", "128m", "--port", "6379"]