95cfd07bb264f6259a93cc4a86ebbab26f34c489
Kevin Montrose <kmontrose@microsoft.com>
2024-06-18 11:32:51 -0400

    correctly indicate failure

--- libs/cluster/Server/Replication/ReplicaOps/ReplicaReceiveCheckpoint.cs
+++ libs/cluster/Server/Replication/ReplicaOps/ReplicaReceiveCheckpoint.cs
@@ -58,7 +58,7 @@ namespace Garnet.cluster
             {
                 replicateLock.WriteUnlock();
             }
-            return true;
+            return false;
         }
 
         /// <summary>
