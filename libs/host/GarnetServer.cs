a297dcce6ddb11fb8ed7606d9a99065ab741db58
Badrish Chandramouli <badrishc@microsoft.com>
2024-06-27 16:21:00 -0700

    Update version to 1.0.15 (#484)
    Co-authored-by: Vasileios Zois <96085550+vazois@users.noreply.github.com>

--- libs/host/GarnetServer.cs
+++ libs/host/GarnetServer.cs
@@ -43,7 +43,7 @@ namespace Garnet
         protected StoreWrapper storeWrapper;
 
         // IMPORTANT: Keep the version in sync with .azure\pipelines\azure-pipelines-external-release.yml line ~6.
-        readonly string version = "1.0.14";
+        readonly string version = "1.0.15";
 
         /// <summary>
         /// Resp protocol version
