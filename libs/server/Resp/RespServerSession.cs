c2afb6a1815224c600d7681324a311243db81145
Hamdaan Khalid <hkhalid@microsoft.com>
2024-08-01 15:16:38 -0700

    move back to conservative strategy

--- libs/server/Resp/RespServerSession.cs
+++ libs/server/Resp/RespServerSession.cs
@@ -320,6 +320,9 @@ namespace Garnet.server
             }
             finally
             {
+                // reset the session's flag for AOF blocking to default value after processing all commands
+                waitForAofBlocking = false;
+
                 networkSender.ExitAndReturnResponseObject();
                 clusterSession?.ReleaseCurrentEpoch();
             }
@@ -912,7 +915,7 @@ namespace Garnet.server
                 src = src.Slice(destSpace);
 
                 // Send and reset output buffer
-                Send(networkSender.GetResponseObjectHead(), true);
+                Send(networkSender.GetResponseObjectHead());
                 networkSender.GetResponseObject();
                 dcurr = networkSender.GetResponseObjectHead();
                 dend = networkSender.GetResponseObjectTail();
@@ -920,7 +923,7 @@ namespace Garnet.server
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private void Send(byte* d, bool isChunked = false)
+        private void Send(byte* d)
         {
             // Note: This SEND method may be called for responding to multiple commands in a single message (pipelining),
             // or multiple times in a single command for sending data larger than fitting in buffer at once.
@@ -937,8 +940,6 @@ namespace Garnet.server
                 {
                     var task = storeWrapper.appendOnlyFile.WaitForCommitAsync();
                     if (!task.IsCompleted) task.AsTask().GetAwaiter().GetResult();
-                    // once the set of commands so for has been processed we can safely reset the session flag to not need AOF blocking
-                    if(!isChunked) waitForAofBlocking = false;
                 }
                 int sendBytes = (int)(dcurr - d);
                 networkSender.SendResponse((int)(d - networkSender.GetResponseObjectHead()), sendBytes);
