7e3b6ff1038a574e94e6e5eec766c7104ca6da43
Vasileios Zois <vazois@microsoft.com>
2024-08-05 20:08:55 -0700

    playground migration proj

---
+++ playground/MigrateBench/MigrateRequest.cs
@@ -0,0 +1,172 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+using System.Diagnostics;
+using Garnet.client;
+using Microsoft.Extensions.Logging;
+
+namespace MigrateBench
+{
+    public enum MigrateRequestType
+    {
+        KEYS,
+        SLOTS,
+        SLOTSRANGE
+    }
+
+    public class MigrateRequest
+    {
+        readonly Options opts;
+
+        readonly string sourceAddress;
+        readonly int sourcePort;
+        readonly string targetAddress;
+        readonly int targetPort;
+        readonly int timeout = 100000;
+        readonly ILogger logger;
+
+        GarnetClientSession sourceNode;
+        GarnetClientSession targetNode;
+
+        public MigrateRequest(Options opts, ILogger logger = null)
+        {
+            this.opts = opts;
+            var sourceEndpoint = opts.SourceEndpoint.Split(':');
+            var targetEndpoint = opts.TargetEndpoint.Split(':');
+            sourceAddress = sourceEndpoint[0];
+            sourcePort = int.Parse(sourceEndpoint[1]);
+            targetAddress = targetEndpoint[0];
+            targetPort = int.Parse(targetEndpoint[1]);
+
+            sourceNode = new(sourceAddress, sourcePort, bufferSize: 1 << 22);
+            targetNode = new(targetAddress, targetPort, bufferSize: 1 << 22);
+            this.logger = logger;
+        }
+
+        public void Run()
+        {
+            try
+            {
+                sourceNode.Connect();
+                targetNode.Connect();
+                var sourceNodeKeys = dbsize(ref sourceNode);
+                var targetNodeKeys = dbsize(ref targetNode);
+                logger?.LogInformation("SourceNode: {endpoint} KeyCount: {keys}", opts.SourceEndpoint, sourceNodeKeys);
+                logger?.LogInformation("TargetNode: {endpoint} KeyCount: {keys}", opts.TargetEndpoint, targetNodeKeys);
+
+                switch (opts.migrateRequestType)
+                {
+                    case MigrateRequestType.KEYS:
+                        MigrateKeys();
+                        break;
+                    case MigrateRequestType.SLOTS:
+                        break;
+                    case MigrateRequestType.SLOTSRANGE:
+                        MigrateSlotRanges();
+                        break;
+                    default:
+                        throw new Exception("Error invalid migrate request type");
+                }
+
+                var sourceNodeKeys2 = dbsize(ref sourceNode);
+                var targetNodeKeys2 = dbsize(ref targetNode);
+                var keysTransferred = targetNodeKeys2 - targetNodeKeys;
+                logger?.LogInformation("SourceNode: {endpoint} KeyCount after migration: {keys}", opts.SourceEndpoint, sourceNodeKeys2);
+                logger?.LogInformation("TargetNode: {endpoint} KeyCount after migration: {keys}", opts.TargetEndpoint, targetNodeKeys2);
+                logger?.LogInformation("KeysTransferred: {keys}", keysTransferred);
+
+                int dbsize(ref GarnetClientSession c)
+                {
+                    var resp = c.ExecuteAsync("dbsize").GetAwaiter().GetResult();
+                    return int.Parse(resp);
+                }
+            }
+            finally
+            {
+                sourceNode.Dispose();
+                targetNode.Dispose();
+            }
+        }
+
+        private void MigrateSlotRanges()
+        {
+            try
+            {
+                var slots = opts.Slots.ToArray();
+                if ((slots.Length & 0x1) > 0)
+                {
+                    logger?.LogError("Malformed SLOTRANGES input; please provide pair of ranges");
+                    return;
+                }
+
+                // migrate 192.168.1.20 7001 "" 0 5000 SLOTSRANGE 1000 7000            
+                ICollection<string> request = ["MIGRATE", targetAddress, targetPort.ToString(), "", "0", timeout.ToString(), "SLOTSRANGE"];
+                foreach (var slot in slots)
+                    request.Add(slot.ToString());
+
+                var startTimestamp = Stopwatch.GetTimestamp();
+                logger?.LogInformation("Issuing MIGRATE SLOTSRANGE");
+                var resp = sourceNode.ExecuteAsync(request.ToArray()).GetAwaiter().GetResult();
+
+                logger?.LogInformation("Waiting for Migration to Complete");
+                resp = sourceNode.ExecuteAsync("CLUSTER", "MTASKS").GetAwaiter().GetResult();
+                while (int.Parse(resp) > 0)
+                {
+                    Thread.Yield();
+                    resp = sourceNode.ExecuteAsync("CLUSTER", "MTASKS").GetAwaiter().GetResult();
+                }
+
+                var elapsed = Stopwatch.GetTimestamp() - startTimestamp;
+                var t = TimeSpan.FromTicks(elapsed);
+                logger?.LogInformation("SlotsRange Elapsed Time: {elapsed} seconds", t.TotalSeconds);
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex, "Error at MigrateSlotsRange");
+            }
+        }
+
+        private void MigrateKeys()
+        {
+            try
+            {
+                var slots = opts.Slots.ToArray();
+                var sourceNodeId = sourceNode.ExecuteAsync("cluster", "myid").GetAwaiter().GetResult();
+                var targetNodeId = targetNode.ExecuteAsync("cluster", "myid").GetAwaiter().GetResult();
+
+                string[] migrating = ["cluster", "setslot", "<slot>", "migrating", targetNodeId];
+                string[] importing = ["cluster", "setslot", "<slot>", "importing", sourceNodeId];
+                string[] node = ["cluster", "setslot", "<slot>", "node", targetNodeId];
+                string[] countkeysinslot = ["cluster", "countkeysinslot", "<slot>"];
+                string[] getkeysinslot = ["cluster", "getkeysinslot", "<slot>"];
+                foreach (var slot in slots)
+                {
+                    var slotStr = slot.ToString();
+                    migrating[2] = slotStr;
+                    importing[2] = slotStr;
+                    node[2] = slotStr;
+                    countkeysinslot[2] = slotStr;
+                    getkeysinslot[2] = slotStr;
+
+                    var resp = sourceNode.ExecuteAsync(migrating).GetAwaiter().GetResult();
+                    resp = targetNode.ExecuteAsync(importing).GetAwaiter().GetResult();
+                    resp = sourceNode.ExecuteAsync(countkeysinslot).GetAwaiter().GetResult();
+                    var keys = sourceNode.ExecuteForArrayAsync(getkeysinslot).GetAwaiter().GetResult();
+
+                    ICollection<string> migrate = ["MIGRATE", targetAddress, targetPort.ToString(), "", "0", timeout.ToString(), "KEYS"];
+                    foreach (var key in keys)
+                        migrate.Add(key);
+
+                    logger?.LogInformation("KeyCount: {keys}", keys.Length);
+                    resp = sourceNode.ExecuteAsync(migrate.ToArray()).GetAwaiter().GetResult();
+
+                    resp = targetNode.ExecuteAsync(node).GetAwaiter().GetResult();
+                    resp = sourceNode.ExecuteAsync(node).GetAwaiter().GetResult();
+                }
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex, "Error at MigrateKeys");
+            }
+        }
+    }
+}
---
+++ playground/MigrateBench/Options.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using CommandLine;
+using Microsoft.Extensions.Logging;
+
+namespace MigrateBench
+{
+    public partial class Options
+    {
+        [Option('s', "source-endpoint", Required = false, Default = "127.0.0.1:7000", HelpText = "SourceEndpoint")]
+        public string SourceEndpoint { get; set; }
+
+        [Option('t', "target-endpoint", Required = false, Default = "127.0.0.1:7001", HelpText = "TargetEndpoint")]
+        public string TargetEndpoint { get; set; }
+
+        [Option("slots", Separator = ',', Default = null, HelpText = "Slots to migrate")]
+        public IEnumerable<int> Slots { get; set; }
+
+        [Option("mtype", Required = false, Default = MigrateRequestType.SLOTSRANGE, HelpText = "Operation type (GET, MGET, INCR, PING, ZADDREM, PFADD, ZADDCARD)")]
+        public MigrateRequestType migrateRequestType { get; set; }
+
+        [Option("logger-level", Required = false, Default = LogLevel.Information, HelpText = "Logging level")]
+        public LogLevel LogLevel { get; set; }
+    }
+}
