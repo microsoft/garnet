6854919aac5bd4e525d4d23968b07b5aa3ffc6d2
Vasileios Zois <vazois@microsoft.com>
2024-08-12 12:18:23 -0700

    remove unused file ref and simplify connection stats for replica

--- libs/cluster/Server/GarnetServerNode.cs
+++ libs/cluster/Server/GarnetServerNode.cs
@@ -68,6 +68,8 @@ namespace Garnet.cluster
                 logger: logger);
             this.initialized = 0;
             this.logger = logger;
+            this.gossip_recv = 0;
+            this.gossip_send = 0;
             ResetCts();
         }
 
@@ -229,9 +231,7 @@ namespace Garnet.cluster
         public ConnectionInfo GetConnectionInfo()
         {
             var nowTicks = DateTimeOffset.UtcNow.Ticks;
-            var last_io_seconds = GossipRecv == -1 ? -1 : nowTicks - GossipSend;
-            last_io_seconds = last_io_seconds < 0 ? 0 : (int)TimeSpan.FromTicks(last_io_seconds).TotalSeconds;
-            var connection_status = IsConnected ? "up" : "down";
+            var last_io_seconds = GossipRecv == 0 ? 0 : (int)TimeSpan.FromTicks(nowTicks - GossipSend).TotalSeconds;
 
             return new ConnectionInfo()
             {
