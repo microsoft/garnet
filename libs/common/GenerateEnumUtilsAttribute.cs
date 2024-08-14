c64790d8ea57d0f4544c9783f68e51d4b958ab99
Meir Blachman <meirblachman@gmail.com>
2024-08-14 09:17:14 +0300

    feature: introduce source generator for enum code generation

--- libs/common/GenerateEnumUtilsAttribute.cs
+++ libs/common/GenerateEnumUtilsAttribute.cs
@@ -5,6 +5,9 @@ using System;
 
 namespace Garnet.common
 {
+    /// <summary>
+    /// Specifies that utility methods for generating enum descriptions should be generated for the target enum.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Enum)]
     public sealed class GenerateEnumDescriptionUtilsAttribute : Attribute
     {
