e1d21e8f3d77a9db35d5b7a788be8a75c79d6643
Vasileios Zois <vazois@microsoft.com>
2024-08-16 19:45:39 -0700

    remove unused code

--- libs/cluster/Server/Migration/MigrationKeyIterationFunctions.cs
+++ libs/cluster/Server/Migration/MigrationKeyIterationFunctions.cs
@@ -29,8 +29,6 @@ namespace Garnet.cluster
                 {
                     cursorRecordResult = CursorRecordResult.Accept; // default; not used here
 
-                    var deleted = recordMetadata.RecordInfo.Tombstone;
-
                     // Do not send key if it is expired
                     if (ClusterSession.Expired(ref value))
                         return true;
