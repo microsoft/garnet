a421222678eb6f0d8b8d1fd3d97f9099f735a7bb
Vasileios Zois <vazois@microsoft.com>
2024-09-06 12:39:23 -0700

    add ACL test for MIGRATEGC

--- test/Garnet.test/Resp/ACL/RespCommandTests.cs
+++ test/Garnet.test/Resp/ACL/RespCommandTests.cs
@@ -3854,7 +3854,36 @@ namespace Garnet.test.Resp.ACL
                 try
                 {
                     await client.ExecuteForStringResultAsync("MIGRATE", ["127.0.0.1", "9999", "KEY", "0", "1000"]);
-                    Assert.Fail("Shouldn't succeed, no replicas are attached");
+                    Assert.Fail("Shouldn't be reachable, cluster isn't enabled");
+                }
+                catch (Exception e)
+                {
+                    if (e.Message == "ERR This instance has cluster support disabled")
+                    {
+                        return;
+                    }
+
+                    throw;
+                }
+            }
+        }
+
+        [Test]
+        public async Task MigrateGCACLsAsync()
+        {
+            // Uses exceptions for control flow, as we're not setting up replicas here
+
+            await CheckCommandsAsync(
+                "MIGRATEGC",
+                [DoMigrateGCAsync]
+            );
+
+            static async Task DoMigrateGCAsync(GarnetClient client)
+            {
+                try
+                {
+                    string val = await client.ExecuteForStringResultAsync("MIGRATEGC");
+                    Assert.Fail("Shouldn't be reachable, cluster isn't enabled");
                 }
                 catch (Exception e)
                 {
