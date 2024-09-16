e7e46c37ceabb9b673078a639750150a3d430a18
Vijay-Nirmal <pon.vijaynirmal@outlook.com>
2024-09-16 18:09:48 +0530

    Code style fix

--- libs/server/ExpireOption.cs
+++ libs/server/ExpireOption.cs
@@ -13,29 +13,27 @@ namespace Garnet.server
         /// <summary>
         /// None
         /// </summary>
-        None  = 0,
+        None = 0,
         /// <summary>
         /// Set expiry only when the key has no expiry
         /// </summary>
-        NX    = 1 << 0,
+        NX = 1 << 0,
         /// <summary>
         /// Set expiry only when the key has an existing expiry 
         /// </summary>
-        XX    = 1 << 1,
+        XX = 1 << 1,
         /// <summary>
         /// Set expiry only when the new expiry is greater than current one
         /// </summary>
-        GT   = 1 << 2,
+        GT = 1 << 2,
         /// <summary>
         /// Set expiry only when the new expiry is less than current one
         /// </summary>
-        LT   = 1 << 3,
-
+        LT = 1 << 3,
         /// <summary>
         /// Set expiry only when the key has an existing expiry and the new expiry is greater than current one
         /// </summary>
         XXGT = XX | GT,
-
         /// <summary>
         /// Set expiry only when the key has an existing expiry and the new expiry is less than current one
         /// </summary>
