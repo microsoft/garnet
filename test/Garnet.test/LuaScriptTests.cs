eeb225c2aabbd37388645132d0f654b651206446
Badrish Chandramouli <badrishc@microsoft.com>
2024-09-17 19:01:44 -0700

    improve test

--- test/Garnet.test/LuaScriptTests.cs
+++ test/Garnet.test/LuaScriptTests.cs
@@ -364,13 +364,13 @@ return redis.status_reply(table.concat(setArgs))
             var db = redis.GetDatabase(0);
             var response = db.ScriptEvaluate(script, ["key1", "key2"], ["value", 1, 12345]);
             ClassicAssert.AreEqual("key1key2", (string)response);
-            response = db.ScriptEvaluate(script, ["key1", "key2"], ["value", 1, 12345]);
+            response = db.ScriptEvaluate(script, ["key1", "key2"], ["value"]);
             ClassicAssert.AreEqual("key1key2", (string)response);
             response = db.ScriptEvaluate(script, ["key1"], ["value", 1, 12345]);
             ClassicAssert.AreEqual("key1", (string)response);
-            response = db.ScriptEvaluate(script, ["key1", "key2"], ["value", 1, 12345]);
+            response = db.ScriptEvaluate(script, ["key1", "key2"]);
             ClassicAssert.AreEqual("key1key2", (string)response);
-            response = db.ScriptEvaluate(script, ["key1", "key2", "key3", "key4"], ["value", 1, 12345]);
+            response = db.ScriptEvaluate(script, ["key1", "key2", "key3", "key4"], ["value", 1]);
             ClassicAssert.AreEqual("key1key2key3key4", (string)response);
             response = db.ScriptEvaluate(script, [], ["value", 1, 12345]);
             ClassicAssert.AreEqual("", (string)response);
