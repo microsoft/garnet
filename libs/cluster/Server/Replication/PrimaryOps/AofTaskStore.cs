62425ed31dfe5ce2730ef7ac3bde91357d3ec74c
Vasileios Zois <vazois@microsoft.com>
2024-09-19 11:21:15 -0700

    rename NetworkBuffers to NetworkBufferSettings

--- libs/cluster/Server/Replication/PrimaryOps/AofTaskStore.cs
+++ libs/cluster/Server/Replication/PrimaryOps/AofTaskStore.cs
@@ -130,7 +130,14 @@ namespace Garnet.cluster
                     this,
                     current.LocalNodeId,
                     remoteNodeId,
-                    new GarnetClientSession(address, port, clusterProvider.replicationManager.GetNetworkBufferSettings, tlsOptions: clusterProvider.serverOptions.TlsOptions?.TlsClientOptions, authUsername: clusterProvider.ClusterUsername, authPassword: clusterProvider.ClusterPassword, logger: logger),
+                    new GarnetClientSession(
+                        address,
+                        port,
+                        clusterProvider.replicationManager.GetNetworkBufferSettings,
+                        tlsOptions: clusterProvider.serverOptions.TlsOptions?.TlsClientOptions,
+                        authUsername: clusterProvider.ClusterUsername,
+                        authPassword: clusterProvider.ClusterPassword,
+                        logger: logger),
                     startAddress,
                     logger);
             }
