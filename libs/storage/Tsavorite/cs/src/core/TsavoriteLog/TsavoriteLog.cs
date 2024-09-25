c34ee3a662a384f78975f931028df9cd969406dc
Badrish Chandramouli <badrishc@microsoft.com>
2024-09-25 15:27:02 -0700

    improve commit work queue to avoid task creation

--- libs/storage/Tsavorite/cs/src/core/TsavoriteLog/TsavoriteLog.cs
+++ libs/storage/Tsavorite/cs/src/core/TsavoriteLog/TsavoriteLog.cs
@@ -2145,7 +2145,7 @@ namespace Tsavorite.core
         {
             // Using count is safe as a fast filtering mechanism to reduce number of invocations despite concurrency
             if (ongoingCommitRequests.Count == 0 && commitInfo.ErrorCode == 0) return;
-            commitQueue.EnqueueAndTryWork(commitInfo, asTask: true);
+            commitQueue.Enqueue(commitInfo);
         }
 
         private unsafe bool TryEnqueueCommitRecord(ref TsavoriteLogRecoveryInfo info)
