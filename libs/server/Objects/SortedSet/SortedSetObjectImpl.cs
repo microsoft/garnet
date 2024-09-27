273a88a80b94261717e7ebd5bcecfb8563c670a2
Badrish Chandramouli <badrishc@microsoft.com>
2024-09-27 15:34:35 -0700

    fix

--- libs/server/Objects/SortedSet/SortedSetObjectImpl.cs
+++ libs/server/Objects/SortedSet/SortedSetObjectImpl.cs
@@ -156,6 +156,7 @@ namespace Garnet.server
                         }
                     }
 
+                    parsedOptions = true;
                     currTokenIdx++;
 
                     // Member
