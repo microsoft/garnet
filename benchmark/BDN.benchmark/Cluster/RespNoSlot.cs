8ff8dd0b15547eef82b3b6ba651e3c38caa51c56
Vasileios Zois <vazois@microsoft.com>
2024-10-14 10:30:34 -0700

    add custom proc cluster bdn

---
+++ benchmark/BDN.benchmark/Cluster/RespNoSlot.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using BenchmarkDotNet.Attributes;
+
+namespace BDN.benchmark.Cluster
+{
+    [MemoryDiagnoser]
+    public unsafe class RespClusterBenchNoSlot : RespClusterBench
+    {
+        public new void GlobalSetup()
+        {
+            enableCluster = false;
+            base.GlobalSetup();
+        }
+
+        public new void GlobalCleanup()
+        {
+            base.GlobalCleanup();
+        }
+    }
+}
