d6b3a99c482533a41820b4282eff4ece817737d2
darrenge <darrenge@microsoft.com>
2024-10-14 15:20:35 -0700

    Removed the perf issue

--- libs/server/Resp/RespServerSession.cs
+++ libs/server/Resp/RespServerSession.cs
@@ -7,7 +7,6 @@ using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading;
 using Garnet.common;
 using Garnet.common.Parsing;
 using Garnet.networking;
