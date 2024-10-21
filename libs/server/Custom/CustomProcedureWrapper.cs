020db536260e72183878cad04f68787421491965
Yoganand Rajasekaran <yrajas@microsoft.com>
2024-10-20 19:50:44 -0700

    Support cmd name matching

--- libs/server/Custom/CustomProcedureWrapper.cs
+++ libs/server/Custom/CustomProcedureWrapper.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license.
 
 using System;
+using System.Diagnostics;
 using Garnet.common;
 
 namespace Garnet.server
@@ -11,6 +12,52 @@ namespace Garnet.server
     /// </summary>
     public abstract class CustomProcedure : CustomFunctions
     {
+        internal ScratchBufferManager scratchBufferManager;
+
+        //unsafe object CustomOperation<TGarnetApi>(TGarnetApi api, string cmd, params object[] args)
+        //    where TGarnetApi : IGarnetApi
+        //unsafe object CustomOperation<TGarnetApi>(TGarnetApi api, string cmd, params object[] args)
+        //    where TGarnetApi : IGarnetApi
+        //{
+        //switch (cmd)
+        //{
+        //    // We special-case a few performance-sensitive operations to directly invoke via the storage API
+        //    case "SET" when args.Length == 2:
+        //    case "set" when args.Length == 2:
+        //        {
+        //            if (!respServerSession.CheckACLPermissions(RespCommand.SET))
+        //                return Encoding.ASCII.GetString(CmdStrings.RESP_ERR_NOAUTH);
+        //            var key = scratchBufferManager.CreateArgSlice(Convert.ToString(args[0]));
+        //            var value = scratchBufferManager.CreateArgSlice(Convert.ToString(args[1]));
+        //            _ = api.SET(key, value);
+        //            return "OK";
+        //        }
+        //    case "GET":
+        //    case "get":
+        //        {
+        //            if (!respServerSession.CheckACLPermissions(RespCommand.GET))
+        //                throw new Exception(Encoding.ASCII.GetString(CmdStrings.RESP_ERR_NOAUTH));
+        //            var key = scratchBufferManager.CreateArgSlice(Convert.ToString(args[0]));
+        //            var status = api.GET(key, out var value);
+        //            if (status == GarnetStatus.OK)
+        //                return value.ToString();
+        //            return null;
+        //        }
+        //    // As fallback, we use RespServerSession with a RESP-formatted input. This could be optimized
+        //    // in future to provide parse state directly.
+        //    default:
+        //        {
+        //var request = scratchBufferManager.FormatCommandAsResp(cmd, args, null);
+        //_ = respServerSession.TryConsumeMessages(request.ptr, request.length);
+        //var response = scratchBufferNetworkSender.GetResponse();
+        //var result = ProcessResponse(response.ptr, response.length);
+        //scratchBufferNetworkSender.Reset();
+        //return result;
+        //        }
+        //}
+        //}
+
+
         /// <summary>
         /// Custom command implementation
         /// </summary>
@@ -28,18 +75,21 @@ namespace Garnet.server
         public readonly byte Id;
         public readonly CustomProcedure CustomProcedureImpl;
 
-        internal CustomProcedureWrapper(string name, byte id, CustomProcedure customScriptProc)
+        internal CustomProcedureWrapper(string name, byte id, CustomProcedure customProcedure, CustomCommandManager customCommandManager)
         {
             if (string.IsNullOrEmpty(name))
                 throw new ArgumentNullException(nameof(name));
 
-            if (customScriptProc == null)
-                throw new ArgumentNullException(nameof(customScriptProc));
+            if (customProcedure == null)
+                throw new ArgumentNullException(nameof(customProcedure));
+
+            Debug.Assert(customCommandManager != null);
 
             NameStr = name.ToUpperInvariant();
             Name = System.Text.Encoding.ASCII.GetBytes(NameStr);
             Id = id;
-            CustomProcedureImpl = customScriptProc;
+            CustomProcedureImpl = customProcedure;
+            CustomProcedureImpl.customCommandManager = customCommandManager;
         }
     }
 }
---
+++ libs/server/Storage/Session/CustomOps.cs
@@ -0,0 +1,121 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+//using System;
+//using System.Diagnostics;
+//using Garnet.common;
+//using Tsavorite.core;
+
+//namespace Garnet.server
+//{
+//    using ObjectStoreAllocator = GenericAllocator<byte[], IGarnetObject, StoreFunctions<byte[], IGarnetObject, ByteArrayKeyComparer, DefaultRecordDisposer<byte[], IGarnetObject>>>;
+//    using ObjectStoreFunctions = StoreFunctions<byte[], IGarnetObject, ByteArrayKeyComparer, DefaultRecordDisposer<byte[], IGarnetObject>>;
+
+//    sealed partial class StorageSession : IDisposable
+//    {
+//        private bool TryCustomObjectCommand<TGarnetApi>(byte* ptr, byte* end, RespCommand cmd, byte subid, CommandType type, ref TGarnetApi storageApi)
+//            where TGarnetApi : IGarnetAdvancedApi
+//        {
+//            var keyBytes = parseState.GetArgSliceByRef(0).SpanByte.ToByteArray();
+
+//            // Prepare input
+//            var input = new ObjectInput
+//            {
+//                header = new RespInputHeader
+//                {
+//                    cmd = cmd,
+//                    SubId = subid
+//                },
+//                parseState = parseState,
+//                parseStateStartIdx = 1
+//            };
+
+//            var output = new GarnetObjectStoreOutput { spanByteAndMemory = new SpanByteAndMemory(null) };
+
+//            GarnetStatus status;
+
+//            if (type == CommandType.ReadModifyWrite)
+//            {
+//                status = storageApi.RMW_ObjectStore(ref keyBytes, ref input, ref output);
+//                Debug.Assert(!output.spanByteAndMemory.IsSpanByte);
+
+//                switch (status)
+//                {
+//                    case GarnetStatus.WRONGTYPE:
+//                        while (!RespWriteUtils.WriteError(CmdStrings.RESP_ERR_WRONG_TYPE, ref dcurr, dend))
+//                            SendAndReset();
+//                        break;
+//                    default:
+//                        if (output.spanByteAndMemory.Memory != null)
+//                            SendAndReset(output.spanByteAndMemory.Memory, output.spanByteAndMemory.Length);
+//                        else
+//                            while (!RespWriteUtils.WriteDirect(CmdStrings.RESP_OK, ref dcurr, dend))
+//                                SendAndReset();
+//                        break;
+//                }
+//            }
+//            else
+//            {
+//                status = storageApi.Read_ObjectStore(ref keyBytes, ref input, ref output);
+//                Debug.Assert(!output.spanByteAndMemory.IsSpanByte);
+
+//                switch (status)
+//                {
+//                    case GarnetStatus.OK:
+//                        if (output.spanByteAndMemory.Memory != null)
+//                            SendAndReset(output.spanByteAndMemory.Memory, output.spanByteAndMemory.Length);
+//                        else
+//                            while (!RespWriteUtils.WriteDirect(CmdStrings.RESP_OK, ref dcurr, dend))
+//                                SendAndReset();
+//                        break;
+//                    case GarnetStatus.NOTFOUND:
+//                        Debug.Assert(output.spanByteAndMemory.Memory == null);
+//                        while (!RespWriteUtils.WriteDirect(CmdStrings.RESP_ERRNOTFOUND, ref dcurr, dend))
+//                            SendAndReset();
+//                        break;
+//                    case GarnetStatus.WRONGTYPE:
+//                        while (!RespWriteUtils.WriteError(CmdStrings.RESP_ERR_WRONG_TYPE, ref dcurr, dend))
+//                            SendAndReset();
+//                        break;
+//                }
+//            }
+
+//            return true;
+//        }
+
+
+//        public unsafe GarnetStatus CustomObjectCommand<TObjectContext>(ArgSlice key, ArgSlice[] elements, ListOperation lop, out int itemsDoneCount, ref TObjectContext objectStoreContext)
+//  where TObjectContext : ITsavoriteContext<byte[], IGarnetObject, ObjectInput, GarnetObjectStoreOutput, long, ObjectSessionFunctions, ObjectStoreFunctions, ObjectStoreAllocator>
+//        {
+//            itemsDoneCount = 0;
+
+//            if (key.Length == 0 || elements.Length == 0)
+//                return GarnetStatus.OK;
+
+//            // Prepare the parse state
+//            var parseState = new SessionParseState();
+//            ArgSlice[] parseStateBuffer = default;
+//            parseState.InitializeWithArguments(ref parseStateBuffer, elements);
+
+//            // Prepare the input
+//            var input = new ObjectInput
+//            {
+//                header = new RespInputHeader
+//                {
+//                    type = GarnetObjectType.List,
+//                    ListOp = lop,
+//                },
+//                parseState = parseState,
+//                parseStateStartIdx = 0,
+//            };
+
+//            var arrKey = key.ToArray();
+//            var status = RMWObjectStoreOperation(arrKey, ref input, out var output, ref objectStoreContext);
+
+//            itemsDoneCount = output.result1;
+//            itemBroker.HandleCollectionUpdate(arrKey);
+//            return status;
+//        }
+
+//    }
+//}
