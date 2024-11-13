33d42b60133305a59d29662c70bc8b2bd7d41fc5
darrenge <darrenge@microsoft.com>
2024-11-13 13:57:20 -0800

    Moved version.json to root (adjusted extract_version.ps1).
    Have debug codestill in too.

--- .azure/pipelines/extract_version.ps1
+++ .azure/pipelines/extract_version.ps1
@@ -1,4 +1,10 @@
-# scripts/extract_version.ps1
-$json = Get-Content -Path "version.json" | ConvertFrom-Json
+<#$f
+.DESCRIPTION
+
+    This script pulls the version number from "version.json" which is located in the root directory.
+    It then assigns the value to PACKAGE_VERSION variable which can be used in pipeline yml files
+#>
+$rootPath = Resolve-Path -Path "$PSScriptRoot/../../version.json"
+$json = Get-Content -Path $rootPath -Raw | ConvertFrom-Json
 $version = $json.version
 Write-Host "##vso[task.setvariable variable=PACKAGE_VERSION]$version"
