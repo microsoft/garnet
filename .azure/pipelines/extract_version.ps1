c8d640060ab220bf4287aed9b6497255b62e27c8
darrenge <darrenge@microsoft.com>
2024-11-13 16:36:52 -0800

    Cleaned up debug messages and have all version implemented. Still have Push to Nuget and Push to GH Release disabled. Also Garnet Library pack uses .nuspec instead of dotnet pack

--- .azure/pipelines/extract_version.ps1
+++ .azure/pipelines/extract_version.ps1
@@ -5,7 +5,6 @@
     It then assigns the value to PACKAGE_VERSION variable which can be used in pipeline yml files
 #>
 
-# scripts/extract_version_prefix.ps1
 $propsFile = Resolve-Path -Path "$PSScriptRoot/../../Directory.Build.props"
 [xml]$xml = Get-Content -Path $propsFile
 $versionPrefix = $xml.Project.PropertyGroup.VersionPrefix
