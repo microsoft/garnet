ebe03f9b9f91c287c44398c1ee19c1bee03ac0d2
darrenge <darrenge@microsoft.com>
2024-11-15 12:38:09 -0800

    Added SymbolPackageFormat=snupkg  for the nuget package symbols

--- .azure/pipelines/extract_version.ps1
+++ .azure/pipelines/extract_version.ps1
@@ -2,7 +2,7 @@
 .DESCRIPTION
 
     This script pulls the version number from Directory.Build.props which is located in the root directory.
-    It then assigns the value to the BuildNumber) which can be used in pipeline yml files
+    It then assigns the value to the BuildNumber which can be used in pipeline yml files
 #>
 
 $propsFile = Resolve-Path -Path "$PSScriptRoot/../../Directory.Build.props"
