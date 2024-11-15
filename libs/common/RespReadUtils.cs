47f1a5981a108d55f1196a88cfbafbf30f1c685d
Tal Zaccai <talzacc@microsoft.com>
2024-11-14 20:15:29 -0800

    Comment fix

--- libs/common/RespReadUtils.cs
+++ libs/common/RespReadUtils.cs
@@ -378,7 +378,7 @@ namespace Garnet.common
             }
 
             // Validate length
-            if (value > int.MaxValue + (ulong)(negative ? 1 : 0)) // int.MinValue = -int.MaxValue + 1
+            if (value > int.MaxValue && (!negative || value > int.MaxValue + (ulong)1)) // int.MinValue = -int.MaxValue + 1
             {
                 RespParsingException.ThrowIntegerOverflow(readHead - digitsRead, (int)digitsRead);
             }
