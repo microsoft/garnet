8097861c8dd7622db4af4bc22e477200f26afce7
darrenge <darrenge@microsoft.com>
2024-11-18 13:27:39 -0800

    Added Version.props as the single place to update version instead of Directory.Build.props.  Push to GH Release and Push to Nuget ARE DISABLED for this check in to be able to test first

--- .azure/pipelines/extract_version.ps1
+++ .azure/pipelines/extract_version.ps1
@@ -1,11 +1,11 @@
 <#$f
 .DESCRIPTION
 
-    This script pulls the version number from Directory.Build.props which is located in the root directory.
+    This script pulls the version number from Version.props which is located in the root directory.
     It then assigns the value to the BuildNumber which can be used in pipeline yml files
 #>
 
-$propsFile = Resolve-Path -Path "$PSScriptRoot/../../Directory.Build.props"
+$propsFile = Resolve-Path -Path "$PSScriptRoot/../../Version.props"
 [xml]$xml = Get-Content -Path $propsFile
 $version = $xml.Project.PropertyGroup.VersionPrefix
 Write-Host "##vso[build.updatebuildnumber]$version"
