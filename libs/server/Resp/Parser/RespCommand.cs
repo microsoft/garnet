715f26a1e6f18532186c93cd372507165653eedc
Vasileios Zois <vazois@microsoft.com>
2024-12-04 18:03:27 -0800

    implement separate code path for CLUSTER SPUBLISH

--- libs/server/Resp/Parser/RespCommand.cs
+++ libs/server/Resp/Parser/RespCommand.cs
@@ -304,6 +304,7 @@ namespace Garnet.server
         CLUSTER_MYPARENTID,
         CLUSTER_NODES,
         CLUSTER_PUBLISH,
+        CLUSTER_SPUBLISH,
         CLUSTER_REPLICAS,
         CLUSTER_REPLICATE,
         CLUSTER_RESET,
@@ -1810,6 +1811,10 @@ namespace Garnet.server
                 else if (subCommand.SequenceEqual(CmdStrings.publish))
                 {
                     return RespCommand.CLUSTER_PUBLISH;
+                }
+                else if (subCommand.SequenceEqual(CmdStrings.spublish))
+                {
+                    return RespCommand.CLUSTER_SPUBLISH;
                 }
                 else if (subCommand.SequenceEqual(CmdStrings.MIGRATE))
                 {
