5982be161cca14b5dfa0cc7c3fbf049fccfe430e
Padmanabh Gupta (from Dev Box) <padgupta@microsoft.com>
2024-12-16 22:05:34 +0530

    fix tests for generic path

--- benchmark/BDN.benchmark/Network/Network.cs
+++ benchmark/BDN.benchmark/Network/Network.cs
@@ -16,15 +16,13 @@ namespace BDN.benchmark.Network
     [MemoryDiagnoser]
     public class Network : NetworkBase
     {
-
-
         [Benchmark]
         public async Task TestNetworkTask()
         {
             RemoteCertificateValidationCallback certValidation = (a, b, c, d) => { return true; };
             using (var client = new TcpClient())
             {
-                await client.ConnectAsync("127.0.0.1", 3280);
+                await client.ConnectAsync("127.0.0.1", 3278);
                 using (var sslStream = new SslStream(client.GetStream(), false, certValidation, null))
                 {
                     await sslStream.AuthenticateAsClientAsync("127.0.0.1");
