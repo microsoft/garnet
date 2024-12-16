3fc28b0160baab087ffd52fd180452953cf086dc
Padmanabh Gupta (from Dev Box) <padgupta@microsoft.com>
2024-12-16 22:05:34 +0530

    fix tests for generic path

--- benchmark/BDN.benchmark/Network/NetworkBase.cs
+++ benchmark/BDN.benchmark/Network/NetworkBase.cs
@@ -24,22 +24,27 @@ namespace BDN.benchmark.Network
         [GlobalSetup]
         public virtual void GlobalSetup()
         {
-            var tlsOptions = new GarnetTlsOptions(
-                       certFileName: "C:\\Users\\padgupta\\GarnetPublic\\garnet\\benchmark\\BDN.benchmark\\bin\\Release\\net8.0\\ssl-cert.pfx",
-                       certPassword: "",
-                       clientCertificateRequired: false,
-                       certificateRevocationCheckMode: X509RevocationMode.NoCheck,
-                       issuerCertificatePath: "C:\\Users\\padgupta\\GarnetPublic\\garnet\\benchmark\\BDN.benchmark\\bin\\Release\\net8.0\\ssl-cert.pfx",
-                       null, 0, false, null);
+            try
+            {
+                var tlsOptions = new GarnetTlsOptions(
+                           certFileName: "testcert.pfx",
+                           certPassword: "placeholder",
+                           clientCertificateRequired: false,
+                           certificateRevocationCheckMode: X509RevocationMode.NoCheck,
+                           issuerCertificatePath: "testcert.pfx",
+                           null, 0, false, null);
 
-            var server = new GarnetServerTcp("127.0.0.1", 3280, 0, tlsOptions);
-            _server = server;
-            _networkEchoCommandBuffer = Encoding.ASCII.GetBytes("\r\n\"*1\\r\\n$1\\r\\nECHO\\r\\n\"");
+                var server = new GarnetServerTcp("127.0.0.1", 3278, 0, tlsOptions);
+                _server = server;
+                _networkEchoCommandBuffer = Encoding.ASCII.GetBytes("\r\n\"*1\\r\\n$1\\r\\nECHO\\r\\n\"");
 
-            _server.Start();
+                _server.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
-
-
 
         /// <summary>
         /// Cleanup
