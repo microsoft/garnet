8f5625a0cbf8c7c0fb40c590441269bc957c9d7c
Padmanabh Gupta (from Dev Box) <padgupta@microsoft.com>
2024-12-17 21:38:23 +0530

    fix naming violations

--- benchmark/BDN.benchmark/Network/NetworkBase.cs
+++ benchmark/BDN.benchmark/Network/NetworkBase.cs
@@ -15,20 +15,13 @@ namespace BDN.benchmark.Network
 {
     public abstract class NetworkBase
     {
-        private Socket _serverSocket;
         /// <summary>
         /// Base class for operations benchmarks
         /// </summary>
-
-        protected byte[] _networkEchoCommandBuffer;
-
-        private ConcurrentDictionary<INetworkHandler, byte> activeHandlers;
-        private NetworkBufferSettings _networkBufferSettings;
-        private LimitedFixedBufferPool _fixedBufferPool;
-        protected RemoteCertificateValidationCallback certValidation = (a, b, c, d) => { return true; };
-        private IGarnetServer garnetServer;
-        private Dictionary<TcpClient, SslStream> _tcpClients;
-        private IReadOnlyList<SslStream> sslStreams;
+        protected RemoteCertificateValidationCallback CertValidationCallback = (a, b, c, d) => { return true; };
+        private IGarnetServer GarnetServer;
+        private Dictionary<TcpClient, SslStream> TcpClients;
+        private IReadOnlyList<SslStream> SslStreams;
 
         [GlobalSetup]
         public virtual async Task GlobalSetup()
@@ -43,8 +36,9 @@ namespace BDN.benchmark.Network
                   issuerCertificatePath: "testcert.pfx",
                   null, 0, false, null);
                 var serverBufferSize = BufferSizeUtils.ServerBufferSize(new MaxSizeSettings());
-                garnetServer = new GarnetServerTcp("127.0.0.1", 3278, serverBufferSize, tlsOptions);
-                garnetServer.Start();
+                GarnetServer = new GarnetServerTcp("127.0.0.1", 3278, serverBufferSize, tlsOptions);
+                GarnetServer.Start();
+                // This is done to compare sync vs async workloads since blocking patterns exhaust threadpool.
                 ThreadPool.SetMinThreads(4, 4);
                 await SetupClientPool();
             }
@@ -56,59 +50,21 @@ namespace BDN.benchmark.Network
 
         public IReadOnlyList<SslStream> GetStreams()
         {
-            return sslStreams;
+            return SslStreams;
         }
 
         private async Task SetupClientPool()
         {
-            _tcpClients = new Dictionary<TcpClient, SslStream>();
+            TcpClients = new Dictionary<TcpClient, SslStream>();
             for (int i = 0; i < 128; i++)
             {
                 var client = new TcpClient();
                 await client.ConnectAsync("127.0.0.1", 3278);
-                var sslStream = new SslStream(client.GetStream(), false, certValidation, null);
-                _tcpClients.Add(client, sslStream);
+                var sslStream = new SslStream(client.GetStream(), false, CertValidationCallback, null);
+                TcpClients.Add(client, sslStream);
             }
-            sslStreams = [.. _tcpClients.Values];
+            SslStreams = [..TcpClients.Values];
         }
-
-        public void StartSocketAccept()
-        {
-            var endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3278);
-            var acceptEventArg = new SocketAsyncEventArgs();
-            _serverSocket.Bind(endPoint);
-            _serverSocket.Listen(512);
-            if (!_serverSocket.AcceptAsync(acceptEventArg))
-                AcceptEventArg_Completed(null, acceptEventArg);
-            acceptEventArg.Completed += AcceptEventArg_Completed;
-        }
-
-        private void AcceptEventArg_Completed(object sender, SocketAsyncEventArgs e)
-        {
-            try
-            {
-                do
-                {
-                    var tlsOptions = new GarnetTlsOptions(
-                           certFileName: "testcert.pfx",
-                           certPassword: "placeholder",
-                           clientCertificateRequired: false,
-                           certificateRevocationCheckMode: X509RevocationMode.NoCheck,
-                           issuerCertificatePath: "testcert.pfx",
-                           null, 0, false, null);
-                    MockTcpNetworkHandler networkHandler = new MockTcpNetworkHandler(e.AcceptSocket, _networkBufferSettings, _fixedBufferPool, true);
-                    if (activeHandlers.TryAdd(networkHandler, 0))
-                    {
-                        networkHandler.Start(tlsOptions.TlsServerOptions);
-                    }
-
-                    e.AcceptSocket = null;
-                } while (!_serverSocket.AcceptAsync(e));
-            }
-            // socket disposed
-            catch (ObjectDisposedException) { }
-        }
-
 
         /// <summary>
         /// Cleanup
@@ -116,12 +72,12 @@ namespace BDN.benchmark.Network
         [GlobalCleanup]
         public virtual void GlobalCleanup()
         {
-            foreach (var tcpClient in _tcpClients)
+            foreach (var tcpClient in TcpClients)
             {
                 tcpClient.Value.Dispose();
                 tcpClient.Key.Dispose();
             }
-            garnetServer.Dispose();
+            GarnetServer.Dispose();
         }
     }
 }
