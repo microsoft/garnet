b64dbe8bceab729396d8269e78bb0fd0c15d04fb
github-action-benchmark <github@users.noreply.github.com>
2025-01-14 22:46:58 +0000

    add Cluster.ClusterOperations (ubuntu-latest  net8.0 Release) (benchmarkdotnet) benchmark result for b5997ae88f7782a301144c95a2144ef04b229953

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736894790140,
+  "lastUpdate": 1736894818640,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Benchmark": [],
@@ -7241,6 +7241,90 @@ window.BENCHMARK_DATA = {
             "value": 133787.66983468193,
             "unit": "ns",
             "range": "± 706.9832637582855"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "42720645+hamdaankhalid@users.noreply.github.com",
+            "name": "Hamdaan Khalid",
+            "username": "hamdaankhalid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b5997ae88f7782a301144c95a2144ef04b229953",
+          "message": "ETag Follow Up: Address Open Comments (#912)\n\n* ETag Follow Up: Address Open Comments\r\n\r\n* fmt\r\n\r\n* PR feedback:\r\n\r\n* definitely an issue with pattern matching precedence\r\n\r\n---------\r\n\r\nCo-authored-by: Hamdaan Khalid <hkhalid@microsoft.com>\r\nCo-authored-by: Tal Zaccai <talzacc@microsoft.com>",
+          "timestamp": "2025-01-14T15:42:10-07:00",
+          "tree_id": "41bd3dd1d5c72789b6fce8dd130be30f5a5b584e",
+          "url": "https://github.com/microsoft/garnet/commit/b5997ae88f7782a301144c95a2144ef04b229953"
+        },
+        "date": 1736894815959,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Cluster.ClusterOperations.Get(Params: DSV)",
+            "value": 17317.587332589286,
+            "unit": "ns",
+            "range": "± 70.63586109371393"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterOperations.Set(Params: DSV)",
+            "value": 16599.930295671737,
+            "unit": "ns",
+            "range": "± 94.66042868007894"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterOperations.MGet(Params: DSV)",
+            "value": 15214.736358642578,
+            "unit": "ns",
+            "range": "± 22.02044664897902"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterOperations.MSet(Params: DSV)",
+            "value": 14670.49536895752,
+            "unit": "ns",
+            "range": "± 51.75745041764897"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterOperations.CTXNSET(Params: DSV)",
+            "value": 125208.05119977679,
+            "unit": "ns",
+            "range": "± 444.84745110399575"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterOperations.Get(Params: None)",
+            "value": 21961.901481119792,
+            "unit": "ns",
+            "range": "± 148.16239629689136"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterOperations.Set(Params: None)",
+            "value": 20594.437779353215,
+            "unit": "ns",
+            "range": "± 34.15403924902381"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterOperations.MGet(Params: None)",
+            "value": 16027.666240147182,
+            "unit": "ns",
+            "range": "± 115.494558828419"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterOperations.MSet(Params: None)",
+            "value": 15801.344713483539,
+            "unit": "ns",
+            "range": "± 121.5530124961597"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterOperations.CTXNSET(Params: None)",
+            "value": 131129.77681477866,
+            "unit": "ns",
+            "range": "± 314.70874728014377"
           }
         ]
       }
