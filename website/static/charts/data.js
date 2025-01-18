121fe17c44b7516ee2d73327f67ad540df91cf2e
github-action-benchmark <github@users.noreply.github.com>
2025-01-18 01:07:30 +0000

    add Cluster.ClusterMigrate (ubuntu-latest  net8.0 Release) (benchmarkdotnet) benchmark result for e9f6b5094a88b5285280e0ef2736d635304cbbd6

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1737162423468,
+  "lastUpdate": 1737162450246,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Benchmark": [],
@@ -3599,6 +3599,54 @@ window.BENCHMARK_DATA = {
             "value": 31906.264038085938,
             "unit": "ns",
             "range": "± 42.4315586750673"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "96085550+vazois@users.noreply.github.com",
+            "name": "Vasileios Zois",
+            "username": "vazois"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e9f6b5094a88b5285280e0ef2736d635304cbbd6",
+          "message": "fix bug with getting long value (#922)",
+          "timestamp": "2025-01-17T18:04:11-07:00",
+          "tree_id": "38828cbecbb22af2242609dfb567672c8c565ce2",
+          "url": "https://github.com/microsoft/garnet/commit/e9f6b5094a88b5285280e0ef2736d635304cbbd6"
+        },
+        "date": 1737162446565,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Cluster.ClusterMigrate.Get(Params: None)",
+            "value": 42688.40985514323,
+            "unit": "ns",
+            "range": "± 204.0901085511422"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterMigrate.Set(Params: None)",
+            "value": 42019.46021728516,
+            "unit": "ns",
+            "range": "± 224.6274464066673"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterMigrate.MGet(Params: None)",
+            "value": 32250.84974161784,
+            "unit": "ns",
+            "range": "± 55.917313410623976"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterMigrate.MSet(Params: None)",
+            "value": 31529.919276646204,
+            "unit": "ns",
+            "range": "± 17.07843287646566"
           }
         ]
       }
