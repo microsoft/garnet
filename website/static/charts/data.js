3c487e0d7db4c5f20bcab982405149c562e5decc
github-action-benchmark <github@users.noreply.github.com>
2025-01-21 02:50:10 +0000

    add Network.BasicOperations (ubuntu-latest  net8.0 Release) (benchmarkdotnet) benchmark result for 43654fd79a68aa7cde1f4ef8bf1d1c31a8f27e34

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1737424606535,
+  "lastUpdate": 1737427810102,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Benchmark": [],
@@ -78931,6 +78931,36 @@ window.BENCHMARK_DATA = {
             "value": 90.98365623156229,
             "unit": "ns",
             "range": "± 0.8061609867067967"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tejaskulkarni98902@gmail.com",
+            "name": "Tejas  Kulkarni",
+            "username": "Xizt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "43654fd79a68aa7cde1f4ef8bf1d1c31a8f27e34",
+          "message": "Require version bump for latest gossip changes (#932)\n\nCo-authored-by: Badrish Chandramouli <badrishc@microsoft.com>",
+          "timestamp": "2025-01-20T18:47:33-08:00",
+          "tree_id": "abdf5ef6da5a4face42bb1c148a16571a14d6ca2",
+          "url": "https://github.com/microsoft/garnet/commit/43654fd79a68aa7cde1f4ef8bf1d1c31a8f27e34"
+        },
+        "date": 1737427805312,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Network.BasicOperations.InlinePing(Params: None)",
+            "value": 91.16556402615139,
+            "unit": "ns",
+            "range": "± 0.18516177925013116"
           }
         ]
       }
