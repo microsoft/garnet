49a887e3142e4c46df8bd9151ea39eccb7be84d6
github-action-benchmark <github@users.noreply.github.com>
2025-01-21 02:51:05 +0000

    add Operations.BasicOperations (ubuntu-latest  net8.0 Release) (benchmarkdotnet) benchmark result for 43654fd79a68aa7cde1f4ef8bf1d1c31a8f27e34

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1737427853775,
+  "lastUpdate": 1737427864979,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Benchmark": [],
@@ -1805,6 +1805,48 @@ window.BENCHMARK_DATA = {
             "value": 1616.6267449061077,
             "unit": "ns",
             "range": "± 1.6296793500412292"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tejaskulkarni98902@gmail.com",
+            "name": "Tejas  Kulkarni",
+            "username": "Xizt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "43654fd79a68aa7cde1f4ef8bf1d1c31a8f27e34",
+          "message": "Require version bump for latest gossip changes (#932)\n\nCo-authored-by: Badrish Chandramouli <badrishc@microsoft.com>",
+          "timestamp": "2025-01-20T18:47:33-08:00",
+          "tree_id": "abdf5ef6da5a4face42bb1c148a16571a14d6ca2",
+          "url": "https://github.com/microsoft/garnet/commit/43654fd79a68aa7cde1f4ef8bf1d1c31a8f27e34"
+        },
+        "date": 1737427860882,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: ACL)",
+            "value": 1778.1541067270132,
+            "unit": "ns",
+            "range": "± 8.143316222323637"
+          },
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: AOF)",
+            "value": 1775.0517493656703,
+            "unit": "ns",
+            "range": "± 6.757966254558999"
+          },
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: None)",
+            "value": 1624.2669161478677,
+            "unit": "ns",
+            "range": "± 11.229584649672285"
           }
         ]
       }
