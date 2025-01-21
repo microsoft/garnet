4323360ee0e9d0e329beddae2530124512827703
Matt Tremblay <matrembl@microsoft.com>
2025-01-21 16:59:23 -0500

    Fix cluster worker[0] init in 2nd constructor

---
+++ test/Garnet.test.cluster/ClusterConfigTests.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+using Garnet.cluster;
+using Garnet.common;
+using NUnit.Framework;
+using StackExchange.Redis;
+
+namespace Garnet.test.cluster
+{
+    [TestFixture, NonParallelizable]
+    internal class ClusterConfigTests
+    {
+        [Test, Order(1)]
+        [Category("CLUSTER-CONFIG"), CancelAfter(1000)]
+        public void ClusterConfigInitializesUnassignedWorkerTest()
+        {
+            ClusterConfig config = new ClusterConfig().InitializeLocalWorker(
+                Generator.CreateHexId(),
+                "127.0.0.1",
+                7001,
+                configEpoch: 0,
+                NodeRole.PRIMARY,
+                null,
+                "");
+
+            (string address, int port) = config.GetWorkerAddress(0);
+            Assert.That(address == "unassigned");
+            Assert.That(port == 0);
+            Assert.That(NodeRole.UNASSIGNED == config.GetNodeRoleFromNodeId("asdasdqwe"));
+
+            var configBytes = config.ToByteArray();
+            var restoredConfig = ClusterConfig.FromByteArray(configBytes);
+
+            (address, port) = restoredConfig.GetWorkerAddress(0);
+            Assert.That(address == "unassigned");
+            Assert.That(port == 0);
+            Assert.That(NodeRole.UNASSIGNED == restoredConfig.GetNodeRoleFromNodeId("asdasdqwe"));
+        }
+    }
+}
