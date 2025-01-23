5036a985a0f133757721fb9ac4906a502fbfb452
Tal Zaccai <talzacc@microsoft.com>
2025-01-22 18:56:00 -0700

    Added comment in GOSO

--- libs/server/Objects/Types/GarnetObjectStoreOutput.cs
+++ libs/server/Objects/Types/GarnetObjectStoreOutput.cs
@@ -30,6 +30,8 @@ namespace Garnet.server
 
     /// <summary>
     /// Output type used by Garnet object store.
+    /// Any field / property added to this struct must be set in the back-end (IFunctions) and used in the front-end (GarnetApi caller).
+    /// That is in order to justify transferring data in this struct through the Tsavorite storage layer.
     /// </summary>
     public struct GarnetObjectStoreOutput
     {
