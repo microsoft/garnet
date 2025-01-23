ed241b9995c07a52ed757db69be79768f1e9b937
Vasileios Zois <96085550+vazois@users.noreply.github.com>
2025-01-23 14:01:43 -0800

    added separate active connection tracker (#938)

--- libs/server/Resp/ClientCommands.cs
+++ libs/server/Resp/ClientCommands.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Garnet.common;
-using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Logging;
 
 namespace Garnet.server
