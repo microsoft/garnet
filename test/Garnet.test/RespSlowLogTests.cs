16690ec324f74cb1995f433552d3388e1054c879
Badrish Chandramouli <badrishc@microsoft.com>
2025-01-29 15:07:02 -0800

    add a better test with added entry to slow log

--- test/Garnet.test/RespSlowLogTests.cs
+++ test/Garnet.test/RespSlowLogTests.cs
@@ -11,12 +11,13 @@ namespace Garnet.test
     public class RespSlowLogTests
     {
         GarnetServer server;
+        int slowLogThreshold = 3_000_000;
 
         [SetUp]
         public void Setup()
         {
             TestUtils.DeleteDirectory(TestUtils.MethodTestDir, wait: true);
-            server = TestUtils.CreateGarnetServer(TestUtils.MethodTestDir, disablePubSub: true, latencyMonitor: false, DisableObjects: true, slowLogThreshold: 5_000_000);
+            server = TestUtils.CreateGarnetServer(TestUtils.MethodTestDir, disablePubSub: true, latencyMonitor: false, DisableObjects: false, slowLogThreshold: slowLogThreshold);
             server.Start();
         }
 
@@ -54,6 +55,38 @@ namespace Garnet.test
             var db = redis.GetDatabase(0);
             var result = db.Execute("SLOWLOG", "GET", -1);
             ClassicAssert.AreEqual(0, ((string[])result).Length);
+        }
+
+        [Test]
+        public void TestSlowLogGetWithEntry()
+        {
+            using var redis = ConnectionMultiplexer.Connect(TestUtils.GetConfig());
+            var db = redis.GetDatabase(0);
+            var result = db.Execute("SLOWLOG", "GET", -1);
+            ClassicAssert.AreEqual(0, ((string[])result).Length);
+
+            // Run a command that will exceed the slow log threshold
+            string timeout = $"{(float)(0.1 + slowLogThreshold / 1_000_000)}";
+            db.Execute("BLPOP", "foo", timeout);
+
+            var newResult = (RedisResult[])db.Execute("SLOWLOG", "GET", -1);
+            ClassicAssert.AreEqual(1, newResult.Length);
+
+            var entry = (RedisResult[])newResult[0];
+            ClassicAssert.AreEqual(6, entry.Length); // Number of fields in each entry
+            ClassicAssert.AreEqual(0, (int)entry[0]); // Id
+            ClassicAssert.Less(slowLogThreshold, (int)entry[2]); // Execution time
+
+            var args = (string[])entry[3]; // Command and arguments
+            ClassicAssert.AreEqual(3, args.Length);
+            ClassicAssert.AreEqual("BLPOP", args[0]);
+            ClassicAssert.AreEqual("foo", args[1]);
+            ClassicAssert.AreEqual(timeout, args[2]);
+
+            db.Execute("SLOWLOG", "RESET");
+
+            var resetResult = db.Execute("SLOWLOG", "GET", -1);
+            ClassicAssert.AreEqual(0, ((string[])resetResult).Length);
         }
 
         [Test]
