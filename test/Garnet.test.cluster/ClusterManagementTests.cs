7d44d74f464675d12541780cef70eb24b4b6bdd8
prvyk <52283326+prvyk@users.noreply.github.com>
2025-01-30 00:20:57 +0200

    Adding ROLE command (#964)
    * More structed GetReplicaInfo in order to prepare for ROLE command addition.
    
    * Add ROLE command, tests and documentation.
    
    * Maybe this way is cleaner.
    
    * dotnet format
    
    * Move role test
    
    * Consolidate new classes into RoleInfo, add comments.
    
    * dotnet format
    
    * Inheritdoc
    
    * Add CLUSTER NODES test.
    
    * Revert CLUSTER NODES test.
    
    * Separate metrics path out again and use this to simplify ROLE command path.
    
    * Just get the replication offset directly to simplify the code.
    
    * Return RoleInfo list and build output for metrics GarnetInfoMetrics
    
    ---------
    
    Co-authored-by: prvyk <github@privatemail.fastmailbox.net>
    Co-authored-by: Vasileios Zois <vazois@microsoft.com>

--- test/Garnet.test.cluster/ClusterManagementTests.cs
+++ test/Garnet.test.cluster/ClusterManagementTests.cs
@@ -606,5 +606,31 @@ namespace Garnet.test.cluster
                 ClassicAssert.AreEqual("ERR syntax error", errorMsg);
             }
         }
+
+        [Test, Order(11)]
+        public void ClusterRoleCommand()
+        {
+            var node_count = 3;
+            var replica_count = node_count - 1;
+            context.CreateInstances(node_count, enableAOF: true);
+            context.CreateConnection();
+            var (_, _) = context.clusterTestUtils.SimpleSetupCluster(1, replica_count, logger: context.logger);
+
+            var result = context.clusterTestUtils.GetServer(0).Execute("ROLE");
+            ClassicAssert.AreEqual(3, result.Length);
+            ClassicAssert.AreEqual("master", result[0].ToString());
+            ClassicAssert.True(int.TryParse(result[1].ToString(), out _));
+            ClassicAssert.AreEqual(2, result[2].Length);
+            ClassicAssert.AreEqual("127.0.0.1", result[2][0][0].ToString());
+            ClassicAssert.AreEqual("127.0.0.1", result[2][1][0].ToString());
+
+            result = context.clusterTestUtils.GetServer(1).Execute("ROLE");
+            ClassicAssert.AreEqual(5, result.Length);
+            ClassicAssert.AreEqual("slave", result[0].ToString());
+            ClassicAssert.AreEqual("127.0.0.1", result[1].ToString());
+            ClassicAssert.True(int.TryParse(result[2].ToString(), out _));
+            ClassicAssert.AreEqual("connected", result[3].ToString());
+            ClassicAssert.True(int.TryParse(result[4].ToString(), out _));
+        }
     }
 }
