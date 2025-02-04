0ddfc9edad3bad808724ed9fe3c5116df6d4ca88
Badrish Chandramouli <badrishc@microsoft.com>
2025-02-04 14:29:35 -0800

    Add support for slowlog (#971)
    * SLOWLOG command skeleton
    
    * add configs
    
    * update docs
    
    * increase slowlog minimum threshold
    
    * updates
    
    * fix
    
    * update json
    
    * Revert "update json"
    
    This reverts commit 2d1853852ed0124c929da519f8708ad981f20978.
    
    * add json
    
    * update ACL tests
    speed up blocking list tests by reducing timeout
    
    * Fix AofIndependentCommandsTest
    
    * updates
    
    * more cleanup
    
    * more updates and cleanup
    
    * nits
    
    * fix format
    
    * nit
    
    * nit
    
    * updates + tests
    
    * cleanup
    
    * update docs
    
    * add a better test with added entry to slow log
    
    * update redis.conf compat
    
    * use AbortWithWrongNumberOfArguments
    
    * nits
    
    * Fix warning in LightClient

---
+++ libs/server/Metrics/Slowlog/SlowlogEntry.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+namespace Garnet.server
+{
+    /// <summary>
+    /// Each entry in the slow log
+    /// </summary>
+    internal struct SlowLogEntry
+    {
+        public int Id;
+        public int Timestamp;
+        public int Duration;
+        public RespCommand Command;
+        public byte[] Arguments;
+        public string ClientIpPort;
+        public string ClientName;
+    }
+}
