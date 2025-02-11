16fd4fe34985b4c99bd85d98f1d55facb605db68
Vasileios Zois <vazois@microsoft.com>
2025-02-11 13:38:34 -0800

    Fix bitpos bit search

--- libs/server/Resp/Bitmap/BitmapManagerBitPos.cs
+++ libs/server/Resp/Bitmap/BitmapManagerBitPos.cs
@@ -53,32 +53,33 @@ namespace Garnet.server
         /// <param name="endBitOffset"></param>
         /// <param name="searchFor"></param>
         /// <returns></returns>
-        private static long BitPosBitSearch(byte* input, long inputLen, long startBitOffset, long endBitOffset, byte searchFor)
+        private static long BitPosBitSearch2(byte* input, long inputLen, long startBitOffset, long endBitOffset, byte searchFor)
         {
             // Initialize variables
             var currentBitOffset = startBitOffset;
             var searchBit = searchFor == 1;
-            var mask = searchBit ? 0L : -1L;
+            var mask = searchBit ? 0x00 : 0xff;
 
             // Iterate through the byte array
             while (currentBitOffset <= endBitOffset)
             {
                 // Calculate byte and bit positions
                 var byteIndex = currentBitOffset / 8;
-                var bitIndex = (int)(currentBitOffset % 8);
+                var bitIndex = (int)(currentBitOffset & 7);
 
-                // Create a payload with the current byte shifted to the most significant byte position
-                var payload = (long)input[byteIndex] << 56;
-
-                // Trim leading bits
-                payload <<= bitIndex;
-
-                // Transform to count leading zeros
-                payload = searchBit ? payload : ~payload;
-
-                // Check if the payload matches the mask
-                if (payload != mask)
+                var payload = input[byteIndex] & (1 << 8 - (int)bitIndex) - 1;
+                if (mask != payload)
                 {
+                    // Create a payload with the current byte shifted to the most significant byte position
+                    payload = payload << 56;
+
+                    // Trim leading bits
+                    payload <<= bitIndex;
+
+                    // Transform to count leading zeros
+                    payload = searchBit ? payload : ~payload;
+
+                    // Check if the payload matches the mask
                     // Calculate the position of the first matching bit
                     var lzcnt = (long)Lzcnt.X64.LeadingZeroCount((ulong)payload);
                     return currentBitOffset + lzcnt;
@@ -91,6 +92,40 @@ namespace Garnet.server
             // Return -1 if no matching bit is found
             return -1;
         }
+
+        private static long BitPosBitSearch(byte* input, long inputLen, long startBitOffset, long endBitOffset, byte searchFor)
+        {
+            var searchBit = searchFor == 1;
+            var invalidPayload = (byte)(searchBit ? 0x0 : 0xff);
+            var currentBitOffset = (int)startBitOffset;
+            while (currentBitOffset <= endBitOffset)
+            {
+                var byteIndex = currentBitOffset >> 3;
+                var leftBitOffset = currentBitOffset & 7;
+                var boundary = 8 - leftBitOffset;
+                var rightBitOffset = currentBitOffset + boundary < endBitOffset ? currentBitOffset + boundary : (int)(endBitOffset & 7) + 1;
+
+                // Trim byte to start and end bit index
+                var mask = (0xff >> leftBitOffset) ^ (0xff >> rightBitOffset);
+                var payload = (long)(input[byteIndex] & mask);
+
+                // If transformed payload is invalid skip to next byte
+                if (payload != invalidPayload)
+                {
+                    payload <<= (56 + leftBitOffset);
+                    payload = searchBit ? payload : ~payload;
+
+                    var lzcnt = (long)Lzcnt.X64.LeadingZeroCount((ulong)payload);
+                    return currentBitOffset + lzcnt;
+                }
+
+                currentBitOffset += boundary;
+            }
+
+            return -1;
+        }
+
+
 
         /// <summary>
         /// Search for position of bit set in byte array using byte offset for start and end range
