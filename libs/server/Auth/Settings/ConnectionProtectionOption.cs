780c0b8486a394a68a21ea3ae511d4b762980427
prvyk <52283326+prvyk@users.noreply.github.com>
2025-02-13 00:54:53 +0200

    Add a DEBUG command. (#999)
    * Add DEBUG command.
    
    * Correct description in conf file.
    
    ---------
    
    Co-authored-by: prvyk <github@privatemail.fastmailbox.net>

---
+++ libs/server/Auth/Settings/ConnectionProtectionOption.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+namespace Garnet.server.Auth.Settings
+{
+    /// <summary>
+    /// Certain commands can be set at configuration to be limited to a particular connection type.
+    /// They can be blocked for all connections, allowed only for local connections or be allowed for all connections.
+    /// </summary>
+    public enum ConnectionProtectionOption
+    {
+        /// <summary>
+        /// Block for all connections.
+        /// </summary>
+        No = 0,
+        /// <summary>
+        ///  Allow only for local connections.
+        /// </summary>
+        Local = 1,
+        /// <summary>
+        /// Allow for every connection including remote connections.
+        /// </summary>
+        Yes = 2
+    }
+}
