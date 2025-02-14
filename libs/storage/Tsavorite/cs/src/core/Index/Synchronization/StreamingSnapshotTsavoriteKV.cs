5dcb5d2ee733ea9fb1f78d8a4281532b0d5d72af
Badrish Chandramouli <badrishc@microsoft.com>
2025-02-14 11:55:54 -0800

    updates

--- libs/storage/Tsavorite/cs/src/core/Index/Synchronization/StreamingSnapshotTsavoriteKV.cs
+++ libs/storage/Tsavorite/cs/src/core/Index/Synchronization/StreamingSnapshotTsavoriteKV.cs
@@ -81,6 +81,10 @@ namespace Tsavorite.core
             }
             finally
             {
+                // We started this task before entering PREP_STREAMING_SNAPSHOT_CHECKPOINT, so we
+                // need to wait until the state machine is in PREP_STREAMING_SNAPSHOT_CHECKPOINT
+                while (systemState.Phase != Phase.PREP_STREAMING_SNAPSHOT_CHECKPOINT)
+                    Thread.Yield();
                 GlobalStateMachineStep(systemState);
             }
         }
@@ -145,6 +149,11 @@ namespace Tsavorite.core
             {
                 // Release the semaphore to allow the checkpoint waiting task to proceed
                 _hybridLogCheckpoint.flushedSemaphore.Release();
+
+                // We started this task before entering WAIT_FLUSH, so we
+                // need to wait until the state machine is in WAIT_FLUSH
+                while (systemState.Phase != Phase.WAIT_FLUSH)
+                    Thread.Yield();
                 GlobalStateMachineStep(systemState);
             }
         }
