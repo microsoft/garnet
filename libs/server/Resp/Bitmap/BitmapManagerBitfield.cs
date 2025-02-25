fe92871967ddd10f2f5fe8de043526420faa77c6
Vasileios Zois <vazois@microsoft.com>
2025-02-24 20:32:22 -0800

    cleanup bitfield implementation

--- libs/server/Resp/Bitmap/BitmapManagerBitfield.cs
+++ libs/server/Resp/Bitmap/BitmapManagerBitfield.cs
@@ -6,6 +6,32 @@ using Garnet.common;
 
 namespace Garnet.server
 {
+    // [Implementation of getbitfield operation]
+    // The algorithm assumes bits are stored in bitmap in a sequence of bytes
+    // for which the most significant bit is the most significant bit of the
+    // byte appearing at offset position and the least significant bit is the bit
+    // appearing at position offset + bitCount.
+
+    // e.g.example of 13-bit value(bits represented with V) stored within two adjacent bytes
+    //       [offset / 8][offset / 8 + 1]
+    // ..... HHVV VVVV VVVV VVVT....
+
+    // The get algorithm works by constructing a 64-bit value from the individual bits between offset
+    // and offset + bitCount, and shifting those bits the right amount on the right to generate the
+    // given bitCount-bit value.
+    // To extract the given number of bits from the sequence of bytes representing the given bitmap,
+    // we need first to read all bytes between the offset and offset + bitCount position.
+    // e.g. for the previous example we would read 2 bytes
+    // HHVV VVVV VVVV VVVT
+    // After reading we need to discard the leading bits (i.e.HH) and the tail bits
+    // We achieve this by shifting left the same amount of HH bits and then right 64 - bitCount.
+
+    // A special case exists when the offset appears at a position that does not give us enough bits
+    // from the most significant byte portion.
+    // In that case we need to read an additional byte and append it to the of the 64 bit value.
+    // After that we again shift right byt 64-bitCount position to retain the exact number of bits we need
+    // according to the bitCount parameter.
+
     public unsafe partial class BitmapManager
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -61,44 +87,28 @@ namespace Garnet.server
             return valueLen > lengthInBytes ? valueLen : lengthInBytes;
         }
 
-        /// [Implementation of getbitfield operation]
-        /// The algorithm assumes bits are stored in bitmap in a sequence of bytes
-        /// for which the most significant bit is the most significant bit of the 
-        /// byte appearing at offset position and the least significant bit is the bit
-        /// appearing at position offset + bitCount.
-        /// 
-        /// e.g. example of 13-bit value (bits represented with V) stored within two adjacent bytes
-        ///       [offset/8] [offset/8 + 1]
-        /// ..... HHVV VVVV VVVV VVVT ....
-        ///
-        /// The get algorithm works by constructing a 64-bit value from the individual bits between offset 
-        /// and offset + bitCount, and shifting those bits the right amount on the right to generate the 
-        /// given bitCount-bit value.
-        /// To extract the given number of bits from the sequence of bytes representing the given bitmap,
-        /// we need first to read all bytes between the offset and offset + bitCount position.
-        /// e.g. for the previous example we would read 2 bytes
-        /// HHVV VVVV VVVV VVVT
-        /// After reading we need to discard the leading bits (i.e. HH) and the tail bits
-        /// We achieve this by shifting left the same amount of HH bits and then right 64 - bitCount.
-        /// 
-        /// A special case exists when the offset appears at a position that does not give us enough bits 
-        /// from the most significant byte portion.
-        /// In that case we need to read an additional byte and append it to the of the 64 bit value.
-        /// After that we again shift right byt 64-bitCount position to retain the exact number of bits we need
-        /// according to the bitCount parameter.
+        /// <summary>
+        /// Implementation of getbitfield operation.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="valLen"></param>
+        /// <param name="offset"></param>
+        /// <param name="bitCount"></param>
+        /// <param name="typeInfo"></param>
+        /// <returns></returns>
         private static long GetBitfieldValue(byte* value, long valLen, long offset, byte bitCount, byte typeInfo)
         {
-            int byteIndexStart = Index(offset);
-            int byteIndexEnd = Index(offset + bitCount) + 1;
-            byte* buf = stackalloc byte[8];
+            var byteIndexStart = Index(offset);
+            var byteIndexEnd = Index(offset + bitCount) + 1;
+            var buf = stackalloc byte[8];
             buf[0] = buf[1] = buf[2] = buf[3] = buf[4] = buf[5] = buf[6] = buf[7] = 0;
 
             //Simple case value is beyond current length
             if (byteIndexStart >= valLen) return 0;
 
-            byte* vend = (value + valLen);
-            byte* curr = (value + byteIndexStart);
-            byte* cend = (value + byteIndexEnd) < vend ? (value + byteIndexEnd) : vend;
+            var vend = value + valLen;
+            var curr = value + byteIndexStart;
+            var cend = value + byteIndexEnd < vend ? (value + byteIndexEnd) : vend;
 
             if (curr < cend) buf[7] = *curr++;
             if (curr < cend) buf[6] = *curr++;
@@ -108,10 +118,10 @@ namespace Garnet.server
             if (curr < cend) buf[2] = *curr++;
             if (curr < cend) buf[1] = *curr++;
             if (curr < cend) buf[0] = *curr++;
-            long returnValue = (*(long*)buf);
+            var returnValue = *(long*)buf;
 
             //prune leading bits
-            int _left = (int)(offset - (byteIndexStart << 3));
+            var _left = (int)(offset - (byteIndexStart << 3));
             returnValue = returnValue << _left;
 
             //extract an additional byte if 64 bit buffer needs more bytes
@@ -120,12 +130,12 @@ namespace Garnet.server
             if ((64 - _left) < bitCount)
             {
                 //extract number of bits skipped because of offset and position them at the tail of the partially constructed 64-bit value
-                byte _lsb = (byte)(curr < vend ? (*curr) >> (8 - _left) : 0);
+                var _lsb = (byte)(curr < vend ? (*curr) >> (8 - _left) : 0);
                 returnValue |= (long)_lsb;
             }
 
             //shift 64 bit value to construct the given number based of bitCount
-            int _right = (64 - bitCount);
+            var _right = (64 - bitCount);
             returnValue = (typeInfo & (byte)BitFieldSign.SIGNED) > 0 ?
                 returnValue >> _right :
                 (long)(((ulong)returnValue) >> _right);
@@ -133,6 +143,18 @@ namespace Garnet.server
             return returnValue;
         }
 
+        /// <summary>
+        /// Implementation of setbitfield operation.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="valLen"></param>
+        /// <param name="offset"></param>
+        /// <param name="bitCount"></param>
+        /// <param name="typeInfo"></param>
+        /// <param name="newValue"></param>
+        /// <param name="overflowType"></param>
+        /// <returns></returns>
+        /// <exception cref="GarnetException"></exception>
         private static (long, bool) SetBitfieldValue(byte* value, long valLen, long offset, byte bitCount, byte typeInfo, long newValue, byte overflowType)
         {
             int byteIndexStart = Index(offset);
@@ -239,6 +261,18 @@ namespace Garnet.server
             return (oldValue, false);
         }
 
+        /// <summary>
+        /// Implementation of incrbybitfield operation.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="valLen"></param>
+        /// <param name="offset"></param>
+        /// <param name="bitCount"></param>
+        /// <param name="typeInfo"></param>
+        /// <param name="incrValue"></param>
+        /// <param name="overflowType"></param>
+        /// <returns></returns>
+        /// <exception cref="GarnetException"></exception>
         private static (long, bool) IncrByBitfieldValue(byte* value, long valLen, long offset, byte bitCount, byte typeInfo, long incrValue, byte overflowType)
         {
             int byteIndexStart = Index(offset);
@@ -341,6 +375,17 @@ namespace Garnet.server
             return (newValue, overflow);
         }
 
+        /// <summary>
+        /// Check if bitfield operation will overflow.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="incrBy"></param>
+        /// <param name="result"></param>
+        /// <param name="bitCount"></param>
+        /// <param name="overflowType"></param>
+        /// <param name="signed"></param>
+        /// <returns></returns>
+        /// <exception cref="GarnetException"></exception>
         private static bool CheckBitfieldOverflow(long value, long incrBy, out long result, byte bitCount, byte overflowType, bool signed)
         {
             long newValue;
@@ -356,96 +401,95 @@ namespace Garnet.server
                 newValue = (long)nv;
             }
 
-            //ignore overflow flag if warp or sat, do not need to return nil or skip set in that case
-#pragma warning disable IDE0075 // Simplify conditional expression
-            overflow = overflowType == (byte)BitFieldOverflow.FAIL ? overflow : false;
-#pragma warning restore IDE0075 // Simplify conditional expression
+            // Ignore overflow flag if warp or sat, do not need to return nil or skip set in that case
+            overflow = overflowType == (byte)BitFieldOverflow.FAIL && overflow;
             result = newValue;
 
             return overflow;
-        }
 
-        private static (ulong, bool) CheckUnsignedBitfieldOverflow(ulong value, long incrBy, byte bitCount, byte overflowType)
-        {
-            ulong maxVal = bitCount == 64 ? ulong.MaxValue : (1UL << bitCount) - 1;
-            ulong maxAdd = maxVal - value;
-
-            bool neg = incrBy < 0;
-            //get absolute value of given increment
-            ulong absIncrBy = incrBy < 0 ? (ulong)(~incrBy) + 1UL : (ulong)incrBy;
-            //overflow if absolute increment is larger than diff of maxVal and current value
-            bool overflow = (absIncrBy > maxAdd);
-            //underflow if absolute increment bigger than increment and increment is negative
-            bool underflow = (absIncrBy > value) && neg;
-
-            ulong result;
-            ulong mask = maxVal;
-            result = neg ? value - absIncrBy : value + absIncrBy;
-            result &= mask;
-            switch (overflowType)
+            static (ulong, bool) CheckUnsignedBitfieldOverflow(ulong value, long incrBy, byte bitCount, byte overflowType)
             {
-                case (byte)BitFieldOverflow.WRAP:
-                    if (overflow || underflow)
-                        return (result, true);
-                    return (result, false);
-                case (byte)BitFieldOverflow.SAT:
-                    if (overflow) return (maxVal, true);
-                    else if (underflow) return (0, true);
-                    return (result, false);
-                case (byte)BitFieldOverflow.FAIL:
-                    if (overflow || underflow)
-                        return (0, true);
-                    return (result, false);
+                var maxVal = bitCount == 64 ? ulong.MaxValue : (1UL << bitCount) - 1;
+                var maxAdd = maxVal - value;
+
+                var neg = incrBy < 0;
+                //get absolute value of given increment
+                var absIncrBy = incrBy < 0 ? (ulong)(~incrBy) + 1UL : (ulong)incrBy;
+                //overflow if absolute increment is larger than diff of maxVal and current value
+                var overflow = (absIncrBy > maxAdd);
+                //underflow if absolute increment bigger than increment and increment is negative
+                var underflow = (absIncrBy > value) && neg;
+
+                var result = neg ? value - absIncrBy : value + absIncrBy;
+                var mask = maxVal;
+                result &= mask;
+                switch (overflowType)
+                {
+                    case (byte)BitFieldOverflow.WRAP:
+                        if (overflow || underflow)
+                            return (result, true);
+                        return (result, false);
+                    case (byte)BitFieldOverflow.SAT:
+                        if (overflow) return (maxVal, true);
+                        else if (underflow) return (0, true);
+                        return (result, false);
+                    case (byte)BitFieldOverflow.FAIL:
+                        if (overflow || underflow)
+                            return (0, true);
+                        return (result, false);
+                    default:
+                        throw new GarnetException("Invalid overflow type");
+                }
             }
-            return (0, true);
-        }
 
-        private static (long, bool) CheckSignedBitfieldOverflow(long value, long incrBy, byte bitCount, byte overflowType)
-        {
-            long signbit = 1L << (bitCount - 1);
-            long mask = bitCount == 64 ? -1 : (signbit - 1);
-
-            long result = (value + incrBy);
-            //if operands are both negative possibility for underflow
-            //underflow if sign bit is zero
-            bool underflow = (result & signbit) == 0 && value < 0 && incrBy < 0;
-            //if operands are both positive possibility of overflow
-            //overflow if any of the 64-bitcount most significant bits are set.
-            bool overflow = (ulong)(result & ~mask) > 0 && value >= 0 && incrBy > 0;
-
-            switch (overflowType)
+            static (long, bool) CheckSignedBitfieldOverflow(long value, long incrBy, byte bitCount, byte overflowType)
             {
-                case (byte)BitFieldOverflow.WRAP:
-                    if (underflow || overflow)
-                    {
-                        ulong res = (ulong)result;
-                        if (bitCount < 64)
+                var signbit = 1L << (bitCount - 1);
+                var mask = bitCount == 64 ? -1 : (signbit - 1);
+
+                var result = (value + incrBy);
+                //if operands are both negative possibility for underflow
+                //underflow if sign bit is zero
+                var underflow = (result & signbit) == 0 && value < 0 && incrBy < 0;
+                //if operands are both positive possibility of overflow
+                //overflow if any of the 64-bitcount most significant bits are set.
+                var overflow = (ulong)(result & ~mask) > 0 && value >= 0 && incrBy > 0;
+
+                switch (overflowType)
+                {
+                    case (byte)BitFieldOverflow.WRAP:
+                        if (underflow || overflow)
                         {
-                            ulong msb = (ulong)signbit;
-                            ulong smask = (ulong)mask;
-                            res = (res & msb) > 0 ? (res | ~smask) : (res & smask);
+                            var res = (ulong)result;
+                            if (bitCount < 64)
+                            {
+                                ulong msb = (ulong)signbit;
+                                ulong smask = (ulong)mask;
+                                res = (res & msb) > 0 ? (res | ~smask) : (res & smask);
+                            }
+                            return ((long)res, true);
                         }
-                        return ((long)res, true);
-                    }
-                    return (result, false);
-                case (byte)BitFieldOverflow.SAT:
-                    long maxVal = bitCount == 64 ? long.MaxValue : (signbit - 1);
-                    if (overflow) //overflow
-                    {
-                        return (maxVal, true);
-                    }
-                    else if (underflow) //underflow
-                    {
-                        long minVal = -maxVal - 1;
-                        return (minVal, true);
-                    }
-                    return (result, false);
-                case (byte)BitFieldOverflow.FAIL:
-                    if (underflow || overflow)
-                        return (0, true);
-                    return (result, false);
+                        return (result, false);
+                    case (byte)BitFieldOverflow.SAT:
+                        var maxVal = bitCount == 64 ? long.MaxValue : (signbit - 1);
+                        if (overflow) //overflow
+                        {
+                            return (maxVal, true);
+                        }
+                        else if (underflow) //underflow
+                        {
+                            var minVal = -maxVal - 1;
+                            return (minVal, true);
+                        }
+                        return (result, false);
+                    case (byte)BitFieldOverflow.FAIL:
+                        if (underflow || overflow)
+                            return (0, true);
+                        return (result, false);
+                    default:
+                        throw new GarnetException("Invalid overflow type");
+                }
             }
-            return (0, true);
         }
 
         /// <summary>
