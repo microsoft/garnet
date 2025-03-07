53a36f064771c6684295b8f71273da229af10ab8
Kevin Montrose <kmontrose@microsoft.com>
2025-03-07 10:27:13 -0500

    remove more bogus usings

--- libs/server/Resp/CmdStrings.cs
+++ libs/server/Resp/CmdStrings.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license.
 
 using System;
-using System.Collections.Generic;
 
 namespace Garnet.server
 {
--- test/Garnet.test/LuaScriptRunnerTests.cs
+++ test/Garnet.test/LuaScriptRunnerTests.cs
@@ -7,7 +7,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
 using Garnet.common;
 using Garnet.server;
