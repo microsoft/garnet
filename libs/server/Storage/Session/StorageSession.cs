62da62f715601b5976cd7ad6deb6ed3ac4acf4b3
Vijay-Nirmal <pon.vijaynirmal@outlook.com>
2025-03-12 11:56:16 +0530

    Review comment fix

--- libs/server/Storage/Session/StorageSession.cs
+++ libs/server/Storage/Session/StorageSession.cs
@@ -89,6 +89,8 @@ namespace Garnet.server
 
         public void Dispose()
         {
+            _zcollectTaskLock.WriteLock();
+
             sectorAlignedMemoryBitmap?.Dispose();
             basicContext.Session.Dispose();
             objectStoreBasicContext.Session?.Dispose();
