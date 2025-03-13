9865cb2f797faa9490da8e5f3637361bb4d6596b
Badrish Chandramouli <badrishc@microsoft.com>
2025-03-13 11:01:24 -0700

    Update description in index.js page

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-          description="Garnet is a new remote cache-store from Microsoft Research">
+          description="Garnet is a high-performance remote cache-store from Microsoft Research, that works with existing Redis clients.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
