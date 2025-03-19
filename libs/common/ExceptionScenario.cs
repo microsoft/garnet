0e18c0be295c44e386011a19b7b87f17eafdcbf3
Vasileios Zois <vazois@microsoft.com>
2025-03-18 19:50:50 -0700

    add exception test trigger

---
+++ libs/common/ExceptionScenario.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Garnet.common
+{
+    /// <summary>
+    /// Exception scenarios for testing
+    /// </summary>
+    public enum ExceptionScenario
+    {
+        FAIL_RIGHT_BEFORE_AOF_STREAM_STARTS,
+    }
+
+    /// <summary>
+    /// Exception scenario helper
+    /// </summary>
+    public static class ExceptionScenarioHelper
+    {
+        /// <summary>
+        /// Exception scenarios
+        /// </summary>
+        public static bool[] ExceptionScenarios =
+            Enum.GetValues<ExceptionScenario>().Select(debugScenarios => false).ToArray();
+
+        [Conditional("DEBUG")]
+        public static void EnableException(ExceptionScenario debugScenario) => ExceptionScenarios[(int)debugScenario] = true;
+
+        [Conditional("DEBUG")]
+        public static void DisableException(ExceptionScenario debugScenario) => ExceptionScenarios[(int)debugScenario] = false;
+
+        [Conditional("DEBUG")]
+        public static void TriggerException(ExceptionScenario debugScenario)
+        {
+            if (ExceptionScenarios[(int)debugScenario])
+                throw new GarnetException($"Debug scenario triggered {debugScenario}");
+        }
+    }
+}
