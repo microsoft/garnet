19c7f73a523fca7194cd27de8f3f8856f2fe8801
Vasileios Zois <vazois@microsoft.com>
2025-03-20 11:29:11 -0700

    comment for exception handling

--- libs/common/ExceptionScenario.cs
+++ libs/common/ExceptionScenario.cs
@@ -29,12 +29,25 @@ namespace Garnet.common
         public static bool[] ExceptionScenarios =
             Enum.GetValues<ExceptionScenario>().Select(debugScenarios => false).ToArray();
 
+        /// <summary>
+        /// Enable exception scenario (NOTE: enable at beginning of test to trigger the exception at runtime)
+        /// </summary>
+        /// <param name="debugScenario"></param>
         [Conditional("DEBUG")]
         public static void EnableException(ExceptionScenario debugScenario) => ExceptionScenarios[(int)debugScenario] = true;
 
+        /// <summary>
+        /// Disable exception scenario (NOTE: for tests you need to always call disable at the end of the test to avoid breaking other tests in the line)
+        /// </summary>
+        /// <param name="debugScenario"></param>
         [Conditional("DEBUG")]
         public static void DisableException(ExceptionScenario debugScenario) => ExceptionScenarios[(int)debugScenario] = false;
 
+        /// <summary>
+        /// Trigger exception scenario (NOTE: add this to the location where the exception should be emulated/triggered)
+        /// </summary>
+        /// <param name="debugScenario"></param>
+        /// <exception cref="GarnetException"></exception>
         [Conditional("DEBUG")]
         public static void TriggerException(ExceptionScenario debugScenario)
         {
