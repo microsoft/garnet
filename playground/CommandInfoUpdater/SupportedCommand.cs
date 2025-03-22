d8a41f45c13c92f5ab7e12cc0d6c428e13d44002
Aneesh Raman <aneeshr@bu.edu>
2025-03-22 14:26:02 -0400

    added command to RespCommandsInfo

--- playground/CommandInfoUpdater/SupportedCommand.cs
+++ playground/CommandInfoUpdater/SupportedCommand.cs
@@ -304,6 +304,7 @@ namespace CommandInfoUpdater
             new("WATCHMS", RespCommand.WATCHMS),
             new("WATCHOS", RespCommand.WATCHOS),
             new("XADD", RespCommand.XADD),
+            new("XLEN", RespCommand.XLEN),
             new("ZADD", RespCommand.ZADD),
             new("ZCARD", RespCommand.ZCARD),
             new("ZCOUNT", RespCommand.ZCOUNT),
