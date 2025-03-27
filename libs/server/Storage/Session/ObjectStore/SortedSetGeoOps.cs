5e53f12c7cb5b547ca74258d3125eeab6056f7d9
prvyk <52283326+prvyk@users.noreply.github.com>
2025-03-28 00:18:31 +0200

    Implement GEOSEARCH BYRADIUS. (#1129)
    Implement also ASC, DESC orderings, ANY, WITHHASH and FROMLONLAT.
    Implement GEORADIUS_RO and GEORADIUSBYMEMBER_RO.
    Implement GEORADIUS and GEORADIUSBYMEMBER.
    Fix GEOSEARCHSTORE without STOREDIST and other bugs.
    Add tests.
    
    Co-authored-by: prvyk <github@privatemail.fastmailbox.net>
    Co-authored-by: Tal Zaccai <talzacc@microsoft.com>

--- libs/server/Storage/Session/ObjectStore/SortedSetGeoOps.cs
+++ libs/server/Storage/Session/ObjectStore/SortedSetGeoOps.cs
@@ -32,7 +32,6 @@ namespace Garnet.server
         /// GEOHASH: Returns valid Geohash strings representing the position of one or more elements in a geospatial data of the sorted set.
         /// GEODIST: Returns the distance between two members in the geospatial index represented by the sorted set.
         /// GEOPOS: Returns the positions (longitude,latitude) of all the specified members in the sorted set.
-        /// GEOSEARCH: Returns the members of a sorted set populated with geospatial data, which are within the borders of the area specified by a given shape.
         /// </summary>
         /// <typeparam name="TObjectContext"></typeparam>
         /// <param name="key"></param>
@@ -45,16 +44,80 @@ namespace Garnet.server
             => ReadObjectStoreOperationWithOutput(key, ref input, ref objectContext, ref outputFooter);
 
         /// <summary>
-        /// Geospatial search and store in destination key.
+        /// Geospatial search and return result..
+        /// GEOSEARCH: Returns the members of a sorted set populated with geospatial data, which are within the borders of the area specified by a given shape.
+        /// GEORADIUS (read variant): Return the members of a sorted set populated with geospatial data, which are inside the circular area delimited by center and radius.
+        /// GEORADIUS_RO: Return the members of a sorted set populated with geospatial data, which are inside the circular area delimited by center and radius.
+        /// GEORADIUSBYMEMBER (read variant): Return the members of a sorted set populated with geospatial data, which are inside the circular area delimited by center (derived from member) and radius.
+        /// GEORADIUSBYMEMBER_RO: Return the members of a sorted set populated with geospatial data, which are inside the circular area delimited by center (derived from member) and radius.
         /// </summary>
         /// <typeparam name="TObjectContext"></typeparam>
         /// <param name="key"></param>
-        /// <param name="destination"></param>
+        /// <param name="opts"></param>
         /// <param name="input"></param>
         /// <param name="output"></param>
         /// <param name="objectContext"></param>
         /// <returns></returns>
-        public unsafe GarnetStatus GeoSearchStore<TObjectContext>(ArgSlice key, ArgSlice destination, ref ObjectInput input, ref SpanByteAndMemory output, ref TObjectContext objectContext)
+        public GarnetStatus GeoSearchReadOnly<TObjectContext>(ArgSlice key, ref GeoSearchOptions opts,
+                                                      ref ObjectInput input,
+                                                      ref SpanByteAndMemory output,
+                                                      ref TObjectContext objectContext)
+          where TObjectContext : ITsavoriteContext<byte[], IGarnetObject, ObjectInput, GarnetObjectStoreOutput, long, ObjectSessionFunctions, ObjectStoreFunctions, ObjectStoreAllocator>
+        {
+            var createTransaction = false;
+
+            if (txnManager.state != TxnState.Running)
+            {
+                Debug.Assert(txnManager.state == TxnState.None);
+                createTransaction = true;
+                txnManager.SaveKeyEntryToLock(key, true, LockType.Shared);
+                txnManager.Run(true);
+            }
+
+            try
+            {
+                // Can we optimize more when ANY is used?
+                var statusOp = GET(key.ToArray(), out var firstObj, ref objectContext);
+                if (statusOp == GarnetStatus.OK)
+                {
+                    if (firstObj.GarnetObject is not SortedSetObject firstSortedSet)
+                    {
+                        return GarnetStatus.WRONGTYPE;
+                    }
+
+                    firstSortedSet.GeoSearch(ref input, ref output, ref opts, true);
+
+                    return GarnetStatus.OK;
+                }
+
+                return GarnetStatus.NOTFOUND;
+            }
+            finally
+            {
+                if (createTransaction)
+                    txnManager.Commit(true);
+            }
+        }
+
+        /// <summary>
+        /// Geospatial search and store in destination key.
+        /// GEOSEARCHSTORE: Store the the members of a sorted set populated with geospatial data, which are within the borders of the area specified by a given shape.
+        /// GEORADIUS (write variant): Store the members of a sorted set populated with geospatial data, which are inside the circular area delimited by center and radius.
+        /// GEORADIUSBYMEMBER (write variant): Store the members of a sorted set populated with geospatial data, which are inside the circular area delimited by center (derived from member) and radius.
+        /// </summary>
+        /// <typeparam name="TObjectContext"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="destination"></param>
+        /// <param name="opts"></param>
+        /// <param name="input"></param>
+        /// <param name="output"></param>
+        /// <param name="objectContext"></param>
+        /// <returns></returns>
+        public unsafe GarnetStatus GeoSearchStore<TObjectContext>(ArgSlice key, ArgSlice destination,
+                                                                  ref GeoSearchOptions opts,
+                                                                  ref ObjectInput input,
+                                                                  ref SpanByteAndMemory output,
+                                                                  ref TObjectContext objectContext)
           where TObjectContext : ITsavoriteContext<byte[], IGarnetObject, ObjectInput, GarnetObjectStoreOutput, long, ObjectSessionFunctions, ObjectStoreFunctions, ObjectStoreAllocator>
         {
             var createTransaction = false;
@@ -77,11 +140,20 @@ namespace Garnet.server
 
             try
             {
-                var sourceKey = key.ToArray();
                 SpanByteAndMemory searchOutMem = default;
-                var searchOut = new GarnetObjectStoreOutput { SpanByteAndMemory = searchOutMem };
-                var status = GeoCommands(sourceKey, ref input, ref searchOut, ref objectStoreLockableContext);
-                searchOutMem = searchOut.SpanByteAndMemory;
+
+                var status = GET(key.ToArray(), out var firstObj, ref objectStoreLockableContext);
+                if (status == GarnetStatus.OK)
+                {
+                    if (firstObj.GarnetObject is SortedSetObject firstSortedSet)
+                    {
+                        firstSortedSet.GeoSearch(ref input, ref searchOutMem, ref opts, false);
+                    }
+                    else
+                    {
+                        status = GarnetStatus.WRONGTYPE;
+                    }
+                }
 
                 if (status == GarnetStatus.WRONGTYPE)
                 {
