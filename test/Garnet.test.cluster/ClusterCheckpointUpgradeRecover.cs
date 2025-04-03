f5f2bda786878cec268c350c79b8de7b03d34192
Vasileios Zois <vazois@microsoft.com>
2025-04-03 10:24:36 -0700

    update test for checkpoint across modes recovery

--- test/Garnet.test.cluster/ClusterCheckpointUpgradeRecover.cs
+++ test/Garnet.test.cluster/ClusterCheckpointUpgradeRecover.cs
@@ -29,14 +29,14 @@ namespace Garnet.test.cluster
         }
 
         //[Test, Order(1)]
-        public void ClusterCheckpointClusterToStandalone([Values] bool clusterMode)
+        public void ClusterCheckpointAcrossModesTest([Values] bool clusterToStandalone)
         {
             // startup in cluster or standalone mode
-            context.CreateInstances(1, enableCluster: clusterMode);
+            context.CreateInstances(1, enableCluster: clusterToStandalone);
             context.CreateConnection();
 
             // If cluster mode assign slots
-            if (clusterMode)
+            if (clusterToStandalone)
                 ClassicAssert.AreEqual("OK", context.clusterTestUtils.AddDelSlotsRange(0, [(0, 16383)], addslot: true, context.logger));
 
             var keyLength = 32;
@@ -50,16 +50,24 @@ namespace Garnet.test.cluster
 
             context.nodes[0].Dispose(false);
             // Restart in standalone or cluster mode
-            context.nodes[0] = context.CreateInstance(context.clusterTestUtils.GetEndPoint(0), enableCluster: !clusterMode, tryRecover: true);
+            context.nodes[0] = context.CreateInstance(context.clusterTestUtils.GetEndPoint(0), enableCluster: !clusterToStandalone, tryRecover: true);
             context.nodes[0].Start();
             context.CreateConnection();
 
             // Assign slot if started initially in standalone
-            if (!clusterMode)
+            if (!clusterToStandalone)
                 ClassicAssert.AreEqual("OK", context.clusterTestUtils.AddDelSlotsRange(0, [(0, 16383)], addslot: true, context.logger));
 
             context.clusterTestUtils.PingAll(logger: context.logger);
-            context.ValidateKVCollectionAgainstReplica(ref kvPairs, 0);
+
+            var db = context.clusterTestUtils.GetDatabase();
+            foreach (var kv in kvPairs)
+            {
+                var key = kv.Key;
+                var value = kv.Value;
+                var dbValue = (int)db.StringGet(key);
+                ClassicAssert.AreEqual(value, dbValue);
+            }
         }
     }
 }
