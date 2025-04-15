efd317eb66ffb707a5885ee613876978ad64321c
github-action-benchmark <github@users.noreply.github.com>
2025-04-15 00:06:20 +0000

    add Operations.ObjectOperations (windows-latest  net9.0 Release) (benchmarkdotnet) benchmark result for 14fb5f9204fa2a0483d71042b180c2df1a562c78

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744675577147,
+  "lastUpdate": 1744675580063,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Network.BasicOperations (ubuntu-latest  net9.0 Release)": [
@@ -1848,6 +1848,50 @@ window.BENCHMARK_DATA = {
             "value": 123865.20867919922,
             "unit": "ns",
             "range": "± 255.1185293702637"
+          }
+        ]
+      }
+    ],
+    "Operations.ObjectOperations (windows-latest  net9.0 Release)": [
+      {
+        "commit": {
+          "author": {
+            "email": "96085550+vazois@users.noreply.github.com",
+            "name": "Vasileios Zois",
+            "username": "vazois"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "14fb5f9204fa2a0483d71042b180c2df1a562c78",
+          "message": "Fix Announce Regression (#1171)\n\n* explicit port declarationg at get endpoint when any is used\n\n* add unit test for IPAddres.Any cluster announce\n\n* change version",
+          "timestamp": "2025-04-14T16:57:09-07:00",
+          "tree_id": "b69af006462aaf52e49267dcc41210d368a6a77e",
+          "url": "https://github.com/microsoft/garnet/commit/14fb5f9204fa2a0483d71042b180c2df1a562c78"
+        },
+        "date": 1744675364510,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.LPushPop(Params: ACL)",
+            "value": 96143.82760184152,
+            "unit": "ns",
+            "range": "± 150.57875253351585"
+          },
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.LPushPop(Params: AOF)",
+            "value": 109324.47591145833,
+            "unit": "ns",
+            "range": "± 1212.1080983855952"
+          },
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.LPushPop(Params: None)",
+            "value": 93924.93204752605,
+            "unit": "ns",
+            "range": "± 252.08202611024305"
           }
         ]
       }
