7304dceb15be46b73d28f315f97c77946556b1d3
prvyk <github@privatemail.fastmailbox.net>
2025-04-16 17:02:05 +0300

    Extract out RespMemoryWriter. This functionality is useful beyond the server.

--- libs/server/Objects/Types/GarnetObjectStoreRespOutput.cs
+++ libs/server/Objects/Types/GarnetObjectStoreRespOutput.cs
@@ -2,189 +2,72 @@
 // Licensed under the MIT license.
 
 using System;
-using System.Buffers;
 using System.Runtime.CompilerServices;
 using Garnet.common;
 using Tsavorite.core;
 
 namespace Garnet.server
 {
-    /// <summary>
-    /// Manage RESP output to SpanByteAndMemory.
-    /// </summary>
     internal unsafe ref struct GarnetObjectStoreRespOutput : IDisposable
     {
-        byte* curr;
-        byte* end;
-        byte* ptr;
-        MemoryHandle ptrHandle;
-        ref SpanByteAndMemory output;
+        RespMemoryWriter writer;
         ObjectOutputHeader outputHeader;
-        bool isMemory;
-        readonly bool resp3;
-        readonly bool skipOutputHeader;
 
-        internal unsafe GarnetObjectStoreRespOutput(byte respVersion, ref SpanByteAndMemory output, bool _skipOutputHeader = false)
+        internal unsafe GarnetObjectStoreRespOutput(byte respVersion, ref SpanByteAndMemory output)
         {
-            this.output = ref output;
-            resp3 = respVersion >= 3;
-            ptr = output.SpanByte.ToPointer();
-            curr = ptr;
-            end = curr + output.Length;
-            skipOutputHeader = _skipOutputHeader;
+            writer = new RespMemoryWriter(respVersion, ref output);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void WriteAsciiBulkString(string bulkString)
-        {
-            while (!RespWriteUtils.TryWriteAsciiBulkString(bulkString, ref curr, end))
-                ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr, ref end);
-        }
+        internal void WriteAsciiBulkString(string bulkString) => writer.WriteAsciiBulkString(bulkString);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void WriteAsciiDirect(ReadOnlySpan<char> asciiString)
-        {
-            while (!RespWriteUtils.TryWriteAsciiDirect(asciiString, ref curr, end))
-                ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr, ref end);
-        }
+        internal void WriteAsciiDirect(ReadOnlySpan<char> asciiString) => writer.WriteAsciiDirect(asciiString);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void WriteArrayItem(long item)
-        {
-            while (!RespWriteUtils.TryWriteArrayItem(item, ref curr, end))
-                ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr, ref end);
-        }
+        internal void WriteArrayItem(long item) => writer.WriteArrayItem(item);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void WriteArrayLength(int len)
-        {
-            while (!RespWriteUtils.TryWriteArrayLength(len, ref curr, end))
-                ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr, ref end);
-        }
+        internal void WriteArrayLength(int len) => writer.WriteArrayLength(len);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void WriteBulkString(ReadOnlySpan<byte> item)
-        {
-            while (!RespWriteUtils.TryWriteBulkString(item, ref curr, end))
-                ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr, ref end);
-        }
+        internal void WriteBulkString(ReadOnlySpan<byte> item) => writer.WriteBulkString(item);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void WriteDirect(ReadOnlySpan<byte> bytes)
-        {
-            while (!RespWriteUtils.TryWriteDirect(bytes, ref curr, end))
-                ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr, ref end);
-        }
+        internal void WriteDirect(ReadOnlySpan<byte> span) => writer.WriteDirect(span);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void WriteDoubleBulkString(double value)
-        {
-            while (!RespWriteUtils.TryWriteDoubleBulkString(value, ref curr, end))
-                ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr, ref end);
-        }
+        internal void WriteDoubleBulkString(double value) => writer.WriteDoubleBulkString(value);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void WriteDoubleNumeric(double value)
-        {
-            if (resp3)
-            {
-                while (!RespWriteUtils.TryWriteDoubleNumeric(value, ref curr, end))
-                    ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr, ref end);
-            }
-            else
-            {
-                while (!RespWriteUtils.TryWriteDoubleBulkString(value, ref curr, end))
-                    ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr, ref end);
-            }
-        }
+        internal void WriteDoubleNumeric(double value) => writer.WriteDoubleNumeric(value);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void WriteEmptyArray()
-        {
-            while (!RespWriteUtils.TryWriteEmptyArray(ref curr, end))
-                ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr, ref end);
-        }
+        internal void WriteEmptyArray() => writer.WriteEmptyArray();
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void WriteError(ReadOnlySpan<byte> error)
-        {
-            while (!RespWriteUtils.TryWriteError(error, ref curr, end))
-                ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr, ref end);
-        }
+        internal void WriteError(ReadOnlySpan<byte> errorString) => writer.WriteError(errorString);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void WriteInt32(int result)
-        {
-            while (!RespWriteUtils.TryWriteInt32(result, ref curr, end))
-                ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr, ref end);
-        }
+        internal void WriteInt32(int value) => writer.WriteInt32(value);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void WriteInt64(long result)
-        {
-            while (!RespWriteUtils.TryWriteInt64(result, ref curr, end))
-                ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr, ref end);
-        }
+        internal void WriteInt64(long value) => writer.WriteInt64(value);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void WriteIntegerFromBytes(byte[] resultBytes)
-        {
-            while (!RespWriteUtils.TryWriteIntegerFromBytes(resultBytes, ref curr, end))
-                ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr,
-                    ref end);
-        }
+        internal void WriteIntegerFromBytes(byte[] resultBytes) => writer.WriteIntegerFromBytes(resultBytes);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void WriteMapLength(int len)
-        {
-            if (resp3)
-            {
-                while (!RespWriteUtils.TryWriteMapLength(len, ref curr, end))
-                    ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr, ref end);
-            }
-            else
-            {
-                while (!RespWriteUtils.TryWriteArrayLength(len * 2, ref curr, end))
-                    ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr, ref end);
-            }
-        }
+        internal void WriteMapLength(int len) => writer.WriteMapLength(len);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void WriteNull()
-        {
-            if (resp3)
-            {
-                while (!RespWriteUtils.TryWriteResp3Null(ref curr, end))
-                    ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr, ref end);
-            }
-            else
-            {
-                while (!RespWriteUtils.TryWriteNull(ref curr, end))
-                    ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr, ref end);
-            }
-        }
+        internal void WriteNull() => writer.WriteNull();
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void WriteNullArray()
-        {
-            while (!RespWriteUtils.TryWriteNullArray(ref curr, end))
-                ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr, ref end);
-        }
+        internal void WriteNullArray() => writer.WriteNullArray();
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void WriteSetLength(int len)
-        {
-            if (resp3)
-            {
-                while (!RespWriteUtils.TryWriteSetLength(len, ref curr, end))
-                    ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr, ref end);
-            }
-            else
-            {
-                while (!RespWriteUtils.TryWriteArrayLength(len, ref curr, end))
-                    ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr, ref end);
-            }
-        }
+        internal void WriteSetLength(int len) => writer.WriteSetLength(len);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal void IncResult1()
@@ -200,14 +83,10 @@ namespace Garnet.server
 
         public void Dispose()
         {
-            if (!skipOutputHeader)
-            {
-                while (!RespWriteUtils.TryWriteDirect(ref outputHeader, ref curr, end))
-                    ObjectUtils.ReallocateOutput(ref output, ref isMemory, ref ptr, ref ptrHandle, ref curr, ref end);
-            }
+            while (!RespWriteUtils.TryWriteDirect(ref outputHeader, ref writer.curr, writer.end))
+                ObjectUtils.ReallocateOutput(ref writer.output, ref writer.isMemory, ref writer.ptr, ref writer.ptrHandle, ref writer.curr, ref writer.end);
 
-            if (isMemory) ptrHandle.Dispose();
-            output.Length = (int)(curr - ptr);
+            writer.Dispose();
         }
     }
 }
