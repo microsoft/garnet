02d082a00dabbc226ecb4fad87c9c6091bc6d615
github-action-benchmark <github@users.noreply.github.com>
2025-04-16 02:19:12 +0000

    add Operations.CustomOperations (ubuntu-latest  net8.0 Release) (benchmarkdotnet) benchmark result for 8ae8fb42c702ca80605195947629be3598d590af

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744769945338,
+  "lastUpdate": 1744769952604,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Network.BasicOperations (ubuntu-latest  net9.0 Release)": [
@@ -3872,6 +3872,102 @@ window.BENCHMARK_DATA = {
             "value": 77149.34142194476,
             "unit": "ns",
             "range": "± 261.63015475701604"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pon.vijaynirmal@outlook.com",
+            "name": "Vijay Nirmal",
+            "username": "Vijay-Nirmal"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8ae8fb42c702ca80605195947629be3598d590af",
+          "message": "Fix wrong type and non existing key issues in S/Z UNION, DIFF, INTER commands (#1162)\n\n* Fix wrong type and non existing key issues in S/Z UNION, DIFF, INTER commants\n\n* Fixed review comment\n\n---------\n\nCo-authored-by: Badrish Chandramouli <badrishc@microsoft.com>",
+          "timestamp": "2025-04-15T19:09:29-07:00",
+          "tree_id": "6b771dd93a997a56b2e58858648cbad33b02cee3",
+          "url": "https://github.com/microsoft/garnet/commit/8ae8fb42c702ca80605195947629be3598d590af"
+        },
+        "date": 1744769750769,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.CustomOperations.CustomRawStringCommand(Params: ACL)",
+            "value": 36410.09276529948,
+            "unit": "ns",
+            "range": "± 285.8552751764316"
+          },
+          {
+            "name": "BDN.benchmark.Operations.CustomOperations.CustomObjectCommand(Params: ACL)",
+            "value": 161856.70451472356,
+            "unit": "ns",
+            "range": "± 588.5518940885396"
+          },
+          {
+            "name": "BDN.benchmark.Operations.CustomOperations.CustomTransaction(Params: ACL)",
+            "value": 112717.01393345425,
+            "unit": "ns",
+            "range": "± 702.8858668595357"
+          },
+          {
+            "name": "BDN.benchmark.Operations.CustomOperations.CustomProcedure(Params: ACL)",
+            "value": 78445.12591552734,
+            "unit": "ns",
+            "range": "± 53.6552989642733"
+          },
+          {
+            "name": "BDN.benchmark.Operations.CustomOperations.CustomRawStringCommand(Params: AOF)",
+            "value": 33203.40789358957,
+            "unit": "ns",
+            "range": "± 290.30018024634546"
+          },
+          {
+            "name": "BDN.benchmark.Operations.CustomOperations.CustomObjectCommand(Params: AOF)",
+            "value": 171323.66598307292,
+            "unit": "ns",
+            "range": "± 1442.6188497212759"
+          },
+          {
+            "name": "BDN.benchmark.Operations.CustomOperations.CustomTransaction(Params: AOF)",
+            "value": 122716.3314115084,
+            "unit": "ns",
+            "range": "± 653.4109793850155"
+          },
+          {
+            "name": "BDN.benchmark.Operations.CustomOperations.CustomProcedure(Params: AOF)",
+            "value": 106825.11704799107,
+            "unit": "ns",
+            "range": "± 559.2761275413133"
+          },
+          {
+            "name": "BDN.benchmark.Operations.CustomOperations.CustomRawStringCommand(Params: None)",
+            "value": 33731.71775465745,
+            "unit": "ns",
+            "range": "± 181.29686140913256"
+          },
+          {
+            "name": "BDN.benchmark.Operations.CustomOperations.CustomObjectCommand(Params: None)",
+            "value": 168434.62877604167,
+            "unit": "ns",
+            "range": "± 743.904660507621"
+          },
+          {
+            "name": "BDN.benchmark.Operations.CustomOperations.CustomTransaction(Params: None)",
+            "value": 111833.47737339565,
+            "unit": "ns",
+            "range": "± 485.9837658209682"
+          },
+          {
+            "name": "BDN.benchmark.Operations.CustomOperations.CustomProcedure(Params: None)",
+            "value": 77977.62018291767,
+            "unit": "ns",
+            "range": "± 74.39539547704118"
           }
         ]
       }
