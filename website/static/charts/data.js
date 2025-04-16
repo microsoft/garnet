1308f6c637a535ef65fc277a932bbb652690eaf1
github-action-benchmark <github@users.noreply.github.com>
2025-04-16 02:15:52 +0000

    add Operations.PubSubOperations (ubuntu-latest  net8.0 Release) (benchmarkdotnet) benchmark result for 8ae8fb42c702ca80605195947629be3598d590af

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744769751613,
+  "lastUpdate": 1744769752719,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Network.BasicOperations (ubuntu-latest  net9.0 Release)": [
@@ -1280,6 +1280,48 @@ window.BENCHMARK_DATA = {
             "value": 18296.79940678523,
             "unit": "ns",
             "range": "± 73.46771770923436"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pon.vijaynirmal@outlook.com",
+            "name": "Vijay Nirmal",
+            "username": "Vijay-Nirmal"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8ae8fb42c702ca80605195947629be3598d590af",
+          "message": "Fix wrong type and non existing key issues in S/Z UNION, DIFF, INTER commands (#1162)\n\n* Fix wrong type and non existing key issues in S/Z UNION, DIFF, INTER commants\n\n* Fixed review comment\n\n---------\n\nCo-authored-by: Badrish Chandramouli <badrishc@microsoft.com>",
+          "timestamp": "2025-04-15T19:09:29-07:00",
+          "tree_id": "6b771dd93a997a56b2e58858648cbad33b02cee3",
+          "url": "https://github.com/microsoft/garnet/commit/8ae8fb42c702ca80605195947629be3598d590af"
+        },
+        "date": 1744769563462,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: ACL)",
+            "value": 18261.523419893703,
+            "unit": "ns",
+            "range": "± 13.859856952478884"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: AOF)",
+            "value": 18922.517920939128,
+            "unit": "ns",
+            "range": "± 197.1157005725388"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: None)",
+            "value": 18226.669254847937,
+            "unit": "ns",
+            "range": "± 64.48506671794456"
           }
         ]
       }
