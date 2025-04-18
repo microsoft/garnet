8bd995fc772a397a11eb4a71ca4d91c810264559
github-action-benchmark <github@users.noreply.github.com>
2025-04-18 02:44:49 +0000

    add Network.BasicOperations (windows-latest  net9.0 Release) (benchmarkdotnet) benchmark result for 06bf4e5621459ecf6ea8ea41f075d7578d8abdb1

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744944286150,
+  "lastUpdate": 1744944288755,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Network.BasicOperations (ubuntu-latest  net9.0 Release)": [
@@ -6420,6 +6420,36 @@ window.BENCHMARK_DATA = {
             "value": 70.71429916790554,
             "unit": "ns",
             "range": "± 0.16148988625351726"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kmontrose@microsoft.com",
+            "name": "kevin-montrose",
+            "username": "kevin-montrose"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "06bf4e5621459ecf6ea8ea41f075d7578d8abdb1",
+          "message": "fixes for fuzzers, some assumptions about pinning were not held (#1174)",
+          "timestamp": "2025-04-17T22:36:27-04:00",
+          "tree_id": "ef3c66076a04b5be96bad8973f36f1071fc7fbb2",
+          "url": "https://github.com/microsoft/garnet/commit/06bf4e5621459ecf6ea8ea41f075d7578d8abdb1"
+        },
+        "date": 1744944070576,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Network.BasicOperations.InlinePing(Params: None)",
+            "value": 73.68347724278767,
+            "unit": "ns",
+            "range": "± 0.1756854324353271"
           }
         ]
       }
