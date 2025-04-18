3348d264e07e1d0a4db67993965929dff974ce56
github-action-benchmark <github@users.noreply.github.com>
2025-04-18 02:43:07 +0000

    add Operations.PubSubOperations (ubuntu-latest  net9.0 Release) (benchmarkdotnet) benchmark result for 06bf4e5621459ecf6ea8ea41f075d7578d8abdb1

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744944186243,
+  "lastUpdate": 1744944187736,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Network.BasicOperations (ubuntu-latest  net9.0 Release)": [
@@ -2728,6 +2728,48 @@ window.BENCHMARK_DATA = {
             "value": 18208.06346482497,
             "unit": "ns",
             "range": "± 27.395604232769674"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kmontrose@microsoft.com",
+            "name": "kevin-montrose",
+            "username": "kevin-montrose"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "06bf4e5621459ecf6ea8ea41f075d7578d8abdb1",
+          "message": "fixes for fuzzers, some assumptions about pinning were not held (#1174)",
+          "timestamp": "2025-04-17T22:36:27-04:00",
+          "tree_id": "ef3c66076a04b5be96bad8973f36f1071fc7fbb2",
+          "url": "https://github.com/microsoft/garnet/commit/06bf4e5621459ecf6ea8ea41f075d7578d8abdb1"
+        },
+        "date": 1744943991531,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: ACL)",
+            "value": 17634.664217267717,
+            "unit": "ns",
+            "range": "± 33.74460267359028"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: AOF)",
+            "value": 17382.306897844588,
+            "unit": "ns",
+            "range": "± 47.556216516603065"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: None)",
+            "value": 17771.63969203404,
+            "unit": "ns",
+            "range": "± 70.08845579285453"
           }
         ]
       }
