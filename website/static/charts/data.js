7a90e6c49420f6a80aec9979d12eb1249a95d47c
github-action-benchmark <github@users.noreply.github.com>
2025-04-20 14:48:57 +0000

    add Network.BasicOperations (ubuntu-latest  net9.0 Release) (benchmarkdotnet) benchmark result for 83b202a339c73fad64918d3f85b5f4ef642f6df8

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744946302339,
+  "lastUpdate": 1745160536930,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Network.BasicOperations (ubuntu-latest  net9.0 Release)": [
@@ -150,6 +150,36 @@ window.BENCHMARK_DATA = {
             "value": 83.42774923031146,
             "unit": "ns",
             "range": "± 0.3581940411320223"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nightroman@gmail.com",
+            "name": "Roman Kuzmin",
+            "username": "nightroman"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "83b202a339c73fad64918d3f85b5f4ef642f6df8",
+          "message": "Docs: use plus signs for commands with links. (#1175)",
+          "timestamp": "2025-04-20T07:42:51-07:00",
+          "tree_id": "1697ee5b26ded92e3d98070bacca54bc950e95c7",
+          "url": "https://github.com/microsoft/garnet/commit/83b202a339c73fad64918d3f85b5f4ef642f6df8"
+        },
+        "date": 1745160334984,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Network.BasicOperations.InlinePing(Params: None)",
+            "value": 83.58128637075424,
+            "unit": "ns",
+            "range": "± 0.8201936379606094"
           }
         ]
       }
