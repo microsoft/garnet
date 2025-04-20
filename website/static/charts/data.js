27b4f9f2210ed93797020bc299ff1be05934ed02
github-action-benchmark <github@users.noreply.github.com>
2025-04-20 14:49:28 +0000

    add Operations.PubSubOperations (ubuntu-latest  net9.0 Release) (benchmarkdotnet) benchmark result for 83b202a339c73fad64918d3f85b5f4ef642f6df8

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745160564440,
+  "lastUpdate": 1745160568924,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Network.BasicOperations (ubuntu-latest  net9.0 Release)": [
@@ -3034,6 +3034,48 @@ window.BENCHMARK_DATA = {
             "value": 17771.63969203404,
             "unit": "ns",
             "range": "± 70.08845579285453"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nightroman@gmail.com",
+            "name": "Roman Kuzmin",
+            "username": "nightroman"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "83b202a339c73fad64918d3f85b5f4ef642f6df8",
+          "message": "Docs: use plus signs for commands with links. (#1175)",
+          "timestamp": "2025-04-20T07:42:51-07:00",
+          "tree_id": "1697ee5b26ded92e3d98070bacca54bc950e95c7",
+          "url": "https://github.com/microsoft/garnet/commit/83b202a339c73fad64918d3f85b5f4ef642f6df8"
+        },
+        "date": 1745160366749,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: ACL)",
+            "value": 18084.388381958008,
+            "unit": "ns",
+            "range": "± 35.35178454500974"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: AOF)",
+            "value": 18348.65657043457,
+            "unit": "ns",
+            "range": "± 79.24426814025827"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: None)",
+            "value": 18005.870815022787,
+            "unit": "ns",
+            "range": "± 65.80844778466434"
           }
         ]
       }
