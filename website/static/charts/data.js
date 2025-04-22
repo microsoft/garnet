cb9daa7f97e1b907ff899e19fb2c92afea43cf28
github-action-benchmark <github@users.noreply.github.com>
2025-04-22 19:46:46 +0000

    add Operations.PubSubOperations (windows-latest  net9.0 Release) (benchmarkdotnet) benchmark result for 38bb088e2011e5382be19c35c0d6bdf5dcd6c8e0

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745351175979,
+  "lastUpdate": 1745351206292,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Network.BasicOperations (ubuntu-latest  net9.0 Release)": [
@@ -9562,6 +9562,48 @@ window.BENCHMARK_DATA = {
             "value": 15160.089346078727,
             "unit": "ns",
             "range": "± 11.336394401536934"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "42720645+hamdaankhalid@users.noreply.github.com",
+            "name": "Hamdaan Khalid",
+            "username": "hamdaankhalid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "38bb088e2011e5382be19c35c0d6bdf5dcd6c8e0",
+          "message": "Bugfix Incorrect Etag increment on SETIFGREATER (#1177)\n\n* Bugfix Incorrect Etag increment on SETIFGREATER\n\n* Don't increment SETIFGREATER set ETags\n\n---------\n\nCo-authored-by: Hamdaan Khalid <hkhalid@microsoft.com>",
+          "timestamp": "2025-04-22T12:38:27-07:00",
+          "tree_id": "f056b59b4fa870a547fa7f11c64fdc6dbdba4853",
+          "url": "https://github.com/microsoft/garnet/commit/38bb088e2011e5382be19c35c0d6bdf5dcd6c8e0"
+        },
+        "date": 1745351014949,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: ACL)",
+            "value": 15522.526346842447,
+            "unit": "ns",
+            "range": "± 14.36261783428636"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: AOF)",
+            "value": 15212.256876627604,
+            "unit": "ns",
+            "range": "± 11.829370646255356"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: None)",
+            "value": 15892.955017089844,
+            "unit": "ns",
+            "range": "± 11.390958546773359"
           }
         ]
       }
