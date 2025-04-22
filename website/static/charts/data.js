7ec4e7bbcf1522ee89e9d9b063976b1c3c0419ff
github-action-benchmark <github@users.noreply.github.com>
2025-04-22 19:47:06 +0000

    add Cluster.ClusterMigrate (windows-latest  net9.0 Release) (benchmarkdotnet) benchmark result for 38bb088e2011e5382be19c35c0d6bdf5dcd6c8e0

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745351224131,
+  "lastUpdate": 1745351226544,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Network.BasicOperations (ubuntu-latest  net9.0 Release)": [
@@ -9308,6 +9308,54 @@ window.BENCHMARK_DATA = {
             "value": 30172.847696940105,
             "unit": "ns",
             "range": "± 66.51667820234931"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "42720645+hamdaankhalid@users.noreply.github.com",
+            "name": "Hamdaan Khalid",
+            "username": "hamdaankhalid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "38bb088e2011e5382be19c35c0d6bdf5dcd6c8e0",
+          "message": "Bugfix Incorrect Etag increment on SETIFGREATER (#1177)\n\n* Bugfix Incorrect Etag increment on SETIFGREATER\n\n* Don't increment SETIFGREATER set ETags\n\n---------\n\nCo-authored-by: Hamdaan Khalid <hkhalid@microsoft.com>",
+          "timestamp": "2025-04-22T12:38:27-07:00",
+          "tree_id": "f056b59b4fa870a547fa7f11c64fdc6dbdba4853",
+          "url": "https://github.com/microsoft/garnet/commit/38bb088e2011e5382be19c35c0d6bdf5dcd6c8e0"
+        },
+        "date": 1745351033488,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Cluster.ClusterMigrate.Get(Params: None)",
+            "value": 34849.77559407552,
+            "unit": "ns",
+            "range": "± 50.28127089638321"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterMigrate.Set(Params: None)",
+            "value": 35990.77970064603,
+            "unit": "ns",
+            "range": "± 68.21542000392708"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterMigrate.MGet(Params: None)",
+            "value": 31660.979755108172,
+            "unit": "ns",
+            "range": "± 28.214070385970842"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterMigrate.MSet(Params: None)",
+            "value": 30250.46630859375,
+            "unit": "ns",
+            "range": "± 24.882427104797948"
           }
         ]
       }
