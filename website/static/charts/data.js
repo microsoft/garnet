c66316bbe4787b9d3e33235a60f011ad93f8feed
github-action-benchmark <github@users.noreply.github.com>
2025-04-22 19:45:02 +0000

    add Operations.ObjectOperations (ubuntu-latest  net8.0 Release) (benchmarkdotnet) benchmark result for 38bb088e2011e5382be19c35c0d6bdf5dcd6c8e0

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745351097348,
+  "lastUpdate": 1745351102764,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Network.BasicOperations (ubuntu-latest  net9.0 Release)": [
@@ -2106,6 +2106,48 @@ window.BENCHMARK_DATA = {
             "value": 139790.11499023438,
             "unit": "ns",
             "range": "± 457.9221344001216"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "42720645+hamdaankhalid@users.noreply.github.com",
+            "name": "Hamdaan Khalid",
+            "username": "hamdaankhalid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "38bb088e2011e5382be19c35c0d6bdf5dcd6c8e0",
+          "message": "Bugfix Incorrect Etag increment on SETIFGREATER (#1177)\n\n* Bugfix Incorrect Etag increment on SETIFGREATER\n\n* Don't increment SETIFGREATER set ETags\n\n---------\n\nCo-authored-by: Hamdaan Khalid <hkhalid@microsoft.com>",
+          "timestamp": "2025-04-22T12:38:27-07:00",
+          "tree_id": "f056b59b4fa870a547fa7f11c64fdc6dbdba4853",
+          "url": "https://github.com/microsoft/garnet/commit/38bb088e2011e5382be19c35c0d6bdf5dcd6c8e0"
+        },
+        "date": 1745350912370,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.LPushPop(Params: ACL)",
+            "value": 136003.40053710938,
+            "unit": "ns",
+            "range": "± 1137.0275030106263"
+          },
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.LPushPop(Params: AOF)",
+            "value": 158303.2847202846,
+            "unit": "ns",
+            "range": "± 892.6611992446958"
+          },
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.LPushPop(Params: None)",
+            "value": 138448.62692057292,
+            "unit": "ns",
+            "range": "± 1167.618114458546"
           }
         ]
       }
