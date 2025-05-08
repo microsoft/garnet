4850861e6054862f5820013de4ccadf97c61f50d
Hamdaan Khalid <hkhalid@microsoft.com>
2025-05-07 23:36:52 -0700

    rm test

--- test/Garnet.test/RespLowMemoryTests.cs
+++ test/Garnet.test/RespLowMemoryTests.cs
@@ -85,49 +85,5 @@ namespace Garnet.test
             // Verify that key0 exists with correct value
             ClassicAssert.AreEqual(key0, (string)db.StringGet(key0));
         }
-
-
-        [Test]
-        public void RevivificationViaRmw()
-        {
-            using var redis = ConnectionMultiplexer.Connect(TestUtils.GetConfig(allowAdmin: true));
-            var db = redis.GetDatabase(0);
-            var server = redis.GetServer(TestUtils.EndPoint);
-            var info = TestUtils.GetStoreAddressInfo(server);
-
-            // Start at tail address of 64
-            ClassicAssert.AreEqual(64, info.TailAddress);
-
-            var expire = 100;
-            var key0 = $"key{0:00000}";
-            _ = db.StringSet(key0, key0, TimeSpan.FromSeconds(expire));
-
-            // Record size for key0 is 8 bytes header + 16 bytes key + 16 bytes value + 8 bytes expiry = 48 bytes
-            // so the new tail address should be 64 + 48 = 112
-            // That is, key0 is located at [64, 112)
-            info = TestUtils.GetStoreAddressInfo(server);
-            ClassicAssert.AreEqual(112, info.TailAddress);
-
-            // Make the record read-only by adding more records
-            MakeReadOnly(info.TailAddress, server, db);
-
-            info = TestUtils.GetStoreAddressInfo(server);
-            var previousTail = info.TailAddress;
-
-            // The first record inserted (key0) is now read-only
-            ClassicAssert.IsTrue(info.ReadOnlyAddress >= 112);
-
-            // Persist the key, which should cause RMW to CopyUpdate to tail
-            var response = db.KeyPersist(key0);
-            ClassicAssert.IsTrue(response);
-
-            // Now key0 is only 40 bytes, as we are removing the expiration
-            // That is, key0 is now moved to [previousTail, previousTail + 40)
-            info = TestUtils.GetStoreAddressInfo(server);
-            ClassicAssert.AreEqual(previousTail + 40, info.TailAddress);
-
-            // Verify that key0 exists with correct value
-            ClassicAssert.AreEqual(key0, (string)db.StringGet(key0));
-        }
     }
 }
