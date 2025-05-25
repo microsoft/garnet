e9bfa2a79a5b43d8b3adc55ba20b1cf23a17bfec
prvyk <github@privatemail.fastmailbox.net>
2025-05-25 12:40:46 +0300

    Add verbatim string support. Emit RESP3 "txt" where appropriate. CLIENT INFO should emit Bulkstring in RESP2, not simple. Use this to add final newline to make output easier to read.

--- libs/server/Resp/BasicCommands.cs
+++ libs/server/Resp/BasicCommands.cs
@@ -1686,6 +1686,7 @@ namespace Garnet.server
             into.Append($" resp={resp}");
             into.Append($" lib-name={targetSession.clientLibName}");
             into.Append($" lib-ver={targetSession.clientLibVersion}");
+            into.AppendLine();
         }
 
         bool ParseGETAndKey(ref SpanByte key)
