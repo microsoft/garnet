10529feb7ece4658c3bb978e44f562dc2a49d0ab
Badrish Chandramouli <badrishc@microsoft.com>
2025-06-12 11:32:32 -0700

    updates

--- libs/server/GarnetCheckpointManager.cs
+++ libs/server/GarnetCheckpointManager.cs
@@ -9,24 +9,23 @@ using Tsavorite.core;
 namespace Garnet.server
 {
     /// <summary>
-    /// Log commit manager for a generic IDevice
+    /// Garnet checkpoint manager, inherits from Tsavorite's DeviceLogCommitCheckpointManager
     /// </summary>
     public class GarnetCheckpointManager : DeviceLogCommitCheckpointManager
     {
         public string CurrentHistoryId { get; set; }
         public string RecoveredHistoryId { get; set; }
-
         public long CurrentSafeAofAddress { get; set; }
         public long RecoveredSafeAofAddress { get; set; }
 
         /// <summary>
-        /// Create new instance of log commit manager
+        /// Create new instance of Garnet checkpoint manager
         /// </summary>
         /// <param name="deviceFactoryCreator">Factory for getting devices</param>
         /// <param name="checkpointNamingScheme">Checkpoint naming helper</param>
-        /// <param name="removeOutdated">Remote older Tsavorite log commits</param>
+        /// <param name="removeOutdated">Remove older Tsavorite log commits</param>
         /// <param name="fastCommitThrottleFreq">FastCommit throttle frequency - use only in FastCommit mode</param>
-        /// <param name="logger">Remote older Tsavorite log commits</param>
+        /// <param name="logger">Logger</param>
         public GarnetCheckpointManager(INamedDeviceFactoryCreator deviceFactoryCreator, ICheckpointNamingScheme checkpointNamingScheme, bool removeOutdated = true, int fastCommitThrottleFreq = 0, ILogger logger = null)
             : base(deviceFactoryCreator, checkpointNamingScheme, removeOutdated, fastCommitThrottleFreq, logger)
         {
@@ -36,6 +35,7 @@ namespace Garnet.server
             RecoveredSafeAofAddress = 0;
         }
 
+        /// <inheritdoc />
         public override unsafe byte[] GetCookie()
         {
             if (CurrentHistoryId == null) return null;
