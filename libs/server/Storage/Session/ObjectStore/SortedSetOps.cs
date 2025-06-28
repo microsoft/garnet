ce16a58ac2bcbf917c7ec349c73ca8f8c8e9e389
prvyk <github@privatemail.fastmailbox.net>
2025-06-28 23:31:11 +0300

    ZDIFF of a key against nonexisting keys should return the original contents. ZDIFF 1 zset should return contents of zset.

--- libs/server/Storage/Session/ObjectStore/SortedSetOps.cs
+++ libs/server/Storage/Session/ObjectStore/SortedSetOps.cs
@@ -1241,9 +1241,9 @@ namespace Garnet.server
                 return GarnetStatus.WRONGTYPE;
             }
 
+            pairs = SortedSetObject.CopyDiff(firstSortedSet, null);
             if (keys.Length == 1)
             {
-                pairs = firstSortedSet.Dictionary;
                 return GarnetStatus.OK;
             }
 
@@ -1262,10 +1262,7 @@ namespace Garnet.server
                     return GarnetStatus.WRONGTYPE;
                 }
 
-                if (pairs == default)
-                    pairs = SortedSetObject.CopyDiff(firstSortedSet, nextSortedSet);
-                else
-                    SortedSetObject.InPlaceDiff(pairs, nextSortedSet);
+                SortedSetObject.InPlaceDiff(pairs, nextSortedSet);
             }
 
             return GarnetStatus.OK;
