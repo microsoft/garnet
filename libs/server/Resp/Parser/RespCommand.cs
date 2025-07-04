03a66f38e4fc867015af383b9fd2d77772a193d6
prvyk <github@privatemail.fastmailbox.net>
2025-07-04 05:21:16 +0300

    Handle root cause of earleir test failure. Checked against those test that failed.
    Nonetheless, IMHO using input.length is more correct in GetOperationDirection so I feel it should be kept.

--- libs/server/Resp/Parser/RespCommand.cs
+++ libs/server/Resp/Parser/RespCommand.cs
@@ -2701,7 +2701,7 @@ namespace Garnet.server
                     }
                 }
 
-                readHead = (int)(ptr - recvBufferPtr);
+                endReadHead = (int)(ptr - recvBufferPtr);
             }
             else
             {
@@ -2739,9 +2739,9 @@ namespace Garnet.server
 
                 // Note that arguments are initialized from the actual command string, and not our made-up RESP string.
                 parseState.InitializeWithArguments(result[(result.Length - count)..]);
+                endReadHead = readHead;
             }
 
-            endReadHead = readHead;
             if (storeWrapper.serverOptions.EnableAOF && storeWrapper.serverOptions.WaitForCommit)
                 HandleAofCommitMode(cmd);
 
