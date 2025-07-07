94599e83b7a301b317489ffee3406e5949112ce5
Kevin Montrose <kmontrose@microsoft.com>
2025-07-07 13:23:34 -0400

    formatting

--- test/Garnet.test/LuaScriptTests.cs
+++ test/Garnet.test/LuaScriptTests.cs
@@ -225,13 +225,13 @@ namespace Garnet.test
             List<string> illegalToModify = ["_G", "bit", "cjson", "cmsgpack", "math", "os", "redis", "string", "struct", "table"];
             List<string> legalToModify = ["KEYS", "ARGV"];
 
-            foreach(var illegal in illegalToModify)
+            foreach (var illegal in illegalToModify)
             {
                 var exc = ClassicAssert.Throws<RedisServerException>(() => db.ScriptEvaluate($"table.insert({illegal}, 'foo')"));
                 ClassicAssert.IsTrue(exc.Message.Contains("Attempt to modify a readonly table"));
             }
 
-            foreach(var legal in legalToModify)
+            foreach (var legal in legalToModify)
             {
                 var res = (string[])db.ScriptEvaluate($"table.insert({legal}, 'fizz'); return {legal};");
                 ClassicAssert.IsTrue(res.Contains("fizz"));
