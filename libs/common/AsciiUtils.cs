9ddce0513e5a2d0713325e677d4ef550728caa76
prvyk <github@privatemail.fastmailbox.net>
2025-07-10 03:14:15 +0300

    Follow review suggestion.

--- libs/common/AsciiUtils.cs
+++ libs/common/AsciiUtils.cs
@@ -92,7 +92,7 @@ public static class AsciiUtils
     /// <summary>
     /// Check if two byte spans are equal, where right is an all-upper-case span, ignoring case if there are ASCII bytes.
     /// </summary>
-    public static bool EqualsUpperCaseSpanIgnoringCase(this ReadOnlySpan<byte> left, ReadOnlySpan<byte> right, bool allowAllPrintableCharacters)
+    public static bool EqualsUpperCaseSpanIgnoringCase(this ReadOnlySpan<byte> left, ReadOnlySpan<byte> right, bool allowNonAlphabeticChars)
     {
         if (left.SequenceEqual(right))
             return true;
@@ -104,17 +104,10 @@ public static class AsciiUtils
             var b1 = left[i];
             var b2 = right[i];
 
-            if ((allowAllPrintableCharacters) && ((b2 is < 65 and >= 32) || (b2 is < 127 and > 96)))
-            {
-                if (b1 == b2)
-                    continue;
+            if (!allowNonAlphabeticChars && b2 is < 65 or > 90)
                 return false;
-            }
 
-            // Debug assert that b2 is an upper case letter 'A'-'Z'
-            Debug.Assert(b2 is >= 65 and <= 90);
-
-            if (b1 == b2 || b1 - 32 == b2)
+            if (b1 == b2 || ((!allowNonAlphabeticChars || b2 is >= 65 and <= 90) && (b1 - 32 == b2)))
                 continue;
             return false;
         }
