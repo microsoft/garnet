5d7d958e3206e5751b10713dec93c670c66f822f
Kevin Montrose <kmontrose@microsoft.com>
2025-07-18 12:52:20 -0400

    cleanup

--- test/Garnet.test.cluster/ClusterRedirectTests.cs
+++ test/Garnet.test.cluster/ClusterRedirectTests.cs
@@ -1,10 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Text;
 using Garnet.common;
 using Microsoft.Extensions.Logging;
