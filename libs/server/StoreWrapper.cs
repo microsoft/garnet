d11b4f1618f8eddf1c3d3ca2b39fbf9bd09f4def
Kevin Montrose <kmontrose@microsoft.com>
2025-07-18 14:44:17 -0400

    extend role prevention changes to all ops on StoreApi; refactor a bit so we can express this with a using; save reusable instance of the locking object, acquire in one place

--- libs/server/StoreWrapper.cs
+++ libs/server/StoreWrapper.cs
@@ -483,19 +483,21 @@ namespace Garnet.server
         /// <summary>
         /// Wait for commits from all active databases
         /// </summary>
-        internal void WaitForCommit() =>
+        /// <returns>false if config prevents committing to AOF</returns>
+        internal bool WaitForCommit() =>
             WaitForCommitAsync().GetAwaiter().GetResult();
 
         /// <summary>
         /// Asynchronously wait for commits from all active databases
         /// </summary>
         /// <param name="token">Cancellation token</param>
-        /// <returns>ValueTask</returns>
-        internal async ValueTask WaitForCommitAsync(CancellationToken token = default)
+        /// <returns>false if commit is skipped for config reasons</returns>
+        internal async ValueTask<bool> WaitForCommitAsync(CancellationToken token = default)
         {
-            if (!serverOptions.EnableAOF) return;
+            if (!serverOptions.EnableAOF) return false;
 
             await databaseManager.WaitForCommitToAofAsync(token);
+            return true;
         }
 
         /// <summary>
@@ -504,21 +506,22 @@ namespace Garnet.server
         /// </summary>
         /// <param name="dbId">Specific database ID to commit AOF for (optional)</param>
         /// <param name="token">Cancellation token</param>
-        /// <returns>ValueTask</returns>
-        internal async ValueTask CommitAOFAsync(int dbId = -1, CancellationToken token = default)
+        /// <returns>false if commit is skipped for config reasons</returns>
+        internal async ValueTask<bool> CommitAOFAsync(int dbId = -1, CancellationToken token = default)
         {
-            if (!serverOptions.EnableAOF) return;
+            if (!serverOptions.EnableAOF) return false;
 
             if (dbId == -1)
             {
                 await databaseManager.CommitToAofAsync(token, logger);
-                return;
+                return true;
             }
 
             if (dbId != 0 && !CheckMultiDatabaseCompatibility())
                 throw new GarnetException($"Unable to call {nameof(databaseManager.CommitToAofAsync)} with DB ID: {dbId}");
 
             await databaseManager.CommitToAofAsync(dbId, token);
+            return true;
         }
 
         /// <summary>
