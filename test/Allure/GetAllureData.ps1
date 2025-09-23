31b4e4445ba7861b95b874556040d503167bbd1c
darrenge <darrenge@microsoft.com>
2025-09-23 16:08:09 -0700

    Scripts running successfully on local machine. Next step is to get them running in GH action

---
+++ test/Allure/GetAllureData.ps1
@@ -0,0 +1,56 @@
+#Requires -Version 7
+
+<#$f
+.SYNOPSIS
+    This script copies the Allure results into the single location of arctifacts so that all can be merged into one big report.
+#>
+
+param (
+    [string]$framework,
+    [string]$configuration,
+    [string]$testsuite
+)
+
+$OFS = "`r`n"
+
+Write-Host "Framework: $framework"
+Write-Host "Configuration: $configuration"
+Write-Host "Test Suite: $testsuite"
+
+# Get base path since paths can differ from machine to machine
+$pathstring = $pwd.Path
+if ($pathstring.Contains("test")) {
+    $position = $pathString.IndexOf("test")
+    $basePath = $pathstring.Substring(0,$position-1)  # take off slash off end as well
+} else {
+    $basePath = $pathstring  # already in base and not in test
+    Set-Location .\test\Allure\
+}
+
+# Don't need this yet
+$os = (Get-CimInstance -ClassName Win32_OperatingSystem).Caption
+Write-Host "Operating System: $os"
+
+# C:\AllureTestProject\AllureTestProject\AllureTests\CombinedResults -- might not be needed as will have combined results dir in artifacts
+$allureResultsCombinedDir = "$basePath/test/Allure/CombinedResults"
+
+# Create CombinedResults dir if it doesn't exist
+if (-not (Test-Path -Path $allureResultsCombinedDir)) {
+    New-Item -Path $allureResultsCombinedDir -ItemType Directory
+}
+
+# Copy all the results from the test directory to the CombinedResults directory
+# $sourceAllureResultsDir = "$basePath/test/$testsuite/bin/$configuration/$framework/allure-results"   # <-- will use this one when we have real data
+$sourceAllureResultsDir = "$basePath/test/Allure/TestData/$testsuite/bin/$configuration/$framework/allure-results"   # <--- this is just using the test data that is checked in because real data not available yet
+if (Test-Path -Path $sourceAllureResultsDir) {
+    Write-Host "Copying Allure results from $sourceAllureResultsDir to $allureResultsCombinedDir"
+    Copy-Item -Path "$sourceAllureResultsDir\*" -Destination $allureResultsCombinedDir -Recurse -Force
+} else {
+    Write-Error -Message "The source Allure results directory $sourceAllureResultsDir does not exist. Make sure the tests have been run and generated Allure results." -Category ObjectNotFound
+    exit
+}       
+Write-Output "************************"
+Write-Output "**"
+Write-Output "**  Done!"
+Write-Output "**"
+Write-Output "************************"
