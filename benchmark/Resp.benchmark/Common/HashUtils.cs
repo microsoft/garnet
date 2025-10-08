5e794aab09d07deaa6ab7b05ba4dcb184ab59585
Vasileios Zois <vazois@microsoft.com>
2025-10-07 20:53:43 -0700

    WIP: update benchmark

--- benchmark/Resp.benchmark/Common/HashUtils.cs
+++ benchmark/Resp.benchmark/Common/HashUtils.cs
@@ -45,5 +45,80 @@ namespace Resp.benchmark
             var rotated = (rotate >> 4) | (rotate << 60);
             return (int)(rotated ^ (rotated >> 32));
         }
+
+        public static unsafe ulong MurmurHash2x64A(Span<byte> bString, uint seed = 0)
+        {
+            fixed (byte* p = bString)
+            {
+                return MurmurHash2x64A(p, bString.Length, seed);
+            }
+        }
+
+        /// <summary>
+        /// MurmurHash2 Get 64-bit hash code for a byte array
+        /// </summary>
+        /// <param name="bString"></param>
+        /// <param name="len"></param>
+        /// <param name="seed"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe ulong MurmurHash2x64A(byte* bString, int len, uint seed = 0)
+        {
+            ulong m = (ulong)0xc6a4a7935bd1e995;
+            int r = 47;
+            ulong h = seed ^ ((ulong)len * m);
+            byte* data = bString;
+            byte* end = data + (len - (len & 7));
+
+            while (data != end)
+            {
+                ulong k;
+                k = (ulong)data[0];
+                k |= (ulong)data[1] << 8;
+                k |= (ulong)data[2] << 16;
+                k |= (ulong)data[3] << 24;
+                k |= (ulong)data[4] << 32;
+                k |= (ulong)data[5] << 40;
+                k |= (ulong)data[6] << 48;
+                k |= (ulong)data[7] << 56;
+
+                k *= m;
+                k ^= k >> r;
+                k *= m;
+                h ^= k;
+                h *= m;
+
+                data += 8;
+            }
+
+            int cs = len & 7;
+
+            if (cs >= 7)
+                h ^= ((ulong)data[6] << 48);
+
+            if (cs >= 6)
+                h ^= ((ulong)data[5] << 40);
+
+            if (cs >= 5)
+                h ^= ((ulong)data[4] << 32);
+
+            if (cs >= 4)
+                h ^= ((ulong)data[3] << 24);
+
+            if (cs >= 3)
+                h ^= ((ulong)data[2] << 16);
+
+            if (cs >= 2) h ^= ((ulong)data[1] << 8);
+            if (cs >= 1)
+            {
+                h ^= (ulong)data[0];
+                h *= m;
+            }
+
+            h ^= h >> r;
+            h *= m;
+            h ^= h >> r;
+            return h;
+        }
     }
 }
