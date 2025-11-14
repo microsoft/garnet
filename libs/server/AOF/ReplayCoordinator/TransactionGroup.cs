491d740de892e1fdd231c77cdf1855a68bce4f3f
Vasileios Zois <vazois@microsoft.com>
2025-11-14 14:21:04 -0800

    cleanup and comments

--- libs/server/AOF/ReplayCoordinator/TransactionGroup.cs
+++ libs/server/AOF/ReplayCoordinator/TransactionGroup.cs
@@ -6,12 +6,18 @@ using System.Collections.Generic;
 namespace Garnet.server
 {
     /// <summary>
-    /// Transaction group contains logAccessMap and list of operations associated with this Txn
+    /// Transaction group contains list of operations associated with a given transaction
     /// </summary>
     public class TransactionGroup
     {
+        /// <summary>
+        /// Transaction operation buffer
+        /// </summary>
         public List<byte[]> operations = [];
 
+        /// <summary>
+        /// Clear the underlying buffer that holds the individual transaction operations
+        /// </summary>
         public void Clear() => operations.Clear();
     }
 }
