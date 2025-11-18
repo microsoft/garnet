f9797c296742572b1fb73296cb7e6d883858ef84
kevin-montrose <kmontrose@microsoft.com>
2025-11-18 11:46:30 -0500

    Implement ReadWithPrefetch in Tsavorite (#1431)
    * implement ReadWithPrefetch (pulled off of vectorApiPoC work)
    
    * move MGET (normal and scatter-gather) onto ReadWithPrefetch
    
    * address feedback
    
    * correctly update session metrics with new MGET impls
    
    * address feedback

---
+++ libs/storage/Tsavorite/cs/src/core/Index/Interfaces/IReadArgBatch.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+namespace Tsavorite.core
+{
+    /// <summary>
+    /// Batch of arguments to a read operation, including key, input and output
+    /// </summary>
+    /// <typeparam name="TKey">Type of key</typeparam>
+    /// <typeparam name="TInput">Type of input</typeparam>
+    /// <typeparam name="TOutput">Type of output</typeparam>
+    public interface IReadArgBatch<TKey, TInput, TOutput>
+    {
+        /// <summary>
+        /// Count of keys/args/outputs.
+        /// </summary>
+        int Count { get; }
+
+        /// <summary>
+        /// Get <paramref name="i"/>th key.
+        /// </summary>
+        void GetKey(int i, out TKey key);
+
+        /// <summary>
+        /// Get <paramref name="i"/>th input.
+        /// </summary>
+        void GetInput(int i, out TInput input);
+
+        /// <summary>
+        /// Get <paramref name="i"/>th output.
+        /// </summary>
+        void GetOutput(int i, out TOutput output);
+
+        /// <summary>
+        /// Set <paramref name="i"/>th output.
+        /// </summary>
+        void SetOutput(int i, TOutput output);
+
+        /// <summary>
+        /// Set <paramref name="i"/>th status.
+        /// </summary>
+        void SetStatus(int i, Status status);
+    }
+}
