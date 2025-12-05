f33ff6aa5e5194ee4eb1e8ec986d40682577cef1
Vasileios Zois <96085550+vazois@users.noreply.github.com>
2025-12-05 13:04:34 -0800

    fix txn replay for fuzzy region (#1412)
    * fix txn replay for fuzzy region
    
    * renaming and comments
    
    * fix test waiting logic
    
    * add aof replay coordinator
    
    * augment tests
    
    * cleanup and comments
    
    * fix formatting
    
    * cleanup
    
    * revert dispose at recover to avoid breaking tests
    
    * adding comments and minor cleanup
    
    * addressing comments
    
    * ensure txnManager.Commit on replay does not write to replica AOF
    
    * assert TxnManager Aof disabled when replaying
    
    * ensure read-only txn at replica do not write into the AOF
    
    * skip writes to the AOF transaction contains only read operations
    
    * test fix
    
    * bump version

---
+++ libs/server/AOF/ReplayCoordinator/AofReplayContext.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using Garnet.common;
+using Garnet.networking;
+
+namespace Garnet.server
+{
+    /// <summary>
+    /// AofReplayContext
+    /// </summary>
+    public sealed class AofReplayContext
+    {
+        public readonly List<byte[]> fuzzyRegionOps = [];
+        public readonly Queue<TransactionGroup> txnGroupBuffer = [];
+        public readonly Dictionary<int, TransactionGroup> activeTxns = [];
+
+        public readonly RawStringInput storeInput;
+        public readonly ObjectInput objectStoreInput;
+        public CustomProcedureInput customProcInput;
+        public readonly SessionParseState parseState;
+
+        public readonly byte[] objectOutputBuffer;
+
+        public MemoryResult<byte> output;
+
+        /// <summary>
+        /// Fuzzy region of AOF is the region between the checkpoint start and end commit markers.
+        /// This regions can contain entries in both (v) and (v+1) versions. The processing logic is:
+        /// 1) Process (v) entries as is.
+        /// 2) Store the (v+1) entries in a buffer.
+        /// 3) At the end of the fuzzy region, take a checkpoint
+        /// 4) Finally, replay the buffered (v+1) entries.
+        /// </summary>
+        public bool inFuzzyRegion = false;
+
+        /// <summary>
+        /// AofReplayContext constructor
+        /// </summary>
+        public AofReplayContext()
+        {
+            parseState.Initialize();
+            storeInput.parseState = parseState;
+            objectStoreInput.parseState = parseState;
+            customProcInput.parseState = parseState;
+            objectOutputBuffer = GC.AllocateArray<byte>(BufferSizeUtils.ServerBufferSize(new MaxSizeSettings()), pinned: true);
+        }
+
+        /// <summary>
+        /// Add transaction group to this replay buffer
+        /// </summary>
+        /// <param name="sessionID"></param>
+        public void AddTransactionGroup(int sessionID)
+            => activeTxns[sessionID] = new();
+
+        /// <summary>
+        /// Add transaction group to fuzzy region buffer
+        /// </summary>
+        /// <param name="group"></param>
+        /// <param name="commitMarker"></param>
+        public void AddToFuzzyRegionBuffer(TransactionGroup group, ReadOnlySpan<byte> commitMarker)
+        {
+            // Add commit marker operation
+            fuzzyRegionOps.Add(commitMarker.ToArray());
+            // Enqueue transaction group
+            txnGroupBuffer.Enqueue(group);
+        }
+    }
+}
