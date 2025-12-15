987ac96f73a5e90aa0f7ea3a560296c8177e9e17
tiagonapoli <tiagonapoli@microsoft.com>
2025-12-15 14:37:08 -0300

    typo

--- libs/server/Resp/Vector/RespServerSessionVectors.cs
+++ libs/server/Resp/Vector/RespServerSessionVectors.cs
@@ -1006,7 +1006,7 @@ namespace Garnet.server
 
             if (parseState.Count != 1)
             {
-                return AbortWithWrongNumberOfArguments("VDIM");
+                return AbortWithWrongNumberOfArguments("VINFO");
             }
 
             var key = parseState.GetArgSliceByRef(0);
