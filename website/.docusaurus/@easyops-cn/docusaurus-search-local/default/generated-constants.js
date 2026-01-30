67de850ad351b2cc70528a017d397a0039c4349a
darrenge <darrenge@microsoft.com>
2026-01-30 14:39:39 -0800

    Clean history for continuousbenchmark

---
+++ website/.docusaurus/@easyops-cn/docusaurus-search-local/default/generated-constants.js
@@ -0,0 +1,6 @@
+import lunr from "C:\\GarnetGitHub\\website\\node_modules\\lunr\\lunr.js";
+export const removeDefaultStopWordFilter = [];
+export const language = ["en"];
+export const searchIndexUrl = "search-index{dir}.json?_=cebfd625";
+export const searchResultLimits = 8;
+export const fuzzyMatchingDistance = 1;
