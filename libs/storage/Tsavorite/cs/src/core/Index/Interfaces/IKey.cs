e40f8d6eadb917d0a2a8b85dc8675af56ade9569
Kevin Montrose <kmontrose@microsoft.com>
2026-02-26 11:00:57 -0500

    commit new files

---
+++ libs/storage/Tsavorite/cs/src/core/Index/Interfaces/IKey.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+
+namespace Tsavorite.core
+{
+    /// <summary>
+    /// Defines a key type for Tsavorite operations, providing hashing and equality comparison.
+    /// </summary>
+    public interface IKey
+    {
+        /// <summary>
+        /// Get 64-bit hash code for this key.
+        /// </summary>
+        long GetKeyHashCode64();
+
+        /// <summary>
+        /// The raw bytes of this key.
+        /// </summary>
+        ReadOnlySpan<byte> KeyBytes { get; }
+
+        /// <summary>
+        /// Compare this key for equality with another key.
+        /// </summary>
+        /// <typeparam name="TOther">The type of the other key.</typeparam>
+        /// <param name="other">The other key to compare against.</param>
+        bool KeysEqual<TOther>(TOther other) where TOther : IKey
+#if NET9_0_OR_GREATER
+            , allows ref struct
+#endif
+            ;
+    }
+}
---
+++ libs/storage/Tsavorite/cs/src/core/Utilities/SpanByteKey.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+
+namespace Tsavorite.core
+{
+    /// <summary>
+    /// A readonly ref struct that wraps a <see cref="ReadOnlySpan{T}"/> of bytes as an <see cref="IKey"/> implementation.
+    /// </summary>
+    public readonly
+#if NET9_0_OR_GREATER
+        ref
+#endif
+        struct SpanByteKey : IKey
+    {
+#if NET9_0_OR_GREATER
+        private readonly ReadOnlySpan<byte> _keyBytes;
+#else
+        
+#endif
+
+        /// <inheritdoc/>
+        public readonly ReadOnlySpan<byte> KeyBytes
+        {
+            get
+            {
+#if NET9_0_OR_GREATER
+                return _keyBytes;
+#else
+                return default;
+#endif
+            }
+        }
+
+        /// <summary>
+        /// Create a new SpanByteKey wrapping the given bytes.
+        /// </summary>
+        public SpanByteKey(ReadOnlySpan<byte> key)
+        {
+#if NET9_0_OR_GREATER
+            _keyBytes = key;
+#else
+
+#endif
+        }
+
+        /// <inheritdoc/>
+        public readonly long GetKeyHashCode64() => SpanByteComparer.StaticGetHashCode64(KeyBytes);
+
+        /// <inheritdoc/>
+        public readonly bool KeysEqual<TOther>(TOther other) where TOther : IKey
+#if NET9_0_OR_GREATER
+            , allows ref struct
+#endif
+            => KeyBytes.SequenceEqual(other.KeyBytes);
+
+        /// <summary>
+        /// Explicit conversion from <see cref="ReadOnlySpan{T}"/> of bytes.
+        /// 
+        /// TODO: We could maybe obviate this with a set of extension methods on Tsavorite context?
+        /// </summary>
+        public static explicit operator SpanByteKey(ReadOnlySpan<byte> key) => new(key);
+    }
+}
