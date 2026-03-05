08db206c9d537ca82b03192372efd8586f87d84d
Vasileios Zois <vazois@microsoft.com>
2026-03-04 16:12:29 -0800

    cleanup LeaderFollowerBarrier

--- libs/common/Synchronization/LeaderFollowerBarrier.cs
+++ libs/common/Synchronization/LeaderFollowerBarrier.cs
@@ -12,13 +12,49 @@ namespace Garnet.common
     /// The leader signals work readiness, waits for all participants to complete, then the cycle repeats.
     /// Participants wait for work signal, process, signal completion, then wait for reset before next cycle.
     /// </summary>
-    /// <param name="participantCount">Number of participant tasks that will process work.</param>
-    public sealed class LeaderFollowerBarrier(int participantCount)
+    public sealed class LeaderFollowerBarrier
     {
-        readonly int participantCount = participantCount;
+        readonly int participantCount;
         readonly SemaphoreSlim workReady = new(0);
         readonly SemaphoreSlim workCompleted = new(0);
         readonly SemaphoreSlim resetReady = new(0);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LeaderFollowerBarrier"/> class.
+        /// </summary>
+        /// <param name="participantCount">Number of participant tasks that will process work.</param>
+        public LeaderFollowerBarrier(int participantCount)
+        {
+            ArgumentOutOfRangeException.ThrowIfLessThan(participantCount, 1);
+            this.participantCount = participantCount;
+        }
+
+        static TimeSpan ProcessTimeSpan(TimeSpan timeout)
+            => timeout == default ? Timeout.InfiniteTimeSpan : timeout;
+
+        /// <summary>
+        /// Leader: Waits for all participants to complete, then resets for next cycle.
+        /// </summary>
+        public bool WaitCompleted(TimeSpan timeout = default, CancellationToken cancellationToken = default)
+        {
+            var waitTimeout = ProcessTimeSpan(timeout);
+            for (var i = 0; i < participantCount; i++)
+            {
+                if (!workCompleted.Wait(waitTimeout, cancellationToken))
+                    return false;
+            }
+
+            resetReady.Release(participantCount);
+            return true;
+        }
+
+        /// <summary>
+        /// Participant: Waits for work signal from leader.
+        /// </summary>
+        public async Task WaitReadyWorkAsync(CancellationToken cancellationToken = default)
+        {
+            await workReady.WaitAsync(cancellationToken).ConfigureAwait(false);
+        }
 
         /// <summary>
         /// Leader: Signals all participants that work is ready.
@@ -29,57 +65,12 @@ namespace Garnet.common
         }
 
         /// <summary>
-        /// Leader: Waits for all participants to complete, then resets for next cycle.
-        /// </summary>
-        public bool WaitCompleted(TimeSpan timeout = default, CancellationToken cancellationToken = default)
-            => WaitCompletedAsync(timeout == default ? Timeout.InfiniteTimeSpan : timeout, cancellationToken).GetAwaiter().GetResult();
-
-        /// <summary>
-        /// Leader: Waits for all participants to complete, then resets for next cycle.
-        /// </summary>
-        public async Task<bool> WaitCompletedAsync(TimeSpan timeout = default, CancellationToken cancellationToken = default)
-        {
-            var waitTimeout = timeout == default ? Timeout.InfiniteTimeSpan : timeout;
-
-            // Wait for all participants to signal completion
-            for (var i = 0; i < participantCount; i++)
-            {
-                if (!await workCompleted.WaitAsync(waitTimeout, cancellationToken))
-                    return false;
-            }
-
-            // Reset the barrier by signaling all participants to proceed to next wait
-            resetReady.Release(participantCount);
-            return true;
-        }
-
-        /// <summary>
-        /// Participant: Waits for work signal from leader.
-        /// </summary>
-        public void WaitReadyWork(CancellationToken cancellationToken = default)
-            => WaitReadyWorkAsync(cancellationToken).GetAwaiter().GetResult();
-
-        /// <summary>
-        /// Participant: Waits for work signal from leader.
-        /// </summary>
-        public async Task WaitReadyWorkAsync(CancellationToken cancellationToken = default)
-        {
-            await workReady.WaitAsync(cancellationToken);
-        }
-
-        /// <summary>
         /// Participant: Signals completion and waits for leader to reset.
         /// </summary>
         public void SignalCompleted(CancellationToken cancellationToken = default)
-            => SignalCompletedAsync(cancellationToken).GetAwaiter().GetResult();
-
-        /// <summary>
-        /// Participant: Signals completion and waits for leader to reset.
-        /// </summary>
-        public async Task SignalCompletedAsync(CancellationToken cancellationToken = default)
         {
             workCompleted.Release();
-            await resetReady.WaitAsync(cancellationToken);
+            resetReady.Wait(cancellationToken);
         }
     }
 }
