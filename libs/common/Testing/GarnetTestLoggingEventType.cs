f0bd83c407435f41bcda11346043d62c80e2b64b
Vasileios Zois <vazois@microsoft.com>
2026-04-16 11:43:55 -0700

    implemented targetted logging API for testing

---
+++ libs/common/Testing/GarnetTestLoggingEventType.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Microsoft.Extensions.Logging;
+
+namespace Garnet.common
+{
+    public enum GarnetTestLoggingEventType : int
+    {
+        LogPrimaryStreamType
+    };
+
+    public struct GarnetTestLoggingEvent
+    {
+        public GarnetTestLoggingEventType type;
+        public string msg;
+
+        public override string ToString() => $"<{type}>: {msg}";
+    }
+
+    public static class LoggingExtensions
+    {
+        public static void LogTesting(this ILogger logger, LogLevel logLevel, GarnetTestLoggingEvent state)
+        {
+            logger?.Log(LogLevel.Critical,
+                eventId: default,
+                state: state,
+                exception: null,
+                formatter: static (state, _) => $"{state}");
+        }
+    }
+}
