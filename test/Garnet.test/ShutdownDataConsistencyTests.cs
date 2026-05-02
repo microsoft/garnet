c6fd43b89674f8f52749506458ea822b6a615f00
김유석(Yu Seok Kim) <yuseok.kim@edushare.kr>
2026-05-03 01:08:15 +0900

    Update ShutdownDataConsistencyTests.cs
    - by dotnet format, re allocate using lines

--- test/Garnet.test/ShutdownDataConsistencyTests.cs
+++ test/Garnet.test/ShutdownDataConsistencyTests.cs
@@ -1,12 +1,12 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-using Allure.NUnit;
-using NUnit.Framework;
-using NUnit.Framework.Legacy;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Allure.NUnit;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
 using StackExchange.Redis;
 
 namespace Garnet.test
