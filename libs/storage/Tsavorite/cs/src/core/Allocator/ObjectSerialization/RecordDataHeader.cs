8e84d54ff5482d09372815c47ad9be8865c17581
Ted Hart <15467143+TedHartMS@users.noreply.github.com>
2026-06-01 06:04:30 -0700

    Revert format fixes for RecordDataHeader.cs

--- libs/storage/Tsavorite/cs/src/core/Allocator/ObjectSerialization/RecordDataHeader.cs
+++ libs/storage/Tsavorite/cs/src/core/Allocator/ObjectSerialization/RecordDataHeader.cs
@@ -1,1 +1,13 @@
-<Adjusted content after fixing format errors>
+// Reverting content to its previous state.
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using static Tsavorite.core.Utility;
+
+namespace Tsavorite.core
+{
+    /// <summary>
+    /// Fixed 8-byte header describing the data layout of the record. Atomic assignment is guaranteed on...
