8c300dadd3af78c77df8d8a8e903ac28f95afdec
kevin-montrose <kmontrose@microsoft.com>
2026-06-05 14:29:03 -0400

    [V1] Fix VSIM COUNT when FILTER is set (#1857)
    * failing tests for VSIM with attributes & counts; we're returning too much data
    
    * fix VSIM COUNT when FILTER is set
    
    * correct comment
    
    * rework filtering loop to produce correct results and result count

--- libs/server/Resp/Vector/RespServerSessionVectors.cs
+++ libs/server/Resp/Vector/RespServerSessionVectors.cs
@@ -859,7 +859,7 @@ namespace Garnet.server
 
                                 var totalFound = distancesSpan.Length;
 
-                                // Compute output count: if bitmap is present, popcount it; otherwise all results
+                                // Compute max output count: if bitmap is present, popcount it; otherwise all results
                                 int outputCount;
                                 if (hasFilter)
                                 {
@@ -872,6 +872,10 @@ namespace Garnet.server
                                     outputCount = totalFound;
                                 }
 
+                                // Limit to what is actually asked for
+                                outputCount = Math.Min(count.Value, outputCount);
+
+                                // Each flag doubles output
                                 var arrayItemCount = outputCount;
                                 if (withScores.Value)
                                 {
@@ -885,7 +889,10 @@ namespace Garnet.server
                                 while (!RespWriteUtils.TryWriteArrayLength(arrayItemCount, ref dcurr, dend))
                                     SendAndReset();
 
-                                for (var resultIndex = 0; resultIndex < totalFound; resultIndex++)
+                                var writtenCount = 0;
+                                var resultIndex = 0;
+
+                                while (writtenCount < outputCount)
                                 {
                                     ReadOnlySpan<byte> elementData;
 
@@ -930,6 +937,8 @@ namespace Garnet.server
                                             var skipAttrLen = BinaryPrimitives.ReadInt32LittleEndian(remaininingAttributes);
                                             remaininingAttributes = remaininingAttributes[(sizeof(int) + skipAttrLen)..];
                                         }
+
+                                        resultIndex++;
                                         continue;
                                     }
 
@@ -964,6 +973,9 @@ namespace Garnet.server
                                         var attrLen = BinaryPrimitives.ReadInt32LittleEndian(remaininingAttributes);
                                         remaininingAttributes = remaininingAttributes[(sizeof(int) + attrLen)..];
                                     }
+
+                                    resultIndex++;
+                                    writtenCount++;
                                 }
                             }
                         }
