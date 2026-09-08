a070a2a1687fe07927190e23e4bb8f174cc1fd88
Kevin Montrose <kmontrose@microsoft.com>
2026-09-08 11:50:36 -0400

    hack hack hack; this blocking wait is a problem with SocketSet, for testing purposes just assume success

--- libs/cluster/Session/ReplicaOfCommand.cs
+++ libs/cluster/Session/ReplicaOfCommand.cs
@@ -86,21 +86,23 @@ namespace Garnet.cluster
                 );
 
                 // Cannot avoid blocking here we're on the network thread
-                var (success, errorMessage) =
-                    AsyncUtils.BlockingWait(
+                _ = 
+                //var (success, errorMessage) =
+                //    AsyncUtils.BlockingWait(
                         clusterProvider.serverOptions.ReplicaDisklessSync ?
                             clusterProvider.replicationManager.TryReplicateDisklessSyncAsync(this, syncOpts) :
                             clusterProvider.replicationManager.TryReplicateDiskbasedSyncAsync(this, syncOpts)
-                    );
-
+                    //)
+                    ;
+                
                 clusterProvider.storeWrapper.StartReplicaTasks();
 
-                if (!success)
-                {
-                    while (!RespWriteUtils.TryWriteError(errorMessage.Span, ref dcurr, dend))
-                        SendAndReset();
-                }
-                else
+                //if (!success)
+                //{
+                //    while (!RespWriteUtils.TryWriteError(errorMessage.Span, ref dcurr, dend))
+                //        SendAndReset();
+                //}
+                //else
                 {
                     while (!RespWriteUtils.TryWriteDirect(CmdStrings.RESP_OK, ref dcurr, dend))
                         SendAndReset();
